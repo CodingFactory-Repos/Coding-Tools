aa4de9df60b3947e7dbc974793e432cc3e69a9d7
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2022-12-06 11:45:28 +0100

    feat(vue-vite): full front-end configuration with vue + vite + eslint + prettier + tailwind + commitlint + basic templating

---
+++ front-end/src/layout/AppLayout.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="view">
+		<HeaderLayout></HeaderLayout>
+
+		<div class="content">
+			<router-view></router-view>
+		</div>
+
+		<FooterLayout></FooterLayout>
+	</div>
+</template>
+
+<script setup lang="ts">
+import HeaderLayout from './HeaderLayout.vue';
+import FooterLayout from './FooterLayout.vue';
+</script>
---
+++ front-end/src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+
+const routes: Array<RouteRecordRaw> = [
+	{
+		path: '/',
+		component: () => import('../layout/AppLayout.vue'),
+		children: [
+			{ path: '', component: () => import('../views/HomeView.vue') },
+			{ path: '/about', component: () => import('../views/AboutView.vue') },
+		],
+	},
+];
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+});
+
+export default router;
