95d83c3efcac5749d87ece069bee62bc11aaff45
Kevw94 <pinerokevin@icloud.com>
2022-12-07 12:19:45 +0100

    Implement userRepository for mongoDb + interface + add type for collection

--- back-end/src/api/base/users/interfaces/users.interface.ts
+++ back-end/src/api/base/users/interfaces/users.interface.ts
@@ -1,5 +1,48 @@
+import { ObjectId } from "mongodb";
 export interface User {
-	id: number;
-	email: string;
+	_id?: ObjectId;
+	profile: UserProfile
+	status: number;
+	pedago?: PedagoProfile;
+	productOwner?: ProductOwnerProfile;
+	student?: Student;
+}
+
+
+export interface UserProfile {
+	firstName: string;
+	lastName: string;
 	password: string;
+	email: string;
+	birthDate: Date;
+	locationSite: string;
+	linkedinProfile: string;
+	profilePicture: string;
+}
+
+export interface PedagoProfile {
+	notifications: Array<Notifications>
+}
+
+export interface Notifications {
+	// À définir
+}
+
+export interface ProductOwnerProfile {
+	age: number;
+	disciplinesTaught: Array<string>;
+}
+
+export interface Student {
+	Age: number;
+	academicYear?: AcademicYear;
+}
+
+export interface AcademicYear {
+	title: string;
+	siteLocation: string;
+	created: Date;
+	students?: Array<User>;
+	companyWork: string;
+	phoneNumber: string;
 }
