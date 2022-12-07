5b4bcfcb49e97f24b8f2bedf4eac17bafd9d3bfa
codefactor-io <support@codefactor.io>
2022-12-07 08:48:36 +0000

    [CodeFactor] Apply fixes

--- back-end/tests/index.test.ts
+++ back-end/tests/index.test.ts
@@ -3,15 +3,15 @@ import App from '@/app';
 import { IndexController } from '@controllers/index.controller';
 
 afterAll(async () => {
-  await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
+	await new Promise<void>((resolve) => setTimeout(() => resolve(), 500));
 });
 
 describe('Testing Index', () => {
-  describe('[GET] /', () => {
-    it('response statusCode 200', () => {
-      const app = new App([IndexController]);
+	describe('[GET] /', () => {
+		it('response statusCode 200', () => {
+			const app = new App([IndexController]);
 
-      return request(app.getServer()).get('/').expect(200);
-    });
-  });
+			return request(app.getServer()).get('/').expect(200);
+		});
+	});
 });
