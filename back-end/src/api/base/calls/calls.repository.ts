345cdf3517e49831d8d9dbaf96e68d8825d236df
Kevw94 <pinerokevin@icloud.com>
2022-12-07 15:54:48 +0100

    implement calls module + add repository + interface

---
+++ back-end/src/api/base/calls/calls.repository.ts
@@ -0,0 +1,33 @@
+import { mongodb } from '@/config/config';
+import { ObjectId, Filter, UpdateFilter, FindOneAndUpdateOptions } from 'mongodb';
+import { Call } from '../courses/interfaces/courses.interface';
+
+export class CallsRepository {
+	static callsCollection = mongodb.collection<Call>('calls');
+
+	get courses() {
+		return CallsRepository.callsCollection;
+	}
+
+	async createCall(query: Call) {
+		return this.courses.insertOne(query);
+	}
+
+	async updateOneCall(query: Filter<Call>, update: Partial<Call> | UpdateFilter<Call>) {
+		return this.courses.updateOne(query, update);
+	}
+
+	async findOneAndUpdateCall(
+		query: Filter<Call>,
+		update: Partial<Call>,
+		options: FindOneAndUpdateOptions = undefined,
+	) {
+		return this.courses.findOneAndUpdate(query, update, options);
+	}
+
+	async callExist(query: Filter<Call>) {
+		const options = { projection: { _id: 1 } };
+		return this.courses.findOne(query, options);
+	}
+	// Mongo repo for the courses collection
+}
