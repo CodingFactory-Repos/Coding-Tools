5396b2035a1ec1acdf70870fbb8028f0d93c44f2
Kevw94 <pinerokevin@icloud.com>
2022-12-08 18:19:16 +0100

    implement logic for creating a new retro and implement drag and drop pixie JS -> WIP

---
+++ front-end/src/components/retrospectiveComponents/PixieLayout.vue
@@ -0,0 +1,89 @@
+<template>
+	<canvas ref="pixi" id="pixi"></canvas>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue';
+import * as PIXI from 'pixi.js'
+
+export default defineComponent({
+	setup() {
+		const pixi = ref();
+
+		function drawPixi() {
+
+			const app = new PIXI.Application({
+				width: window.innerWidth,
+				height: window.innerHeight,
+				// width: 640,
+				// height: 360,
+				antialias: true,
+				view: pixi.value,
+				backgroundColor: 0XAAAA,
+				// backgroundAlpha: 0
+			})
+
+			const rectangle = new PIXI.Graphics();
+			rectangle.interactive = true;
+			rectangle.cursor = "pointer";
+			rectangle.on('pointerdown', onClick);
+			rectangle.on('pointerdown', onDragStart, rectangle)
+
+			rectangle.lineStyle({ width: 4, color: 0xFF3300, alpha: 1 });
+			rectangle.beginFill(0x66CCFF);
+			rectangle.drawRect(0, 0, 64, 64);
+			rectangle.endFill();
+			rectangle.x = 170;
+			rectangle.y = 170;
+			app.stage.addChild(rectangle);
+			function onClick() {
+				rectangle.scale.x *= 1.25;
+				rectangle.scale.y *= 1.25;
+			}
+
+			const message = new PIXI.Text("Hello Pixi!");
+			app.stage.addChild(message);
+			message.position.set(54, 96);
+
+			let dragTarget: any = null;
+			app.stage.interactive = true;
+			app.stage.hitArea = app.screen;
+			app.stage.on('pointerup', onDragEnd);
+			app.stage.on('pointerupoutside', onDragEnd);
+			function onDragMove(event: any) {
+				if (dragTarget) {
+					dragTarget.parent.toLocal(event.global, null, dragTarget.position);
+				}
+			}
+
+			function onDragStart(this: any) {
+				// store a reference to the data
+				// the reason for this is because of multitouch
+				// we want to track the movement of this particular touch
+				// this.data = event.data;
+				this.alpha = 0.5;
+				dragTarget = this;
+				app.stage.on('pointermove', onDragMove);
+			}
+
+			function onDragEnd() {
+				if (dragTarget) {
+					app.stage.off('pointermove', onDragMove);
+					dragTarget.alpha = 1;
+					dragTarget = null;
+				}
+			}
+
+		};
+
+		onMounted(() => {
+			drawPixi()
+		})
+
+		return {
+			pixi,
+			drawPixi
+		}
+	}
+})
+</script>
