7754bbab84f08431ab761eac3ed0f1e84ff0d28d
Philémon Wild <philemon.wild@gmail.com>
2022-12-09 11:56:13 +0100

    G10/bugfix(rollcall): removed studentList

--- front-end/src/components/RollCallComponents/StudentList.vue
+++ front-end/src/components/RollCallComponents/StudentList.vue
@@ -1,27 +1,7 @@
 <script>
 import StudentCell from "../RollCallComponents/StudentCell.vue";
 
-
-let testStudentList = [
-    //replace this list with the list of Students from db
-    {
-        name:"jean-claude",
-        status:"absent"
-    },
-    {
-        name:"jean-calude",
-        status:"absent"
-    }
-]
-
-
 export default {
-    setup(){
-        testStudentList
-        return {
-            testStudentList
-        }
-    },
     name:"StudentList",
     components: {
         StudentCell
