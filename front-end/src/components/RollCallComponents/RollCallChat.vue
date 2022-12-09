7f4124a2fdb0e6570d184f576438e82e87771b9a
Philémon Wild <philemon.wild@gmail.com>
2022-12-09 15:52:53 +0100

    G10/feature(chat): added chat v 0.1

---
+++ front-end/src/components/RollCallComponents/RollCallChat.vue
@@ -0,0 +1,74 @@
+<template>
+    <div ref="talkjs" style="width: 90%; margin: 30px; height: 500px">
+        <i>Loading chat...</i>
+    </div>
+</template>
+
+<script>
+import Talk from 'talkjs';
+
+
+export default {
+    async mounted() {
+        await Talk.ready
+
+        const third = new Talk.User({
+            id:"09098",
+            name:'KrukMak',
+            email:"jakub@jakub.mail",
+            role:"default",
+        })
+
+        const me = new Talk.User({
+        id: '001',
+        name: 'Philémax',  
+        email: "philemon@oui.oui",
+        welcomeMessage: "Ecrire message svp", 
+        //The welcomeMessage is the first message any other user will see when they start a chat with this user
+        role: "booker",
+        })
+
+        const other = new Talk.User({
+        id: '654321',
+        name: 'Philémoins',
+        email: 'Sebastian@example.com',
+        photoUrl: 'https://talkjs.com/images/avatar-5.jpg',
+        welcomeMessage: 'Ecrire la con de toi',
+        role: 'default',
+        });
+
+        const talkSession = new Talk.Session({
+            appId: 'tVBikeMj',
+            me: me
+        })
+
+        const conversation = talkSession.getOrCreateConversation(
+        Talk.oneOnOneId(me, other)
+        );
+
+        conversation.setParticipant(me);
+        conversation.setParticipant(other);
+        conversation.setParticipant(third);
+
+        var inbox = talkSession.createInbox();
+        inbox.select(conversation); 
+
+        inbox.mount(this.$refs.talkjs);
+
+    },
+    name:"Chat",
+    data() {
+        return {
+            // get message history through db
+            messageList: []
+        }
+    },
+    methods: {
+        messageHandler() {
+            let message = this.$refs.inputMessage.value
+            this.messageList.push(message);
+            
+        }
+    },
+}
+</script>
--- front-end/src/views/RollCall.vue
+++ front-end/src/views/RollCall.vue
@@ -1,11 +1,13 @@
 <script>
 import StudentList from '../components/RollCallComponents/StudentList.vue';
 import QrCode from '../components/RollCallComponents/QrCode.vue';
+import Chat from '../components/RollCallComponents/RollCallChat.vue';
 
 export default {
     components:{
         StudentList,
-        QrCode
+        QrCode,
+        Chat,
     }
 }
 
@@ -13,4 +15,5 @@ export default {
 <template>
         <QrCode url="https://www.impots.gouv.fr/accueil" />
         <StudentList />
+        <Chat/>
 </template>
