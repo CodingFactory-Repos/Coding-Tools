f52d6cde1343dd52a7968bc384604ea7d861e076
Philémon Wild <philemon.wild@gmail.com>
2022-12-09 11:17:18 +0100

    G10/chores(rollcall): added StudentList and StudentCell Components

---
+++ front-end/src/components/RollCallComponents/StudentList.vue
@@ -0,0 +1,38 @@
+<script>
+import StudentCell from "../RollCallComponents/StudentCell.vue";
+
+
+let testStudentList = [
+    //replace this list with the list of Students from db
+    {
+        name:"jean-claude",
+        status:"absent"
+    },
+    {
+        name:"jean-calude",
+        status:"absent"
+    }
+]
+
+
+export default {
+    setup(){
+        testStudentList
+        return {
+            testStudentList
+        }
+    },
+    name:"StudentList",
+    components: {
+        StudentCell
+    }
+}
+
+</script>
+<template>
+    <div v-for="student in testStudentList" :key="student.name">
+        <div class="flex flex-row ">
+            <StudentCell v-bind:studentname=student.name v-bind:status=student.status />
+        </div>
+    </div>
+</template>
