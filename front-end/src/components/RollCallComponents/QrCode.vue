e8663e244f16a37acc30753052d35fa23d564915
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-12-09 16:42:18 +0100

    Linter modifications

--- front-end/src/components/RollCallComponents/QrCode.vue
+++ front-end/src/components/RollCallComponents/QrCode.vue
@@ -1,32 +1,28 @@
 <template>
-    <div>
-        <VueQRious
-        :value=url
-        @change="onDataUrlChange"
-        size="300"
-        />
-    </div>
+	<div>
+		<VueQRious :value="url" @change="onDataUrlChange" size="300" />
+	</div>
 </template>
 <script>
-import VueQRious from 'vue-qrious'
+import VueQRious from 'vue-qrious';
 
 export default {
-    name:"QrCode",
-    components:{
-        VueQRious,
-    },
-    props:{
-        url: String
-    },
-    data() {
-        return {
-            dataUrl: null,
-        }
-    },
-    computed: {
-        onDataUrlChange(dataUrl){
-            this.dataUrl = dataUrl
-        },
-    },
-}
+	name: 'QrCode',
+	components: {
+		VueQRious,
+	},
+	props: {
+		url: String,
+	},
+	data() {
+		return {
+			dataUrl: null,
+		};
+	},
+	computed: {
+		onDataUrlChange(dataUrl) {
+			this.dataUrl = dataUrl;
+		},
+	},
+};
 </script>
