8d1950cf4f7d23ea09344c35a381eab282df5b75
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-12-09 11:20:38 +0100

    Add a new route that will generate a QRCode in back as a random string of 25 characters to maintain uniqueness of our URL

--- back-end/src/api/base/calls/calls.service.ts
+++ back-end/src/api/base/calls/calls.service.ts
@@ -1,9 +1,15 @@
 import { CallsRepository } from './calls.repository';
+import { randomBytes } from 'crypto';
 
 class CallsService {
 	public repo = new CallsRepository();
 
-	// Business logic methods goes there...
+	public generateQrCode() {
+		const qr = randomBytes(16)
+			.toString('base64')
+			.replace(/[^a-zA-Z0-9]/g, '');
+		return qr;
+	}
 }
 
 export default CallsService;
