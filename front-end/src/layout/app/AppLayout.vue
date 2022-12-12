9597d730363834f39f93a6062cac04d4ae5355b3
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2022-12-12 09:30:45 +0100

    fix(layout): Fix properly the layout

--- front-end/src/layout/app/AppLayout.vue
+++ front-end/src/layout/app/AppLayout.vue
@@ -1,7 +1,7 @@
 <template>
 	<DrawerLayout/>
 
-	<div class="view top-0 right-0 w-auto h-screen text-center ml-[40px]">
+	<div class="view w-auto text-center pl-[40px]">
 		<div class="content !block">
 			<router-view></router-view>
 		</div>
@@ -11,8 +11,11 @@
 <script lang="ts" setup>
 import DrawerLayout from './DrawerLayout.vue';
 
-//! Normally it's bad practice to have two element at the root template.
-//! But since i don't want to break the css for the nav i won't fix it - Chris.
 // Check the comments at the end of the files in src/components/NavBar.vue
 // I did not delete the file because you might have added a few other things.
+
+//! top-0 right-0 w-auto h-screen text-center ml-[40px]
+// top-0 and right-0 have no effect unless it's an absolute element
+// ml-[40px] : Margin push the whole div to the left (cause a scroll), use pl-[40px] instead
+// h-screen = 100vh -> Never use vh, it causes a lot of compatibility issues with safari and iOS phone features
 </script>
--- front-end/src/layout/home/HomeLayout.vue
+++ front-end/src/layout/home/HomeLayout.vue
@@ -2,7 +2,7 @@
 	<div class="view">
 		<HeaderLayout></HeaderLayout>
 
-		<div class="content !block">
+		<div class="content pt-12 pb-12">
 			<router-view></router-view>
 		</div>
 
