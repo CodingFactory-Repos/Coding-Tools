16c76b5c2a59d183917563fbb3af3f1d5d1d5121
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2022-12-25 13:23:19 +0100

    types(class): Define graphicContainer subclass with custom even emitter

---
+++ front-end/src/lib/pixi-tools/types/subclass.ts
@@ -0,0 +1,37 @@
+import { DisplayObjectEvents } from "pixi.js";
+
+/**
+ * ArgumentMap is a type that maps the arguments of a function type to a tuple.
+ *
+ * @template T - A type that represents a set of function types.
+ */
+export type ArgumentMap<T> = {
+	[K in keyof T]: T[K] extends (...args: infer U) => any ? U : never;
+};
+
+/**
+ * ContainerEvents is an interface that extends the event emitter interface of PIXI.DisplayObject to allow for
+ * custom events to be emitted and subscribed to.
+ *
+ * @extends PIXI.DisplayObjectEvents
+ * @emits 'selectUpdated' - Emitted when the container's selection status is updated.
+ * @emits 'scaleUpdated' - Emitted when the container's scale is updated.
+ * @emits 'download' - Emitted when the container is downloaded.
+ */
+export interface ContainerEvents extends DisplayObjectEvents {
+	/**
+	 * Emitted when the container's selection status is updated.
+	 */
+	selectUpdated: [boolean];
+	
+	/**
+	 * Emitted when the container's scale is updated.
+	 */
+	scaleUpdated: [];
+	
+	/**
+	 * Emitted when the container is downloaded.
+	 */
+	download: [string];
+}
+
