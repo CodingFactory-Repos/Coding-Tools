869aefdae354903723f279712285602aff2ad561
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2022-12-25 15:26:24 +0100

    types(shape): Added the shape property interface

--- front-end/src/lib/pixi-tools/types/pixi-element-options.ts
+++ front-end/src/lib/pixi-tools/types/pixi-element-options.ts
@@ -5,7 +5,6 @@
 export namespace ElementOptions {
 	/**
 	 * An interface for scaled dimensions.
-	 * @interface ScaledDimensions
 	 * @memberof ElementOptions
 	 */
 	export interface ScaledDimensions {
@@ -14,6 +13,39 @@ export namespace ElementOptions {
 		 */
 		scale: number;
 	
+		/**
+		 * The width of the element, in pixels.
+		 */
+		width: number;
+	
+		/**
+		 * The height of the element, in pixels.
+		 */
+		height: number;
+	
+		/**
+		 * The X position of the element, in pixels.
+		 * @type {number}
+		 */
+		positionX: number;
+	
+		/**
+		 * The Y position of the element, in pixels.
+		 * @type {number}
+		 */
+		positionY: number;
+	}
+
+	/**
+	 * An interface for shape properties.
+	 * @memberof ElementOptions
+	 */
+	export interface ShapeProperties {
+		/**
+		 * The color of the shape, in hexadecimal.
+		 */
+		color: number;
+
 		/**
 		 * The width of the element, in pixels.
 		 */
