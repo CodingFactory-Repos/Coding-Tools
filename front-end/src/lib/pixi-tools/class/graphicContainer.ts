f979e465730dbc734aa6d4602860b1d78ef592e4
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2022-12-26 17:59:00 +0100

    Update the class emitter and resolve the conflict by splitting the event between dispatch and inherited by extends

--- front-end/src/lib/pixi-tools/class/graphicContainer.ts
+++ front-end/src/lib/pixi-tools/class/graphicContainer.ts
@@ -1,63 +1,28 @@
 import { Container, DisplayObject } from "pixi.js";
-import { ArgumentMap, ContainerEvents } from "../types";
+import { GraphicContainerEvents } from "../types";
+import { CustomEmitter } from "./customEmitter";
 
 /**
- * GraphicContainer is a subclass of PIXI.Container that extends the event emitter interface to allow
- * for custom events to be emitted and subscribed to.
- *
+ * GraphicContainer is a subclass of PIXI.Container that extends a custom event emitter with strongly-typed events.
+ * 
  * @extends PIXI.Container
- * @emits 'selectUpdated' - Emitted when the selection status of the container is updated.
- * @emits 'scaleUpdated' - Emitted when the scale of the container is updated.
- * @emits 'download' - Emitted when the container is ready to be downloaded.
+ * @emits 'dispatch.selectUpdated' - Emitted when the selection status of the container is updated.
+ * @emits 'dispatch.scaleUpdated' - Emitted when the scale of the container is updated.
+ * @emits 'dispatch.download' - Emitted when the container is ready to be downloaded.
  */
 export class GraphicContainer extends Container<DisplayObject> {
 	/**
-	 * Adds a listener for a specific event.
-	 * @param event - The name of the event to listen for.
-	 * @param fn - The event handler function.
-	 * @param context - The context for the event handler function.
-	 * @returns The GraphicContainer instance.
+	 * Dispatch is a custom emitter for this container.
+	 * @public
 	 */
-	on<T extends keyof ContainerEvents>(
-		event: T,
-		fn: (...args: ArgumentMap<ContainerEvents>[Extract<T, keyof ContainerEvents>]) => void,
-		context?: any,
-	): this {
-		//@ts-ignore
-		super.on(event, fn, context);
-		return this;
-	}
+	public dispatch: CustomEmitter<GraphicContainerEvents>;
 
 	/**
-	 * Removes a listener for a specific event.
-	 * @param event - The name of the event to remove the listener for.
-	 * @param fn - The event handler function to remove.
-	 * @param context - The context for the event handler function.
-	 * @param once - If the event handler was added with the `once` option, this should be set to `true`.
-	 * @returns The GraphicContainer instance.
-	 */
-	off<T extends keyof ContainerEvents>(
-		event: T,
-		fn: (...args: ArgumentMap<ContainerEvents>[Extract<T, keyof ContainerEvents>]) => void,
-		context?: any,
-		once?: boolean,
-	): this {
-		//@ts-ignore
-		super.off(event, fn, context, once);
-		return this;
-	}
+	* Creates a new GraphicContainer and intialize the custom emitter;
+	*/
+	constructor() {
+		super();
 
-	/**
-	 * Emits an event, triggering all registered event handlers.
-	 * @param event - The name of the event to emit.
-	 * @param args - Any arguments to pass to the event handlers.
-	 * @returns `true` if the event had listeners, `false` otherwise.
-	 */
-	emit<T extends keyof ContainerEvents>(
-		event: T,
-		...args: ArgumentMap<ContainerEvents>[Extract<T, keyof ContainerEvents>]
-	): boolean {
-		//@ts-ignore
-		return super.emit(event, ...args);
+		this.dispatch = new CustomEmitter();
 	}
 }
--- front-end/src/lib/pixi-tools/class/staticGraphics.ts
+++ front-end/src/lib/pixi-tools/class/staticGraphics.ts
@@ -1,63 +1,27 @@
+import { CustomEmitter } from './customEmitter';
 import { Graphics } from "pixi.js";
-
-import { ArgumentMap, GraphicsEvents } from '../types';
+import { StaticGraphicsEvents } from '../types';
 
 /**
- * StaticGraphics is a subclass of PIXI.Graphics that extends the event emitter interface to allow
- * for custom events to be emitted and subscribed to.
- *
+ * StaticGraphics is a subclass of PIXI.Graphics that extends a custom event emitter with strongly-typed events.
+ * 
  * @extends PIXI.Graphics
- * @emits 'updated' - Emitted when the graphics object should be updated with new size options.
- * @emits 'cleared' - Emitted when the graphics object should be cleared.
+ * @emits 'dispatch.updated' - Emitted when the graphics object should be updated with new size options.
+ * @emits 'dispatch.cleared' - Emitted when the graphics object should be cleared.
  */
 export class StaticGraphics extends Graphics {
 	/**
-	 * Adds a listener for a specific event.
-	 * @param event - The name of the event to listen for.
-	 * @param fn - The event handler function.
-	 * @param context - The context for the event handler function.
-	 * @returns The StaticGraphics instance.
+	 * Dispatch is a custom emitter for this container.
+	 * @public
 	 */
-	on<T extends keyof GraphicsEvents>(
-		event: T,
-		fn: (...args: ArgumentMap<GraphicsEvents>[Extract<T, keyof GraphicsEvents>]) => void,
-		context?: any,
-	): this {
-		//@ts-ignore
-		super.on(event, fn, context);
-		return this;
-	}
+	public dispatch: CustomEmitter<StaticGraphicsEvents>;
 
 	/**
-	 * Removes a listener for a specific event.
-	 * @param event - The name of the event to remove the listener for.
-	 * @param fn - The event handler function to remove.
-	 * @param context - The context for the event handler function.
-	 * @param once - If the event handler was added with the `once` option, this should be set to `true`.
-	 * @returns The StaticGraphics instance.
-	 */
-	off<T extends keyof GraphicsEvents>(
-		event: T,
-		fn: (...args: ArgumentMap<GraphicsEvents>[Extract<T, keyof GraphicsEvents>]) => void,
-		context?: any,
-		once?: boolean,
-	): this {
-		//@ts-ignore
-		super.off(event, fn, context, once);
-		return this;
-	}
+	* Creates a new StaticGraphics and intialize the custom emitter;
+	*/
+	constructor() {
+		super();
 
-	/**
-	 * Emits an event, triggering all registered event handlers.
-	 * @param event - The name of the event to emit.
-	 * @param args - Any arguments to pass to the event handlers.
-	 * @returns `true` if the event had listeners, `false` otherwise.
-	 */
-	emit<T extends keyof GraphicsEvents>(
-		event: T,
-		...args: ArgumentMap<GraphicsEvents>[Extract<T, keyof GraphicsEvents>]
-	): boolean {
-		//@ts-ignore
-		return super.emit(event, ...args);
+		this.dispatch = new CustomEmitter();
 	}
 }
