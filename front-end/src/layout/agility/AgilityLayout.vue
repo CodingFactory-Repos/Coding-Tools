efaa04e69d5a3ec890b8e4f232995e051ca5f310
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-01-09 23:00:31 +0100

    feat(layout): Integrate the agility layout, that's how you use it.

---
+++ front-end/src/layout/agility/AgilityLayout.vue
@@ -0,0 +1,21 @@
+<template>
+	<AgilityHeader @drawer:action="drawerAction"/>
+	<AgilityDrawer :active="isOpen">
+		<div class="pages">
+			<router-view />
+		</div>
+	</AgilityDrawer>
+</template>
+
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import AgilityHeader from './AgilityHeader.vue';
+import AgilityDrawer from './AgilityDrawer.vue';
+
+const isOpen = ref(true);
+
+const drawerAction = () => {
+	isOpen.value = !isOpen.value;
+}
+</script>
