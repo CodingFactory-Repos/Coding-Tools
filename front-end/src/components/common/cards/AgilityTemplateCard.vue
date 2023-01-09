e7be3b7b8b6c5f84a9626bba437303483699d673
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-01-10 00:03:31 +0100

    fix(alt): Add an alt to the image inside the cards

--- front-end/src/components/common/cards/AgilityTemplateCard.vue
+++ front-end/src/components/common/cards/AgilityTemplateCard.vue
@@ -2,7 +2,7 @@
 	<div class="w-32 h-full flex flex-col justify-between gap-2 relative mt-2.5 mr-2.5" style="min-width: 8rem;">
 		<span v-if="isNew" class="absolute -right-2.5 -top-2.5 px-2 bg-pink-600 rounded-lg text-white text-sm z-10">New</span>
 
-		<img v-if="url" class="w-full h-24 rounded-lg cursor-pointer select" :src="url"/>
+		<img v-if="url" class="w-full h-24 rounded-lg cursor-pointer select" :src="url" :alt="`template_${name}_img`"/>
 		<div v-else class="w-full h-24 bg-gradient-to-r from-violet-900 to-pink-600 hover:from-violet-800 hover:to-pink-500 rounded-lg flex items-center justify-center cursor-pointer">
 			<SVGLoader v-bind="{name:'add', size:'27px', color:'dark:fill-gray-400 fill-[#ffffff]'}"/>
 		</div>
