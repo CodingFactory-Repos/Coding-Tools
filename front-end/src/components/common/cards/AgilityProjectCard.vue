c6c7768d615c3dcdc971f9e0fefd8b7cb68856bd
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-01-10 00:03:31 +0100

    fix(alt): Add an alt to the image inside the cards

--- front-end/src/components/common/cards/AgilityProjectCard.vue
+++ front-end/src/components/common/cards/AgilityProjectCard.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="w-52 h-fit flex flex-col justify-between dark:bg-gray-800 bg-white p-3 rounded-lg gap-2 cursor-pointer select">
-		<img v-if="url" class="w-full h-48 bg-gray-400 rounded" :src="url"/>
+		<img v-if="url" class="w-full h-48 bg-gray-400 rounded" :src="url" :alt="`project_${title}_snapshot`"/>
 		<span class="dark:text-gray-400 text-sm font-bold">{{ title }}</span>
 	</div>
 </template>
