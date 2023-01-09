1199b20c74af26a3ff4df09d8ab10da76a7fe913
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-01-09 22:45:07 +0100

    fix(config): Remove unused configuration, it'll be done when it'll be done

--- front-end/src/config/config.ts
+++
@@ -1,8 +0,0 @@
-// Global express.js configuration
-
-export const config = {
-	api: {
-		base: import.meta.env.VITE_API_BASE_URL,
-		credentials: JSON.parse(import.meta.env.VITE_CREDENTIALS),
-	},
-};
--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,5 +1,3 @@
-// import '@/config/env.validator';
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
