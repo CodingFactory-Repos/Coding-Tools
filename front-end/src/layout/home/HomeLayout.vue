265befd5a1c95911363552fe1021e0aec944263b
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-14 23:57:12 +0100

    fix(footer): Remove the footer outside of the hero page

--- front-end/src/layout/home/HomeLayout.vue
+++ front-end/src/layout/home/HomeLayout.vue
@@ -2,15 +2,21 @@
 	<div class="view">
 		<HeaderLayout></HeaderLayout>
 
-		<div class="content mt-16 py-12 px-8 bg-white dark:bg-gray-800">
+		<div class="content bg-white mt-16 dark:bg-gray-800">
 			<router-view></router-view>
 		</div>
 
-		<FooterLayout></FooterLayout>
+		<FooterLayout v-if="isHero"></FooterLayout>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
 import HeaderLayout from './HeaderLayout.vue';
 import FooterLayout from './FooterLayout.vue';
+
+const route = useRoute();
+const isHero = computed(() => route.path === "/");
 </script>
