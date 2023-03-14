708b47744af0cc216953bf22bf1c861f87dc938b
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-15 00:01:23 +0100

    rework(auth): Implement the new auth store with error handler

--- front-end/src/store/modules/auth.store.ts
+++ front-end/src/store/modules/auth.store.ts
@@ -1,113 +1,99 @@
 import { defineStore } from 'pinia';
 
-import { AuthStore, AuthStoreSignin, AuthStoreSignup } from '@/store/interfaces/auth.interfaces';
+import { AuthStore, UserCreds } from '@/store/interfaces/auth.interfaces';
+import { STATUS } from '@/store/interfaces/axios.interface';
+import { withErrorHandler } from '@/utils/storeHandler';
+
 import {
-	apiTrySignup,
-	apiTrySignin,
-	apiTryLogout,
-	apiTrySendNewActivationEmail,
-	apiTryAccountActivate,
-	apiTrySendResetPasswordEmail,
-	apiTryResetPassword,
-	apiTryCheckResetToken,
+	trySignup,
+	trySignin,
+	tryLogout,
+	trySendNewActivationEmail,
+	tryActivateAccount,
+	trySendResetPasswordEmail,
+	tryResetPassword,
+	tryGetMe
 } from '@/api/auth-req';
 
-import Swal from 'sweetalert2';
+import { KeysRequired } from '@/interfaces/advanced-types.interface';
+import { pick } from '@/utils/object.helper';
 
-export const useAuthStore = defineStore('template', {
-	state: (): AuthStore => {
-		return {
-			isAuth: false,
-		};
-	},
+const authStoreDefaultState = (): AuthStore => ({
+	isAuth: false,
+	tempAuthUser: {},
+	tempEmailUser: "",
+	user: {},
+})
+
+export const useAuthStore = defineStore('auth', {
+	state: (): AuthStore => authStoreDefaultState(),
 	actions: {
-		async trySignup(payload: AuthStoreSignup) {
-			try {
-				const res = await apiTrySignup(payload).then((res) => res.data);
-				if (res.status === 'ok') return true;
-				return false;
-			} catch (e) {
-				Swal.fire({
-					icon: 'error',
-					title: 'An error occured when trying to register your account',
-				});
-				return false;
-			}
+		signup: withErrorHandler(async function(this: AuthStore, data: UserCreds) {
+			const res = await trySignup(data);
+			if(res.data.status !== STATUS.OK)
+				throw new Error("The returned status was not expected");
+
+			this.tempEmailUser = data.email;
+			return true;
+		}),
+		signin: withErrorHandler(async function(this: AuthStore, data: UserCreds) {
+			const res = await trySignin(data);
+			if(res.data.status !== STATUS.OK)
+				throw new Error("The returned status was not expected");
+
+			this.isAuth = true;
+			await this.getCurrentUser!();
+			return true;
+		}),
+		logout: withErrorHandler(async function(this: AuthStore) {
+			const res = await tryLogout();
+			if(res.data.status !== STATUS.OK)
+				throw new Error("The returned status was not expected");
+
+			this.isAuth = false;
+			return true;
+		}),
+		sendAnotherEmail: withErrorHandler(async function(this: AuthStore, email: string) {
+			const res = await trySendNewActivationEmail(email);
+			if(res.data.status !== STATUS.OK)
+				throw new Error("The returned status was not expected");
+
+			return true;
+		}),
+		verifyCodeToken: withErrorHandler(async function(this: AuthStore, activationToken: string) {
+			const res = await tryActivateAccount(activationToken);
+			if(res.data.status !== STATUS.OK)
+				throw new Error("The returned status was not expected");
+
+			return true;
+		}),
+		forgotPassword: withErrorHandler(async function(this: AuthStore, email: string) {
+			const res = await trySendResetPasswordEmail(email);
+			if(res.data.status !== STATUS.OK)
+				throw new Error("The returned status was not expected");
+
+			return true;
+		}),
+		sendNewPassword: withErrorHandler(async function(this: AuthStore, password: string, token: string) {
+			const res = await tryResetPassword(password, token);
+			if(res.data.status !== STATUS.OK)
+				throw new Error("The returned status was not expected");
+
+			return true;
+		}),
+		getCurrentUser: withErrorHandler(async function(this: AuthStore) {
+			const res = await tryGetMe();
+			if(res.data.status !== STATUS.OK)
+				throw new Error("The returned status was not expected");
+
+			this.user = res.data.user;
+			return true;
+		}),
+		reset(this: AuthStore, keys?: Array<KeysRequired<AuthStore>>) {
+			Object.assign(this, keys?.length
+				? pick(authStoreDefaultState(), keys)
+				: authStoreDefaultState() // if no keys provided, reset all
+			);
 		},
-		async trySignin(payload: AuthStoreSignin) {
-			try {
-				const res = await apiTrySignin(payload).then((res) => res.data);
-				if (res.status === 'ok') {
-					this.isAuth = true;
-					return true;
-				}
-				return false;
-			} catch {
-				Swal.fire({
-					icon: 'error',
-					title: 'An error occured when trying to log you in',
-				});
-				return false;
-			}
-		},
-		async trySendNewActivationEmail(email: string) {
-			try {
-				await apiTrySendNewActivationEmail(email);
-			} catch {
-				Swal.fire({
-					icon: 'error',
-					title: 'We were not able to send you a new activation email',
-				});
-			}
-		},
-		async tryAccountActivate(token: string) {
-			try {
-				const res = await apiTryAccountActivate(token).then((res) => res.data);
-				if (res.status === 'ok') return true;
-				return false;
-			} catch {
-				return false;
-			}
-		},
-		async trySendResetPasswordEmail(email: string) {
-			try {
-				await apiTrySendResetPasswordEmail(email);
-			} catch {
-				Swal.fire({
-					icon: 'error',
-					title: 'We were not able to send you a new reset email',
-				});
-			}
-		},
-		async tryResetPassword(password: string, token: string) {
-			try {
-				const res = await apiTryResetPassword(password, token).then((res) => res.data);
-				if (res.status === 'ok') return true;
-				return false;
-			} catch {
-				Swal.fire({
-					icon: 'error',
-					title: 'An error occured when trying to change your password',
-				});
-				return false;
-			}
-		},
-		async tryCheckResetToken(token: string) {
-			try {
-				const res = await apiTryCheckResetToken(token).then((res) => res.data);
-				if (res.status === 'ok') return true;
-				return false;
-			} catch {
-				return false;
-			}
-		},
-		async tryLogout() {
-			try {
-				const res = await apiTryLogout().then((res) => res.data);
-				if (res.status === 'ok') this.isAuth = false;
-			} catch {
-				return;
-			}
-		},
-	},
-});
+	}
+})
