1addf9cb56f62d08b74f11d1fdb760dff72c355a
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-16 13:41:25 +0100

    feat(utils): Implement the omit function to remove unwanted keys

--- front-end/src/utils/object.helper.ts
+++ front-end/src/utils/object.helper.ts
@@ -8,3 +8,16 @@ export const pick = <T extends object, K extends keyof T>(obj: T, keys: K[]): Pi
 		return acc;
 	}, {} as Pick<T, K>);
 }
+
+/**
+	This function takes an object and an array of keys, and returns a new object that does not
+	contains the properties defined in the array of keys.
+*/
+export const omit = <T extends object, K extends keyof T>(obj: T, keys: K[]): Omit<T, K> => {
+	return Object.keys(obj).reduce((acc, key) => {
+		if(keys.indexOf(key as K) === -1) {
+			acc[key] = obj[key];
+		}
+		return acc;
+	}, {} as Omit<T, K>);
+}
