e49fc90362850204b415bf1a7aa742e2cd9f9cde
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-16 13:45:40 +0100

    feat(jwt): Add a jwt decoder to retrieve the jwt in the params of a controller route

---
+++ back-end/src/common/decorators/jwt.decorator.ts
@@ -0,0 +1,15 @@
+import { Logger as NestLogger } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+import { JwtPayload } from '@/auth/interfaces/jwt.interface';
+
+export const Jwt = createParamDecorator((_: unknown, context: ExecutionContext) => {
+	try {
+		const args = context.getArgs();
+		const user = <JwtPayload> args[0].user;
+		return user.id;
+	} catch (e) {
+		NestLogger.error(`Could not retrieve the user id from the jwt, ${e}`);
+		return null;
+	}
+});
