bdf144e264708658880a4b8a243b2917499cf8d5
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-19 11:56:23 +0100

    fix(format): Make the component readable

--- front-end/src/components/common/buttons/Icon.vue
+++ front-end/src/components/common/buttons/Icon.vue
@@ -1,5 +1,9 @@
 <template>
-	<component :is="to ? RouterLink : 'button'" :to="to" class="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">
+	<component 
+		:is="to ? RouterLink : 'button'"
+		:to="to"
+		class="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700"
+	>
 		<slot></slot>
 	</component>
 </template>
