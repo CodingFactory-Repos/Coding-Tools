85e2d1285c09c027f2394355e42297cea1ab09c2
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-19 19:52:37 +0100

    fix(rgx): Attempt to fix the validation + fix some bugs in the regex

--- front-end/src/utils/constants.ts
+++ front-end/src/utils/constants.ts
@@ -1,2 +1,4 @@
-export const FORMAT_EMAIL = /^_?(?=.{2,42}@)[0-9a-zA-Z]+(?:[\.-_][0-9a-z]+)*@((?=.{3,64}$)[a-z0-9]{1,}(?:-{1,3}[a-z]{1,})?(?:\.[a-z]{0})?)+(?:[a-z]{1,}\.[a-z]{2,})+$/;
+const FORMAT_EMAIL_RECIPIENT = "^_?(?=.{2,42}@)[0-9a-zA-Z]+(?:[_.-][0-9a-z]+)*"
+const FORMAT_EMAIL_DOMAIN = "((?=.{3,64}$)[a-z0-9]+(?:-{1,3}[a-z]+)?(?:\\.[a-z]{0})?)+(?:[a-z]+\\.[a-z]{2,})+$";
+export const FORMAT_EMAIL = new RegExp(`${FORMAT_EMAIL_RECIPIENT}@${FORMAT_EMAIL_DOMAIN}`);
 export const FORMAT_PASSWORD = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!"#$%&'()*+,\-\/:;<=>?@[\\\]^_`{|}~\.])[A-Za-z\d!"#$%&'()*+,\-\/:;<=>?@[\\\]^_`{|}~\.]{8,}$/;
