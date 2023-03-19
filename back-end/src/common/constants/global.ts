9b2b94c66858f4b3487fceb279e2a55edc53ecbd
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-19 20:12:28 +0100

    fix(rgx): Attempt at fixing the password regex

--- back-end/src/common/constants/global.ts
+++ back-end/src/common/constants/global.ts
@@ -1,4 +1,9 @@
-const FORMAT_EMAIL_RECIPIENT = "^_?(?=.{2,42}@)[0-9a-zA-Z]+(?:[_.-][0-9a-z]+)*"
-const FORMAT_EMAIL_DOMAIN = "((?=.{3,64}$)[a-z0-9]+(?:-{1,3}[a-z]+)?(?:\\.[a-z]{0})?)+(?:[a-z]+\\.[a-z]{2,})+$";
-export const FORMAT_EMAIL = new RegExp(`${FORMAT_EMAIL_RECIPIENT}@${FORMAT_EMAIL_DOMAIN}`);
-export const PASSWORD_FORMAT = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!"#$%&'()*+,\-\/:;<=>?@[\\\]^_`{|}~\.])[A-Za-z\d!"#$%&'()*+,\-\/:;<=>?@[\\\]^_`{|}~\.]{8,}$/;
+const FORMAT_EMAIL_RECIPIENT = "^_?(?=.{2,42}@)[0-9a-zA-Z]+(?:[_.-][0-9a-z]+)*" as const;
+const FORMAT_EMAIL_DOMAIN = "((?=.{3,64}$)[a-z0-9]+(?:-{1,3}[a-z]+)?(?:\\.[a-z]{0})?)+(?:[a-z]+\\.[a-z]{2,})+$" as const;
+export const EMAIL_FORMAT = new RegExp(`${FORMAT_EMAIL_RECIPIENT}@${FORMAT_EMAIL_DOMAIN}`);
+
+const AT_LEAST_ONE_LETTER = "(?=.*[A-Za-z])" as const;
+const AT_LEAST_ONE_DIGIT = "(?=.*\\d)" as const;
+const AT_LEAST_ONE_SPECIAL_CHAR = "(?=.*[!\"#$%&'()*+,\\-\\/:;<=>?@[\\\\\\]^_\`{|}~\\.])" as const;
+const ALLOWED_CHAR = "[A-Za-z\\d!\"#$%&'()*+,\\-\\/:;<=>?@[\\\\\\]^_\`{|}~\\.]{8,}" as const;
+export const PASSWORD_FORMAT = new RegExp(`^${AT_LEAST_ONE_LETTER}${AT_LEAST_ONE_DIGIT}${AT_LEAST_ONE_SPECIAL_CHAR}${ALLOWED_CHAR}$`);
--- front-end/src/utils/constants.ts
+++ front-end/src/utils/constants.ts
@@ -1,4 +1,9 @@
 const FORMAT_EMAIL_RECIPIENT = "^_?(?=.{2,42}@)[0-9a-zA-Z]+(?:[_.-][0-9a-z]+)*"
 const FORMAT_EMAIL_DOMAIN = "((?=.{3,64}$)[a-z0-9]+(?:-{1,3}[a-z]+)?(?:\\.[a-z]{0})?)+(?:[a-z]+\\.[a-z]{2,})+$";
 export const FORMAT_EMAIL = new RegExp(`${FORMAT_EMAIL_RECIPIENT}@${FORMAT_EMAIL_DOMAIN}`);
-export const FORMAT_PASSWORD = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!"#$%&'()*+,\-\/:;<=>?@[\\\]^_`{|}~\.])[A-Za-z\d!"#$%&'()*+,\-\/:;<=>?@[\\\]^_`{|}~\.]{8,}$/;
+
+const AT_LEAST_ONE_LETTER = "(?=.*[A-Za-z])";
+const AT_LEAST_ONE_DIGIT = "(?=.*\\d)";
+const AT_LEAST_ONE_SPECIAL_CHAR = "(?=.*[!\"#$%&'()*+,\\-\\/:;<=>?@[\\\\\\]^_\`{|}~\\.])"
+const ALLOWED_CHAR = "[A-Za-z\\d!\"#$%&'()*+,\\-\\/:;<=>?@[\\\\\\]^_\`{|}~\\.]{8,}"
+export const FORMAT_PASSWORD = new RegExp(`^${AT_LEAST_ONE_LETTER}${AT_LEAST_ONE_DIGIT}${AT_LEAST_ONE_SPECIAL_CHAR}${ALLOWED_CHAR}$`);
