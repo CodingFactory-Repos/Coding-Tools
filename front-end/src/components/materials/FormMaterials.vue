e21d55ef18a87d4fb367fd45ac33e299ad111032
Lucas Goncalves <lucasgoncalves@free.fr>
2023-03-21 22:24:13 +0100

    Adding the reservation modal

---
+++ front-end/src/components/materials/FormMaterials.vue
@@ -0,0 +1,141 @@
+<template>
+	<form @submit.prevent="borrorwingMaterial(id)">
+		<div class="form-group relative z-0 w-full">
+			<label
+				for="borrowingDate"
+				class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+				>Borrowing Date
+			</label>
+			<div class="absolute top-9 left-0 flex items-center pl-3 pointer-events-none">
+				<svg
+					aria-hidden="true"
+					class="w-5 h-5 text-gray-500 dark:text-gray-400"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</div>
+			<input
+				type="date"
+				id="borrowingDate"
+				v-model="borrowingDate"
+				name="borrowingDate"
+				:min="today"
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				placeholder="Select date"
+			/>
+		</div>
+		<div class="mb-5"></div>
+		<div>
+			<textarea
+				id="description"
+				v-model="description"
+				class="max-h-40 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				placeholder="Enter a description"
+			></textarea>
+		</div>
+		<div class="mb-5"></div>
+		<div>
+			<label for="returnDate" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+				>Return Date
+			</label>
+			<input
+				type="date"
+				id="returnDate"
+				v-model="returnDate"
+				name="returnDate"
+				:min="today"
+				class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				placeholder="Select date"
+			/>
+			<div class="mb-5"></div>
+		</div>
+		<button
+			type="submit"
+			class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+		>
+			Submit
+		</button>
+	</form>
+</template>
+
+<script lang="ts" setup>
+//Import datepicker from flowbite from node_modules
+import { defineProps } from 'vue';
+import axios from 'axios';
+import { http } from '@/api/network/axios';
+import Vue from 'vue';
+
+defineProps({
+	id: { type: String, required: true },
+});
+let today = new Date().toISOString().substr(0, 10);
+let borrowingDate = '';
+let borrowingUser = '';
+let description = '';
+let returnDate = '';
+
+function borrorwingMaterial(identifiant) {
+	console.log('Borrowing material');
+	let borrowingUsers = '';
+
+	http.get('/materials/user').then((response) => {
+		console.log(response.data);
+		this.borrowingUsers = response.data;
+		console.log(this.borrowingUsers);
+		axios
+			.put('http://localhost:8000/materials/reservation/' + identifiant, {
+				borrowingHistory: {
+					borrowingDate: this.borrowingDate,
+					borrowingUser: this.borrowingUsers,
+					description: this.description,
+					returnDate: this.returnDate,
+				},
+			})
+			.then((response) => {
+				console.log(response);
+			});
+	});
+}
+// export default {
+// 	name: 'FormMaterials',
+// 	data() {
+// 		return {
+// 			borrowingDate: '',
+// 			borrowingUser: '',
+// 			description: '',
+// 			returnDate: '',
+// 			today: new Date().toISOString().substr(0, 10),
+// 		};
+// 	},
+// 	methods: {
+// 		borrowMaterial(identifiant) {
+// 			console.log('Borrowing material');
+// 			console.log(identifiant);
+// 			// axios.put('http://localhost:8000/materials/update/' + this.id, {
+// 			// 	borrowingDate: this.borrowingDate,
+// 			// 	borrowingUser: this.borrowingUser,
+// 			// 	description: this.description,
+// 			// 	returnDate: this.returnDate,
+// 			// });
+// 		},
+// 		// getActualDate() {
+// 		// 	var today: Date | string = new Date();
+// 		// 	var dd = String(today.getDate()).padStart(2, '0');
+// 		// 	var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+// 		// 	var yyyy = today.getFullYear();
+
+// 		// 	today = yyyy + '-' + mm + '-' + dd;
+// 		// 	this.today = today;
+// 		// },
+// 	},
+// };
+</script>
+
+<style scoped></style>
--- front-end/src/components/materials/MaterialCard.vue
+++ front-end/src/components/materials/MaterialCard.vue
@@ -1,11 +1,22 @@
 <template>
+	<div v-if="showModal" class="popup">
+		<div class="popup-content">
+			<FormMaterial :id="id" />
+			<button
+				@click="showModal = false"
+				class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+			>
+				Fermer
+			</button>
+		</div>
+	</div>
 	<div
-		class="w-100 h-100 flex flex-col gap-3 rounded-lg bg-light-primary dark:bg-dark-tertiary py-2 px-4 justify-start items-start"
+		class="w-auto h-100 flex flex-col gap-3 rounded-lg bg-light-primary dark:bg-dark-tertiary py-2 px-4 justify-start items-start"
 	>
 		<div class="w-32 h-full flex flex-col justify-between gap-2 relative" style="min-width: 8rem">
 			<img
 				v-if="url"
-				class="w-full h-24 rounded-lg cursor-pointer select"
+				class="w-100 h-100 rounded-lg cursor-pointer select h-100"
 				:src="url"
 				:alt="`template_${name}_img`"
 			/>
@@ -15,24 +26,76 @@
 				src="https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive-960x540.jpg"
 			/>
 
-			<span class="dark:text-dark-font text-s font-bold">{{ name }}</span>
+			<span class="dark:text-dark-font text-s font-bold text-center">{{ name }}</span>
 			<span
-				class="bg-green-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300"
-				>Available</span
+				v-if="status === true"
+				class="bg-green-100 text-green-800 text-sm font-medium px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300"
+				>Available
+			</span>
+			<span
+				v-else
+				class="bg-red-100 text-red-800 text-sm font-medium px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300"
 			>
+				Not Available
+			</span>
+			<button
+				v-if="status === false"
+				disabled
+				data
+				class="font-bold rounded-lg text-sm px-4 py-2 focus:outline-none flex justify-center items-center gap-2 gradiant"
+			>
+				<span class="text-white">Reserved</span>
+			</button>
+			<button
+				v-else
+				@click="sendIdAndShowMOdal(id)"
+				class="font-bold rounded-lg text-sm px-4 py-2 focus:outline-none flex justify-center items-center gap-2 gradiant"
+			>
+				<span class="text-white">Reservation</span>
+			</button>
 		</div>
 	</div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+import FormMaterial from '@/components/materials/FormMaterials.vue';
+
 defineProps({
+	id: { type: String, required: true },
 	name: { type: String, required: true },
 	url: { type: String, required: true },
+	status: { type: Boolean, required: true },
 });
+let showModal = ref(false);
+
+function sendIdAndShowMOdal(identifiant){
+	console.log(identifiant);
+	this.id = identifiant;
+	this.showModal = true;
+}
 </script>
 
 <style scoped>
 .select:hover {
 	box-shadow: 0 0 0 1px #3b82f6;
+}
+.popup {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	z-index: 9999;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.popup-content {
+	background-color: white;
+	padding: 20px;
+	border-radius: 10px;
+	width: 35%;
 }
 </style>
