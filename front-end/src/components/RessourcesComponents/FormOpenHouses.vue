f870f72c8ff4341c25fdef104011e02af3846860
ThomasCast3 <91255859+ThomasCast3@users.noreply.github.com>
2023-03-21 09:28:36 +0100

    add form to add open houses

---
+++ front-end/src/components/RessourcesComponents/FormOpenHouses.vue
@@ -0,0 +1,43 @@
+<template>
+	<form action="">
+		<input type="text" v-model="title" placeholder="Title :"><br>
+		<input type="date" v-model="date"><br>
+		<input type="text" v-model="adress" placeholder="adress :"><br>
+		<textarea placeholder="Description :"></textarea><br>
+		<input id="buttonUser" type="text" placeholder="participants :" v-model="participant">
+		<button type="button"  @click="addParticipants">Ok</button> <br>
+		<p v-for="user in participants">{{user}}</p><br>
+		<input type="file" placeholder="Documents :"><br>
+		<button type="submit">Submit</button>
+
+	</form>
+</template>
+
+<script lang="ts">
+import axios from 'axios';
+export default {
+	data() {
+		return {
+			title: '',
+			date: '',
+			schedule: [],
+			adress: '',
+			project: [],
+			participants: [],
+			participant: '',
+			description: '',
+			count: 0,
+		};
+	},
+	methods :  {
+		addParticipants(){
+			if(this.participant != null){
+				this.participants.push(this.participant);
+				this.participant = null;
+				
+			}
+		}
+	},
+}
+
+</script>
--- front-end/src/components/RessourcesComponents/OpenHouses.vue
+++ front-end/src/components/RessourcesComponents/OpenHouses.vue
@@ -1,7 +1,24 @@
 <template>
-
 	<div>
         <h2>Open Houses</h2>
+        <button @click="openForm=!openForm" >Add open house</button>
+        <FormOpenHouse v-show="openForm"/>
 	</div>
 </template>
+
+<script>
+import FormOpenHouse from './FormOpenHouses.vue';
+export default {
+    data(){
+        return{
+            openForm : false,
+        }   
+    },
+    components: {
+		FormOpenHouse,
+	},
+}
+</script>
+
+
 
