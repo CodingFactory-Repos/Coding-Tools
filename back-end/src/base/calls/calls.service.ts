66aed559437cfe8ac48e2294833174d09a7875d6
AyakorK <guillaume.moret@yahoo.com>
2023-03-21 15:45:07 +0100

    All functions related to the generation of the temporary token and of the QrCode in the back end

--- back-end/src/base/calls/calls.service.ts
+++ back-end/src/base/calls/calls.service.ts
@@ -1,8 +1,12 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { randomBytes } from 'crypto';
+import { Req } from '@nestjs/common';
 
 import { CallsRepository } from 'src/base/calls/calls.repository';
 import { UsersRepository } from 'src/base/users/users.repository';
+import {ObjectId} from "mongodb";
+import {JwtService} from "@nestjs/jwt";
+import {JwtQRCode} from "@/base/calls/interfaces/calls.interface";
 
 @Injectable()
 export class CallsService {
@@ -11,16 +15,30 @@ export class CallsService {
 		@Inject(forwardRef(() => CallsRepository))
 		private usersRepository: UsersRepository,
 		private callsRepository: CallsRepository,
+		private jwtTokenService: JwtService,
 	) {}
-	public generateQrCode() {
-		const qr = randomBytes(16)
-			.toString('base64')
-			.replace(/[^a-zA-Z0-9]/g, '');
-		return qr;
+
+	async generateTempToken(payload: Record<string, any>) {
+		const jwt = await this.jwtTokenService.signAsync(payload, { expiresIn: '3min' });
+		console.log(this.jwtTokenService.decode(jwt));
+		return jwt;
 	}
-	public generateQrLink() {
-		const qr = this.generateQrCode();
-		const link = `http://localhost:3000/calls/presence?qr=${qr}`;
-		return link;
+
+	async updateUserPresence(jwt: string, presence: boolean) {
+		console.log(jwt);
+		const jwt_decode = this.jwtTokenService.decode(jwt);
+		const userId = await jwt_decode['id'];
+		const courseId = await jwt_decode['courseId'];
+		await this.callsRepository.updateUserPresence(userId, courseId, presence);
+	}
+
+	async generator(userId: ObjectId, courseId: ObjectId) {
+		const jwt = await this.generateTempToken({ id: userId, courseId: new ObjectId() });
+		const url = await this.generateUrl(jwt);
+		return url;
+	}
+
+	async generateUrl(jwt: string) {
+		return `https://72ef-92-174-83-81.eu.ngrok.io/calls/presence/` + jwt;
 	}
 }
