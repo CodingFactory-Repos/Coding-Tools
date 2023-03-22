2f930225acb3410c07552e5848aacc1df252ac94
Lucas Goncalves <lucasgoncalves@free.fr>
2023-03-22 19:50:29 +0100

    Adding the route to get the role of the user

--- back-end/src/base/materials/materials.controller.ts
+++ back-end/src/base/materials/materials.controller.ts
@@ -2,6 +2,7 @@ import {
 	Controller,
 	Delete,
 	Get,
+	Param,
 	Post,
 	Put,
 	Req,
@@ -36,10 +37,13 @@ export class MaterialsController {
 	}
 	@Put('/update/:id')
 	updateMaterial(@Req() req, @Res() res: Response) {
-		const query = { _id: new ObjectId(req.params.id) };
+		console.log(req.params.id);
+		const query = { _id: new ObjectId(req.params.id)};
 		const update = { $set: req.body };
-		this.materialsService.updateMaterial(query, update).then((material) => {
-			res.status(200).json(material);
+		// console.log(req.body);
+		this.materialsService.updateMaterial(query, update).then((response) => {
+			console.log(response);
+			res.status(200).json({ message: 'Material updated' });
 		});
 	}
 	@Put('reservation/:id')
@@ -69,9 +73,20 @@ export class MaterialsController {
 		res.status(200).json(user);
 	}
 
-	@Get('/:id')
-	async getMaterialById(@Param('id') id: string, @Res() res: Response) {
-		const material = await this.materialsService.getMaterialById(id);
-		res.status(200).json(material);
+	@Get('test/:id')
+	getMaterialById(@Req() req, @Res() res: Response) {
+		console.log('ok');
+		const id = req.params.id;
+		const material = this.materialsService.getMaterialById(id).then((response => {
+			res.status(200).json(response);
+		}));
+	}
+
+	@Get('user/role/:id')
+	getUserRole(@Req() req, @Res() res: Response) {
+		const id = req.params.id;
+		const role = this.materialsService.getUserRole(id).then((response) => {
+			res.status(200).json(response.role);
+		});
 	}
 }
