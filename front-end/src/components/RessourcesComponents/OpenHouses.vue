4a895895171e1dc300d8daabc9dfd9653f24e44b
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2023-03-22 14:15:56 +0100

    Afficher JPO + bug résolu

--- front-end/src/components/RessourcesComponents/OpenHouses.vue
+++ front-end/src/components/RessourcesComponents/OpenHouses.vue
@@ -1,6 +1,30 @@
 <template>
-	<div>
-		<h2>Open Houses</h2>
+	<div class="py-8">
+		<h2 class="text-2xl font-bold mb-4">Open Houses</h2>
+		<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+			<div
+				v-for="openHouse in openHouses"
+				:key="openHouse.id"
+				class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+			>
+				<img :src="openHouse.picture" alt="" class="rounded-t-lg" />
+
+				<div class="p-4">
+					<h3 class="text-lg font-bold mb-2">{{ openHouse.title }}</h3>
+					<p class="mb-2">
+						<span class="font-bold">Date :</span> {{ formattedDate(openHouse.date) }}
+					</p>
+					<p class="mb-2">
+						<span class="font-bold">Address :</span> {{ formattedAddress(openHouse.address) }}
+					</p>
+				</div>
+				<div style="display: flex; justify-content: center; align-items: center">
+					<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-2 rounded">
+						See more
+					</button>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -10,16 +34,34 @@ import axios from 'axios';
 export default {
 	data() {
 		return {
-			/* list of all open houses */
 			openHouses: [],
 		};
 	},
-	async mounted() {
-		await axios.get('http://localhost:8000/openhouses').then((response) => {
-			console.log(response);
-			this.openHouses = response.data;
-			console.log(this.openHouses);
-		});
+	mounted() {
+		axios
+			.get('http://localhost:8010/openhouses')
+			.then((response) => {
+				this.openHouses = response.data;
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	},
+	methods: {
+		// Change computed to methods
+		formattedDate(date) {
+			// Remove the check for openHouses length and accept date as a parameter
+			const parsedDate = new Date(date);
+			return parsedDate.toLocaleDateString('fr-FR', {
+				day: '2-digit',
+				month: '2-digit',
+				year: 'numeric',
+			});
+		},
+		formattedAddress(address) {
+			// Remove the check for openHouses length and accept address as a parameter
+			return `${address.street} ${address.city} ${address.zipCode}`;
+		},
 	},
 };
 </script>
--- front-end/src/views/Ressources.vue
+++ front-end/src/views/Ressources.vue
@@ -1,21 +1,20 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
 	<!--<Ressources></Ressources>-->
-    <div>
-        <h1>Ressources</h1>
-        <div>
-            <button v-on:click=SetPage(0)>Cours</button>
-            <p></p>
-            <button v-on:click="SetPage(1)">JPO</button>
-        </div>
-        <div>
-            <h2 >{{state}}</h2>
-	        <CoursPage v-show="state == 0" />
-		    <OpenHousesPage  v-show="state == 1" />
-        </div>
+	<div>
+		<h1>Ressources</h1>
+		<div>
+			<button v-on:click="SetPage(0)">Cours</button>
+			<p></p>
+			<button v-on:click="SetPage(1)">JPO</button>
+		</div>
+		<div>
+			<h2>{{ state }}</h2>
+			<CoursPage v-show="state == 0" />
+			<OpenHousesPage v-show="state == 1" />
+		</div>
 	</div>
 </template>
-
-
 
 <script lang="ts">
 import { useRetrospectiveStore } from '@/store/retrospective.store';
@@ -26,20 +25,17 @@ import OpenHousesPage from '@/components/RessourcesComponents/OpenHouses.vue';
 export default defineComponent({
 	components: {
 		OpenHousesPage,
-        CoursPage,
+		CoursPage,
 	},
-	data(){
-        return {
-            state : 0,
-        }
-  
-    },
-    computed : {
-        SetPage(index){
-           return this.state = index;
-        },
-    },
-
+	data() {
+		return {
+			state: 0,
+		};
+	},
+	methods: {
+		SetPage(index) {
+			this.state = index;
+		},
+	},
 });
 </script>
-
