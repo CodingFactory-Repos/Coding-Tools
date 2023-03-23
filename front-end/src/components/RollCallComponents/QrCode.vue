ae14c693977dd76ce2828d15c07ddfa5534d2daf
AyakorK <guillaume.moret@yahoo.com>
2023-03-23 16:54:25 +0100

    Send to the back the course_id

--- front-end/src/components/RollCallComponents/QrCode.vue
+++ front-end/src/components/RollCallComponents/QrCode.vue
@@ -1,6 +1,9 @@
 <template>
 	<div>
 		<qrcode v-if="url" :value="url" />
+		<div v-else>
+			<p>{{ message }}</p>
+		</div>
 	</div>
 </template>
 
@@ -9,6 +12,8 @@ import Qrcode from 'vue-qrcode';
 import { http } from '@/api/network/axios';
 
 let url = '';
+let courseId = '';
+let message = '';
 
 export default {
 	name: 'QrCode',
@@ -18,20 +23,37 @@ export default {
 	data() {
 		return {
 			url,
+			courseId,
+			message,
 		};
 	},
 	mounted() {
 		setInterval(() => {
-			this.getQrCode();
+			this.getCourseId();
 		}, 180000);
 
-		this.getQrCode();
+		this.getCourseId();
 	},
 	methods: {
 		getQrCode() {
-			http.get('/calls/qrcode_generator').then((response) => {
+			const courseId = this.courseId;
+			http.get(`/calls/qrcode_generator/${courseId}`).then((response) => {
 				this.url = response.data.qrcode;
 			});
+		},
+		getCourseId() {
+			http.get(`/calls/actual_course/`).then((response) => {
+				this.courseId = response.data.actualCourse;
+				this.isThereCourse();
+			});
+		},
+		isThereCourse() {
+			if (this.courseId) {
+				this.getQrCode();
+				this.message = '';
+			} else {
+				this.message = "Vous n'avez pas de cours aujourd'hui";
+			}
 		},
 	},
 };
