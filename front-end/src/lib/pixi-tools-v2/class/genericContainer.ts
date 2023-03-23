107379c46d8719fbea95d9c7e99d7062f86262b2
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-23 21:20:31 +0100

    feat(generic-container): Define a simple constructor for the generic container

---
+++ front-end/src/lib/pixi-tools-v2/class/genericContainer.ts
@@ -0,0 +1,8 @@
+import { Container } from "pixi.js";
+import { ContainerContext } from "../types/pixi-container-options";
+
+export class GenericContainer extends Container {
+	constructor(context: ContainerContext) {
+		super();
+	}
+}
