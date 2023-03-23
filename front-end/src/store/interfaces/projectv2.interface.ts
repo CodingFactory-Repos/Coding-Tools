0ff023379a327278c663e0f75d3555d20e5b6265
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-23 21:18:21 +0100

    feat(project-store): Implement the setDeferredEvent function aswell as the createGeometry functino

--- front-end/src/store/interfaces/projectv2.interface.ts
+++ front-end/src/store/interfaces/projectv2.interface.ts
@@ -1,6 +1,12 @@
+import { Normalizer } from "@/lib/pixi-tools-v2/class/normalyzer";
 import { Scene } from "@/lib/pixi-tools-v2/scene";
+import { LiteralGeometryTypes } from "@/lib/pixi-tools-v2/types/pixi-enums";
 
 export interface ProjectStorev2 {
 	scene: Scene;
 	canvas: HTMLCanvasElement;
+	deferredGeometry: LiteralGeometryTypes;
+
+	setDeferredEvent?: (cursor: CSStyleProperty.Cursor) => void;
+	createGeometry?: () => void;
 }
--- front-end/src/store/modules/project2.store.ts
+++ front-end/src/store/modules/project2.store.ts
@@ -1,14 +1,46 @@
 import { defineStore } from 'pinia';
+import { toRaw } from 'vue';
 
 import { ProjectStorev2 } from '@/store/interfaces/projectv2.interface';
+import { Normalizer } from '@/lib/pixi-tools-v2/class/normalyzer';
+import { GenericContainer } from '@/lib/pixi-tools-v2/class/genericContainer';
 
 export const useProjectStorev2 = defineStore('projectv2', {
 	state: (): ProjectStorev2 => {
 		return {
-			scene: undefined,
-			canvas: undefined,
+			scene: null,
+			canvas: null,
+			deferredGeometry: null,
 		};
 	},
 	actions: {
+		setDeferredEvent(
+			this: ProjectStorev2,
+			cursor: CSStyleProperty.Cursor
+		) {
+			this.canvas.classList.toggle(cursor);
+			this.canvas.addEventListener('pointerup', this.createGeometry);
+		},
+		createGeometry(
+			this: ProjectStorev2,
+			event: PointerEvent
+		) {
+			const scene = toRaw(this.scene);
+			const normalizer = new Normalizer(scene.stage, scene.viewport);
+			
+			const context = normalizer.normalizeOneGraphic({
+				geometry: this.deferredGeometry,
+				clientX: event.clientX,
+				clientY: event.clientY,
+				color: 0xffffff,
+			});
+
+			const genericContainer = new GenericContainer(context);
+			scene.viewport.addChild(genericContainer);
+			this.scene = scene;
+
+			this.canvas.classList.toggle("default");
+			this.canvas.removeEventListener('pointerup', this.createGeometry);
+		}
 	},
 });
