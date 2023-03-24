1295c68cce5a5b7c9cb533217f4cad9cd0353355
Massimiliano HUBERT-ABBATE <massimilianoha@MacBook-Pro-de-Massimiliano.local>
2023-03-24 10:31:34 +0100

    Adding onchange on the input

--- front-end/src/components/materials/ListMaterials.vue
+++ front-end/src/components/materials/ListMaterials.vue
@@ -32,6 +32,7 @@
 					class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					placeholder="Search Mockups, Logos..."
 					required
+					@change="filteredList()"
 				/>
 				<button
 					type="submit"
@@ -68,6 +69,7 @@ import { withErrorHandler } from '@/utils/storeHandler';
 const materialStore = useMaterialStore();
 const materials = computed(() => materialStore.filteredMaterial);
 const input = ref('');
+
 const filteredList = () => {
 	materialStore.input = input.value;
 };
