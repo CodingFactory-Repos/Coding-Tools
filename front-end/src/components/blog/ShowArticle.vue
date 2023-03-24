91a57f1c35dc3c9d2f5ac130acd2942b0b12b0bd
Théo Landemaine <91159174+TheoLandemaine@users.noreply.github.com>
2023-03-24 15:58:32 +0100

    add caption to the table

--- front-end/src/components/blog/ShowArticle.vue
+++ front-end/src/components/blog/ShowArticle.vue
@@ -13,6 +13,9 @@
 			<template #body>
 				<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
 					<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+						<caption class="hidden">
+							List of participants
+						</caption>
 						<thead
 							class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
 						>
