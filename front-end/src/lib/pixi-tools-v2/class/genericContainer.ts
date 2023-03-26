d69fbbbaa47c507c5677313287691f0d240d137e
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-26 23:43:17 +0200

    fix(generic): Clear out the logic of the children and pass throught the generic container for the selection

--- front-end/src/lib/pixi-tools-v2/class/genericContainer.ts
+++ front-end/src/lib/pixi-tools-v2/class/genericContainer.ts
@@ -1,51 +1,54 @@
-import { Container } from "pixi.js";
+import { ContainerManager } from './containerManager';
+import { Container, FederatedPointerEvent } from "pixi.js";
 import { Border } from "../model/model-constructor/border";
-import { ContainerContext, GraphicAttributes } from "../types/pixi-container-options";
+import { ContainerContext } from "../types/pixi-container-options";
 import { Viewport } from 'pixi-viewport';
 
 export class GenericContainer extends Container {
 	public id: string;
-	private _viewport: Viewport;
 	private _border: Border;
-	private _containerX = Infinity;
-	private _containerY = Infinity;
+	private _viewport: Viewport;
+	private _manager: ContainerManager;
 
 	constructor(context: ContainerContext) {
 		super();
 
-		this.id = "frame";
 		this.cursor = "pointer";
 		this.interactive = true;
 		this._viewport = context.viewport;
+		this._manager = context.manager;
 		
 		for(let i = 0; i < context.constructors.length; i++) {
 			const { Graphic, attributes } = context.constructors[i];
 			const element = new Graphic(attributes);
-			element.on("pointerdown", (e) => element.onSelect(e, this));
+			element.on("pointerdown", this._onChildSelected);
 			this.addChild(element);
-
-			if (attributes.x < this._containerX) this._containerX = attributes.x;
-			if (attributes.y < this._containerY) this._containerY = attributes.y;
 		}
 	}
 
-	private _destroyBorder() {
+	private _onChildSelected(e: FederatedPointerEvent) {
+		// e.shiftKey even if known in the object return undefined;
+		const isShift = e.originalEvent.shiftKey;
+		this._manager.selectContainer(this, isShift);
+	}
+
+	public destroyBorder() {
 		if(this._border) {
 			this._border.destroy();
 			this._border = null;
 		}
 	}
 
-	public drawChildBorder(attr: GraphicAttributes) {
-		this._destroyBorder();
+	public drawBorder() {
+		const graphic = this.children.find(el => el.id === "graphic");
 
 		this._border = new Border({
-			x: attr.x,
-			y: attr.y,
-			width: attr.width,
-			height: attr.height,
-			scale: this._viewport.scale.x,
+			x: graphic.x,
+			y: graphic.y,
+			width: graphic.width,
+			height: graphic.height,
+			scale: this._viewport.scale.x
 		})
-		this.addChild(this._border);
+		this.addChild(this._border)
 	}
 }
