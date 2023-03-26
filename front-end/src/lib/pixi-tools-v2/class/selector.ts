443f2d8b1672b12335ac87ab272cfa77b377972c
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-26 23:41:04 +0200

    fix(selector): Remove the selector abstract class

--- front-end/src/lib/pixi-tools-v2/class/selector.ts
+++
@@ -1,6 +0,0 @@
-import { FederatedPointerEvent } from "pixi.js";
-import { GenericContainer } from "./genericContainer";
-
-export declare class GraphicSelector {
-	public onSelect(e: FederatedPointerEvent, parent: GenericContainer): void;
-}
