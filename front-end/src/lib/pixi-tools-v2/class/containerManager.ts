693b2f2ef611d42f57f6556c923e8c8c2c1c6cd0
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-26 23:43:43 +0200

    feat(manager): Define and handle the wrapped container and the selection of the element on the canvas

--- front-end/src/lib/pixi-tools-v2/class/containerManager.ts
+++ front-end/src/lib/pixi-tools-v2/class/containerManager.ts
@@ -1,10 +1,11 @@
-import { Container, DisplayObject } from 'pixi.js';
+import { Container } from 'pixi.js';
 import { Scene } from "@/lib/pixi-tools-v2/scene";
+import { WrappedContainer } from './wrappedContainer';
 
 export class ContainerManager {
 	private _scene: Scene;
 	private _selectedContainers: Array<Container>;
-	private _wrapperContainer: Container;
+	private _wrappedContainer: WrappedContainer;
 	private _dragPlugin: any;
 	private _selectPlugin: any;
 	private _resizePlugin: any;
@@ -13,54 +14,114 @@ export class ContainerManager {
 	constructor(scene: Scene) {
 		this._scene = scene;
 		this._selectedContainers = [];
-		this._wrapperContainer = new Container();
+		this._wrappedContainer = new WrappedContainer(scene.viewport);
 		this._dragPlugin = null;
 		this._selectPlugin = null;
 		this._resizePlugin = null;
 		this._downloadPlugin = null;
 	}
 
-	public selectContainer(container: Container) {
+	/**
+	 * Selects the given container and updates the selected containers list
+	 * @param container
+	 * @param isShift 
+	 * @returns void
+	 */
+	public selectContainer(container: Container, isShift: boolean) {
 		if (!this._selectedContainers.includes(container)) {
-			this._selectedContainers.push(container);
-		}
+			const len = this._selectedContainers.push(container);
+			const index = len - 1;
 
-		if (this._selectedContainers.length > 1) {
-			this.createTemporaryParent();
-		} else {
-			this.attachPlugins(container);
-			container.activePlugin = true;
+			// If the shift key is not pressed and there is more than one children of the wrappedContainer,
+			// add all of its children to the viewport + remove them and destroy its border.
+			if (!isShift && this._wrappedContainer.children.length > 0) {
+				this._wrappedContainer.destroyBorder();
+				this._scene.viewport.addChild(...this._wrappedContainer.children);
+				this._wrappedContainer.removeChildren();
+			}
+			
+			// If the shift key is pressed and there is more than one container selected,
+			// wrap the selected containers in a temporary parent container and destroy any existing borders.
+			 if(isShift && len > 1) {
+				this.destroyBorder(this._selectedContainers);
+				this.wrapWithTemporaryParent();
+				return;
+			
+			// If there is more than one container selected,
+			// deselect all other containers except for the current container and destroy any existing borders.
+			} else if (len > 1) {
+				const unselected = this.deselectAllExceptThisContainer(index);
+				this.destroyBorder(unselected);
+				this.drawBorder();
+				return;
+			}
+
+			// Draw the border of the currently selected element.
+			this.drawBorder();
+
+		// If there is more than one children of the wrappedContainer,
+		//  add all of its children to the viewport + remove them and destroy its border, then draw the border of the clicked element.
+		} else if(this._wrappedContainer.children.length > 0) {
+			const index = this._selectedContainers.findIndex(el => el === container);
+			if(index === -1) return;
+
+			this._wrappedContainer.destroyBorder();
+			this._scene.viewport.addChild(...this._wrappedContainer.children);
+			this._wrappedContainer.removeChildren();
+			this.deselectAllExceptThisContainer(index);
+			this.drawBorder();
 		}
 	}
 
-	private hasChild(container: Container) {
-		return this._scene.viewport.children.indexOf(container) !== -1;
-	}
+	public deselectAllExceptThisContainer(index: number) {		
+		const selected: Array<Container> = [];
+		const unselected: Array<Container> = [];
 
-	public createTemporaryParent() {
 		for(let n = 0; n < this._selectedContainers.length; n++) {
-			if(this._selectedContainers[n].activePlugin) {
-				this.detachPlugins(this._selectedContainers[n]);
-			}
-
-			if(this.hasChild(this._selectedContainers[n])) {
-				this._scene.viewport.removeChild(this._selectedContainers[n]);
-				this._wrapperContainer.addChild(this._selectedContainers[n]);
+			if(index === n) {
+				selected.push(this._selectedContainers[n]);
+			} else {
+				unselected.push(this._selectedContainers[n]);
 			}
 		}
 
-		if(!this.hasChild(this._wrapperContainer)) {
-			this._scene.viewport.addChild(this._wrapperContainer);
+		this._selectedContainers = selected;
+		return unselected;
+	}
+
+	public destroyBorder(container: Array<Container>) {
+		for(let n = 0; n < container.length; n++) {
+			container[n].destroyBorder();
 		}
 	}
 
-	public deselectContainer(container: Container) {
-		const index = this._selectedContainers.indexOf(container);
-		if (index !== -1) {
-			this._selectedContainers.splice(index, 1);
-			this.detachPlugins(container);
+	public drawBorder() {
+		for(let n = 0; n < this._selectedContainers.length; n++) {
+			this._selectedContainers[n].drawBorder();
 		}
 	}
+
+	public wrapWithTemporaryParent() {
+		this._wrappedContainer.addChild(...this._selectedContainers);
+		this._wrappedContainer.drawBorder();
+		this._scene.viewport.addChild(this._wrappedContainer);
+	}
+
+
+
+
+
+
+
+	// Everything below is work in progress
+
+
+
+
+
+
+
+
 
 	public attachPlugins(container: Container) {
 		this._dragPlugin.attach(container);
@@ -85,11 +146,11 @@ export class ContainerManager {
 		if (clickedContainer) {
 			if (event.shiftKey && this._selectedContainers.length > 0) {
 				// shift-click to add to selection
-				this.selectContainer(clickedContainer);
+				// this.selectContainer(clickedContainer);
 			} else {
 				// regular click to select only the clicked container
 				// this.deselectAllContainers();
-				this.selectContainer(clickedContainer);
+				// this.selectContainer(clickedContainer);
 			}
 		} else {
 			// click outside any container to deselect all
