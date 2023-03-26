240f90ef5f05d0fd5065d09e36ab9b2600be0e30
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-26 06:42:39 +0200

    feat(selector): Declare an select class to be implemented

---
+++ front-end/src/lib/pixi-tools-v2/class/selector.ts
@@ -0,0 +1,6 @@
+import { FederatedPointerEvent } from "pixi.js";
+import { GenericContainer } from "./genericContainer";
+
+export declare class GraphicSelector {
+	public onSelect(e: FederatedPointerEvent, parent: GenericContainer): void;
+}
