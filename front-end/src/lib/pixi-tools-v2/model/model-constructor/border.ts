fbff9ea2dde40221434602a2f005b56f939b8915
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-03-28 14:00:11 +0200

    fix(border): Instead of using lineTo for the border, replace it with drawRect

--- front-end/src/lib/pixi-tools-v2/model/model-constructor/border.ts
+++ front-end/src/lib/pixi-tools-v2/model/model-constructor/border.ts
@@ -8,17 +8,12 @@ export class Border extends Graphics {
 		super();
 
 		const { x, y, width, height, scale } = attr;
-		const lineWidth = Math.max(3 / scale, 0.5);
 
 		this.id = "border";
 		this.x = x;
 		this.y = y;
-		this.lineStyle(lineWidth, 0x0c8ce9, 2);
-		this.moveTo(x, y);
-		this.lineTo(x + width, y);
-		this.lineTo(x + width, y + height);
-		this.lineTo(x, y + height);
-		this.lineTo(x, y - lineWidth / 2);
+		this.lineStyle(2 / scale, 0x0c8ce9);
+		this.drawRect(x, y, width, height);
 		this.endFill();
 	}
 }
