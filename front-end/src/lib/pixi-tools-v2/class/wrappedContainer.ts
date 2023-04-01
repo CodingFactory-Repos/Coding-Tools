7ab2dd781394342a3b365f184466de58f3f494e7
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-04-01 22:27:14 +0200

    feat(wrapped): Cleanup the wrapped container, remove the border and implement the bounds container that will be used to manage the resize

--- front-end/src/lib/pixi-tools-v2/class/wrappedContainer.ts
+++ front-end/src/lib/pixi-tools-v2/class/wrappedContainer.ts
@@ -1,57 +1,87 @@
-import { Viewport } from "pixi-viewport";
-import { Container } from "pixi.js";
-import { Border } from "../model/model-constructor/border";
+import { BoundsContainer } from "../types/pixi-class";
 import { CanvasContainer } from "../types/pixi-container-options";
+import { ViewportUI } from "../viewportUI";
 import { FramedContainer } from "./framedContainer";
 
 
-export class WrappedContainer extends Container {
-	public id: string;
-	public readonly children: Array<CanvasContainer>
-	private _viewport: Viewport;
-	private _border: Border;
+export class WrappedContainer extends BoundsContainer {
+	protected readonly viewport: ViewportUI;
+	public readonly children: Array<CanvasContainer>;
+	public readonly id: string;
 
-	constructor(viewport: Viewport) {
+	public absMinX: number;
+	public absMinY: number;
+	public absMaxX: number;
+	public absMaxY: number;
+
+	constructor(viewport: ViewportUI) {
 		super();
 
 		this.id = "wrap";
 		this.cursor = "pointer";
 		this.interactive = true;
-		this._viewport = viewport;
-	}
-
-	public destroyBorder() {
-		if(this._border) {
-			this._border.destroy();
-			this._border = null;
-		}
-	}
-
-	public drawBorder() {
-		this.destroyBorder();
-
-		const { x, y } = this.getLocalBounds();
-
-		this._border = new Border({
-			x: (x / 2),
-			y: (y / 2),
-			width: this.width,
-			height: this.height,
-			scale: this._viewport.scale.x
-		})
-		this.addChild(this._border);
+		this.viewport = viewport;
 	}
 
 	public restoreOriginChildren() {
 		const framed = this.children.filter(ctn => ctn.isAttachedToFrame);
 		for(let n = 0; n < framed.length; n++) {
-			const frame = this._viewport.children.find(ctn => ctn.id === "frame" && ctn.frameNumber === framed[n].frameNumber) as FramedContainer;
+			const frame = this.viewport.children.find(ctn => ctn.id === "frame" && ctn.frameNumber === framed[n].frameNumber) as FramedContainer;
 			if(frame) frame.mainContainer.addChild(framed[n]);
 		}
 
 		if(this.children.length > 0) {
-			this._viewport.addChild(...this.children);
-			this._viewport.removeChild(this);
+			this.viewport.addChild(...this.children);
+			this.viewport.removeChild(this);
 		}
+	}
+
+	protected onChildrenChange(_length?: number): void {
+		super.onChildrenChange(_length);
+		this.updateAbsoluteBounds();
+	}
+
+	protected updateAbsoluteBounds() {
+		let minX = Infinity;
+		let minY = Infinity;
+		let maxX = 0;
+		let maxY = 0;
+
+		for(let n = 0; n < this.children.length; n++) {
+			const geometry = this.children[n].getGeometry();
+			if(geometry === null) continue;
+
+			const { x, y, width, height } = geometry;
+
+			if(x < minX) minX = x;
+			if(y < minY) minY = y;
+			if(x + width > maxX) maxX = x + width;
+			if(y + height > maxY) maxY = y + height;
+		}
+
+		this.absMinX = minX;
+		this.absMinY = minY;
+		this.absMaxX = maxX;
+		this.absMaxY = maxY;
+	}
+
+	public getGeometry() {
+		this.updateAbsoluteBounds();
+		return {
+			x: this.absMinX,
+			y: this.absMinY,
+			width: this.width,
+			height: this.height,
+		}
+	}
+
+	public getGraphicChildren() {
+		const graphics = [];
+
+		for(let n = 0; n < this.children.length; n++) {
+			graphics.push(this.children[n].getGraphicChildren());
+		}
+
+		return graphics.flat();
 	}
 }
