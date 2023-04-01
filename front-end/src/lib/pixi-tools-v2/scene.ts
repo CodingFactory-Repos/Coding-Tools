d2e2e185e856b516ee90676439d89b6f5cbe3b48
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-04-01 22:20:44 +0200

    feat(scene): Cleanup the scene class with the recent change to the viewport ui class

--- front-end/src/lib/pixi-tools-v2/scene.ts
+++ front-end/src/lib/pixi-tools-v2/scene.ts
@@ -1,12 +1,10 @@
-import { FederatedPointerEvent } from 'pixi.js';
-import { Viewport } from 'pixi-viewport';
+import { IViewportOptions } from 'pixi-viewport';
 import { Application, EventSystem } from 'pixi.js';
-import { ContainerManager } from './class/containerManager';
+import { ViewportUI } from './viewportUI';
 
 
 export class Scene extends Application {
-	private _viewport: Viewport;
-	private _manager: ContainerManager;
+	public readonly viewport: ViewportUI;
 
 	constructor(canvas: HTMLCanvasElement) {
 		super({
@@ -22,47 +20,16 @@ export class Scene extends Application {
 		event.domElement = this.renderer.view as HTMLCanvasElement;
 		event.resolution = devicePixelRatio + 1;
 
-		this._viewport = new Viewport({
+		const viewportOptions: IViewportOptions = {
 			worldWidth: 1000,
 			worldHeight: 1000,
 			screenWidth: window.innerWidth,
 			screenHeight: window.innerHeight,
 			events: event,
-		});
+		}
 
-		this._viewport.drag().pinch({ percent: 2 }).wheel().decelerate();
-		this.stage.addChild(this._viewport);
+		this.viewport = new ViewportUI(viewportOptions, this);
+		this.stage.addChild(this.viewport);
 		this.ticker.start();
-
-		this._manager = new ContainerManager(this);
-
-		window.addEventListener('resize', () => {
-			const newWidth = window.innerWidth;
-			const newHeight = window.innerHeight;
-
-			this.renderer.resize(newWidth, newHeight);
-			this._viewport.screenWidth = newWidth;
-			this._viewport.screenHeight = newHeight;
-			this._viewport.worldWidth = newWidth;
-			this._viewport.worldHeight = newHeight;
-		});
-
-		this._viewport.on("pointerdown", (e: FederatedPointerEvent) => {
-			const wrap = this._manager.wrappedContainer;
-			const loc = wrap.toLocal(e.global);
-			if(wrap.getBounds().contains(loc.x, loc.y)) return;
-			else {
-				this._manager.deselectAll();
-				this._manager.detachPlugins();
-			}
-		})
-	}
-
-	get viewport() {
-		return this._viewport;
-	}
-
-	get manager() {
-		return this._manager;
 	}
 }
