6375cba623d7c5b1f536a689f80579c496902a1e
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-04-01 22:24:35 +0200

    feat(selection): Cleanup the selection box

--- front-end/src/lib/pixi-tools-v2/class/selectionBox.ts
+++ front-end/src/lib/pixi-tools-v2/class/selectionBox.ts
@@ -1,57 +1,54 @@
 
-import { Graphics, FederatedPointerEvent, Point, Container } from 'pixi.js';
-import { Drag, Viewport } from 'pixi-viewport';
-import { Scene } from '@/lib/pixi-tools-v2/scene';
-import { ContainerManager } from './containerManager';
+import { Graphics, FederatedPointerEvent, Point } from 'pixi.js';
+import { Drag } from 'pixi-viewport';
 import { CanvasContainer } from '../types/pixi-container-options';
 import { GenericContainer } from './genericContainer';
 import { FramedContainer } from './framedContainer';
+import { ViewportUI } from '../viewportUI';
 
 export class SelectionBox extends Graphics {
+	protected readonly nativeDragPlugin: Drag;
+	protected readonly viewport: ViewportUI;
+	protected readonly box: Graphics;
 	private _startPos: Point;
-	private _viewport: Viewport;
-	private _box: Graphics;
-	private _dragPlugin: Drag;
-	private _manager: ContainerManager;
 
-	constructor(scene: Scene) {
+	constructor(viewport: ViewportUI) {
 		super();
 
 		this.visible = false;
-		this._box = new Graphics();
-		this._box.alpha = 0.2;
-		this._box.visible = false;
-		this.addChild(this._box);
+		this.box = new Graphics();
+		this.box.alpha = 0.2;
+		this.box.visible = false;
+		this.addChild(this.box);
 		
-		this._manager = scene.manager;
-		this._viewport = scene.viewport;
-		this._viewport.on('pointerdown', this.startSelection);
-		this._dragPlugin = this._viewport.plugins.get("drag");
-		this._viewport.addChild(this);
+		this.viewport = viewport;
+		this.viewport.on('pointerdown', this.startSelection);
+		this.nativeDragPlugin = this.viewport.plugins.get("drag");
+		this.viewport.addChild(this);
 	}
 
-	public startSelection = (e: FederatedPointerEvent) => {
+	private startSelection = (e: FederatedPointerEvent) => {
 		if(e.originalEvent.shiftKey) {
-			this._startPos = this._viewport.toWorld(e.global.clone());
+			this._startPos = this.viewport.toWorld(e.global.clone());
 
 			this.clear();
-			this._box.clear();
+			this.box.clear();
 			this.visible = true;
-			this._box.visible = true;
+			this.box.visible = true;
 
-			this._viewport.on('pointermove', this.updateSelection);
-			this._viewport.on('pointerup', this.endSelection);
+			this.viewport.on('pointermove', this.updateSelection);
+			this.viewport.on('pointerup', this.endSelection);
 		}
 	}
 
-	public updateSelection = (e: FederatedPointerEvent) => {
+	private updateSelection = (e: FederatedPointerEvent) => {
 		if (!this.visible) return;
-		if (!this._dragPlugin.paused) {
-			this._dragPlugin.pause();
+		if (!this.nativeDragPlugin.paused) {
+			this.nativeDragPlugin.pause();
 		}
 
-		const scale = this._viewport.scaled;
-		const point = this._viewport.toWorld(e.global.clone());
+		const scale = this.viewport.scaled;
+		const point = this.viewport.toWorld(e.global.clone());
 		const startX = Math.min(point.x, this._startPos.x);
 		const startY = Math.min(point.y, this._startPos.y);
 		const endX = Math.max(point.x, this._startPos.x);
@@ -62,42 +59,42 @@ export class SelectionBox extends Graphics {
 		this.clear();
 		this.lineStyle(2 / scale, 0x0c8ce9);
 		this.drawRect(startX, startY, width, height);
-		this._box.clear();
-		this._box.beginFill(0x0c8ce9);
-		this._box.drawRect(startX, startY, width, height);
-		this._box.endFill();
+		this.box.clear();
+		this.box.beginFill(0x0c8ce9);
+		this.box.drawRect(startX, startY, width, height);
+		this.box.endFill();
 	}
 
-	public endSelection = () => {
+	private endSelection = () => {
 		this.visible = false;
-		this._box.visible = false;
-		this._dragPlugin.resume();
-		this._viewport.off('pointermove', this.updateSelection);
-		this._viewport.off('pointerup', this.endSelection);
+		this.box.visible = false;
+		this.nativeDragPlugin.resume();
+		this.viewport.off('pointermove', this.updateSelection);
+		this.viewport.off('pointerup', this.endSelection);
 
 		const selectionBounds = this.getBounds();
 
 		const selectedChildren: Array<CanvasContainer> = [];
-		for(let n = 0; n < this._viewport.children.length; n++) {
-			const child = this._viewport.children[n];
+		for(let n = 0; n < this.viewport.children.length; n++) {
+			const child = this.viewport.children[n];
 
 			if(child instanceof GenericContainer || child instanceof FramedContainer) {
 				if(child.getBounds().intersects(selectionBounds)) {
-					selectedChildren.push(this._viewport.children[n]);
+					selectedChildren.push(this.viewport.children[n]);
 				}
 			}
 		}
 
 		for(let n = 0; n < selectedChildren.length; n++) {
-			this._manager.selectContainer(selectedChildren[n], true);
+			this.viewport.manager.selectContainer(selectedChildren[n], true);
 		}
 	}
 
 	public destroy() {
 		super.destroy();
-		this._viewport.off('pointerdown', this.startSelection);
-		this._viewport.off('pointermove', this.updateSelection);
-		this._viewport.off('pointerup', this.endSelection);
-		this._viewport.removeChild(this);
+		this.viewport.off('pointerdown', this.startSelection);
+		this.viewport.off('pointermove', this.updateSelection);
+		this.viewport.off('pointerup', this.endSelection);
+		this.viewport.removeChild(this);
 	}
 }
