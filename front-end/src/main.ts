0962e93a9256b1ac4a4b7e8a0853fd57b94f7be9
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-04-02 21:00:06 +0200

    feat(app): Register the context menu global components

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
+import ContextMenu from '@imengyu/vue3-context-menu';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 import 'flowbite';
+import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css'
 import 'sweetalert2/dist/sweetalert2.min.css';
 import './styles/scss/config.scss';
 import './styles/scss/glassmorphism.scss';
@@ -25,6 +27,7 @@ withErrorHandler(async function () {
 function bootVueApp() {
 	const app = createApp(App);
 
+	app.use(ContextMenu);
 	app.use(router);
 	app.use(store);
 	app.mount('#app');
