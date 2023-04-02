5654c64ca6d19547bda529841e8a3bc47fc64d90
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-04-02 21:06:47 +0200

    feat(ui): Implement the compatibility between the old ui, the new ui and the miro ui

--- front-end/src/components/agility/AgilityCanvasUI.vue
+++ front-end/src/components/agility/AgilityCanvasUI.vue
@@ -1,25 +1,34 @@
 <template>
-	<AgilityFocusUI v-if="focusMode" @update:focus-mode="updateFocusMode"/>
-	<div class="w-full h-full relative">
-		<div class="w-full h-full absolute pointer-events-none flex align-center items-center justify-center text-red-500">+</div>
-		<AgilitySceneUI2 v-if="!focusMode && route.path === '/app/agility/new'" @update:focus-mode="updateFocusMode"/>
-		<AgilitySceneUI v-else @update:focus-mode="updateFocusMode"/>
-		<slot></slot>
+	<div class="w-full h-full flex flex-col gap-0">
+		<template v-if="$route.path === '/app/agility/new'">
+			<AgilitySceneUI>
+				<ToolsEditorUI v-if="!immersion"/>
+				<div class="flex w-full h-full relative">
+					<DefaultSelectionBoxUI v-if="!immersion"/>
+					<MiroSelectionBox v-else/>
+					<slot></slot>
+				</div>
+			</AgilitySceneUI>
+		</template>
+		<template v-else>
+			<div class="flex w-full h-full relative">
+				<OldSelectionBox/>
+				<slot></slot>
+			</div>
+		</template>
 	</div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
-import { useRoute } from 'vue-router';
-import AgilitySceneUI from '@/components/agility/UI/SceneMode.vue';
-import AgilitySceneUI2 from '@/components/agility/UI/SceneMode2.vue';
-import AgilityFocusUI from '@/components/agility/UI/FocusMode.vue';
+import { computed } from 'vue';
+import { useProjectStorev2 } from '@/store/modules/project2.store';
 
+import MiroSelectionBox from '@/components/agility/UI/MiroSelectionBox.vue';
+import OldSelectionBox from '@/components/agility/UI/OldSelectionBox.vue';
+import ToolsEditorUI from '@/components/agility/UI/ToolsEditor.vue';
+import DefaultSelectionBoxUI from '@/components/agility/UI/DefaultSelectionBox.vue';
+import AgilitySceneUI from '@/components/agility/UI/SceneUI.vue';
 
-const route = useRoute();
-const focusMode = ref(false);
-
-const updateFocusMode = (val: boolean) => {
-	focusMode.value = val;
-}
+const projectStore = useProjectStorev2();
+const immersion = computed(() => projectStore.immersion);
 </script>
