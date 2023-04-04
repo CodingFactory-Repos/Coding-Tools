1f5e8ba6c078ccc32f0639b2f9ceda884d16c082
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-04-04 22:58:52 +0200

    types(augment): Augment the css cursor type and federated pointer event

--- front-end/types/index.d.ts
+++ front-end/types/index.d.ts
@@ -1,6 +1,6 @@
 declare global {
 	export namespace CSStyleProperty {
-		type Cursor = "alias" | "all-scroll" | "auto" | "cell" | "context-menu" | "col-resize" | "copy" | "crosshair" | "default" | "e-resize" | "ew-resize" | "help"
+		type Cursor = "alias" | "all-scroll" | "auto" | "cell" | "context-menu" | "col-resize" | "copy" | "crosshair" | "default" | "e-resize" | "ew-resize" | "grabbing" | "help"
 			| "move" | "n-resize" | "ne-resize" | "nesw-resize" | "ns-resize" | "nw-resize" | "nwse-resize" | "no-drop" | "none" | "not-allowed" | "pointer" | "progress"
 			| "row-resize" | "s-resize" | "se-resize" | "sw-resize" | "text" | "URL" | "vertical-text" | "w-resize" | "wait" | "zoom-in" | "zoom-out" | "inital" | "inherit";
 	}
--- front-end/types/pixi-event.d.ts
+++ front-end/types/pixi-event.d.ts
@@ -4,4 +4,12 @@ declare module "pixi.js" {
 	interface FederatedEvent {
 	  shiftKey: boolean;
 	}
+
+	interface FederatedPointerEvent {
+		/**
+		 * Augmented property defining when an event was emitted forcibly without a user action.
+		 * Attach a forced FederatedPointerEvent.
+		 */
+		forced: boolean;
+	}
 }
