8d8fd21fc67b7bda39bf6e3de5d2ecafe7e4b554
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-04-06 19:21:04 +0200

    fix(manager): Remove the fix function and manage it before adding child to the wrapped

--- front-end/src/lib/pixi-tools-v2/class/containerManager.ts
+++ front-end/src/lib/pixi-tools-v2/class/containerManager.ts
@@ -1,7 +1,6 @@
 import { WrappedContainer } from './wrappedContainer';
 import { CanvasContainer, PluginContainer } from '../types/pixi-container-options';
 import { FramedContainer } from './framedContainer';
-import { GenericContainer } from './genericContainer';
 import { ResizePlugin } from '../plugins/resizePlugin';
 import { ViewportUI } from "../viewportUI";
 import { Point } from 'pixi.js';
@@ -16,7 +15,7 @@ export class ContainerManager {
 
 	constructor(viewport: ViewportUI) {
 		this.viewport = viewport;
-		this.wrappedContainer = new WrappedContainer(this.viewport);
+		this.wrappedContainer = new WrappedContainer(this, this.viewport);
 		this.resizePlugin = new ResizePlugin(this.viewport);
 		this.dragPlugin = new DragPlugin(this.viewport);
 		this._selectedContainers = [];
@@ -42,11 +41,8 @@ export class ContainerManager {
 	 * @param isShift
 	 * @returns void
 	 */
-	public selectContainer(container: CanvasContainer, isShift: boolean, ) {
+	public selectContainer(container: CanvasContainer, isShift: boolean) {
 		if (!this._selectedContainers.includes(container)) {
-			const shouldReturn = this.fixFrameChildrenContext(container);
-			if(shouldReturn) return;
-
 			// Select the container and retrieve the position index
 			const len = this._selectedContainers.push(container);
 			const index = len - 1;
@@ -56,6 +52,7 @@ export class ContainerManager {
 			if (!isShift && this.wrappedContainer.children.length > 0) {
 				this.detachPlugins();
 				this.viewport.destroyBorder();
+				console.log("detached")
 				this.wrappedContainer.restoreOriginChildren();
 				this.wrappedContainer.removeChildren();
 			}
@@ -92,6 +89,7 @@ export class ContainerManager {
 
 			this.detachPlugins();
 			this.viewport.destroyBorder();
+			console.log("one of more")
 			this.wrappedContainer.restoreOriginChildren();
 			this.wrappedContainer.removeChildren();
 			this.deselectAllExceptThisContainer(index);
@@ -100,39 +98,10 @@ export class ContainerManager {
 		}
 	}
 
-	/**
-	 * This method is used once, and has its name suggest it's a fix of an issue.
-	 * This issue could potentially be reworked later, but for readability it will be separated to avoid dirtying the selectContainer method.
-	 */
-	private fixFrameChildrenContext(container: CanvasContainer) {
-		// If the container is a FramedContainer,
-		// try to find all its selected children,
-		// if some children were found, remove them from the selected array and destroy any existing borders and finally add them back to the frame.
-		//! This prevent a bug where the children context is lost when doing : select children of frame then select frame
-		if(container instanceof FramedContainer) {
-			const childsOfFrame = this._selectedContainers.filter((ctn) => ctn.frameNumber === container.frameNumber);
-			if(childsOfFrame.length > 0) {
-				this._selectedContainers = this._selectedContainers.filter((ctn) => !childsOfFrame.includes(ctn));
-				this.viewport.destroyBorder();
-				container.mainContainer.addChild(...childsOfFrame);
-			}
-
-		//! This prevent a bug where the children context is lost when doing : select frame then select children of frame
-		} else if(container instanceof GenericContainer && container.frameNumber !== -1) {
-			const frames = this._selectedContainers.filter((ctn) => ctn.id === "frame") as Array<FramedContainer>;
-			for(let n = 0; n < frames.length; n++) {
-				if(frames[n].mainContainer.children[n].parent === container.parent) {
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-
 	public deselectAll() {
 		if(this.wrappedContainer.children.length > 0) {
 			this.viewport.destroyBorder();
+			console.log("deselect all")
 			this.wrappedContainer.restoreOriginChildren();
 			this.wrappedContainer.removeChildren();
 		}
@@ -167,7 +136,27 @@ export class ContainerManager {
 	}
 
 	public wrapWithTemporaryParent() {
-		this.wrappedContainer.addChild(...this._selectedContainers);
+		const frames = this._selectedContainers.filter((ctn) => ctn.id === "frame") as Array<FramedContainer>;
+		const childs = [];
+		for(let n = 0; n < this._selectedContainers.length; n++) {
+			if(this._selectedContainers[n].id !== "frame") {
+				let found = false;
+				frames.forEach((frame) => {
+					if(frame.frameNumber === this._selectedContainers[n].frameNumber) {
+						// ensure context is kept, it won't add it if it already exist
+						frame.mainContainer.addChild(this._selectedContainers[n])
+						found = true;
+					}
+				})
+
+				if(!found) childs.push(this._selectedContainers[n]);
+			} else {
+				childs.push(this._selectedContainers[n]);
+			}
+		}
+
+		this.wrappedContainer.removeChildren();
+		this.wrappedContainer.addChild(...childs);
 		const borderOptions = this.wrappedContainer.getGeometry();
 		this.viewport.createBorder({
 			...borderOptions,
@@ -175,6 +164,7 @@ export class ContainerManager {
 		});
 		this.wrappedContainer.hitArea = this.wrappedContainer.getLocalBounds();
 		this.viewport.addChild(this.wrappedContainer);
+		this.wrappedContainer.toggleChildrenInteractive(false);
 		this.attachPlugins(this.wrappedContainer);
 	}
 
