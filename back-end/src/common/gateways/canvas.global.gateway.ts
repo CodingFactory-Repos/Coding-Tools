ea5d3245bd66b8f8d907395f2b596a7c767584f8
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-04-15 20:57:22 +0200

    feat(socket-connection): Mongodb query to retrieve the user email

--- back-end/src/common/gateways/canvas.global.gateway.ts
+++ back-end/src/common/gateways/canvas.global.gateway.ts
@@ -7,6 +7,8 @@ import { WSServiceErrorCatcher } from '@/common/decorators/ws.catch.decorator';
 import { AuthSocket, WSAuthMiddleware } from '@/common/middlewares/socket.auth.middleware';
 import { CanvasRoomRepository } from '@/base/canvasRoom/canvasRoom.repository';
 import { ElementBounds, SerializedContainer } from '@/base/canvasRoom/interfaces/canvasRoom.interface';
+import { UsersRepository } from '@/base/users/users.repository';
+import { ObjectId } from 'mongodb';
 
 @UseFilters(WSServiceErrorCatcher)
 @WebSocketGateway({
@@ -20,6 +22,8 @@ export class CanvasGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 	constructor(
 		@Inject(forwardRef(() => CanvasRoomRepository))
 		private canvasRoomRepository: CanvasRoomRepository,
+		@Inject(forwardRef(() => UsersRepository))
+		private usersRepository: UsersRepository,
 		private readonly jwtService: JwtService
 	) {}
 	@WebSocketServer() server: Server;
@@ -29,13 +33,21 @@ export class CanvasGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 	}
 
 	async handleConnection(client: AuthSocket) {
+		const query = { _id: client.user.id as ObjectId };
+		const projection = { projection: { profile: { email: 1 } } }
+		const user = await this.usersRepository.findOne(query, projection);
+
 		client.join(client.roomId);
-		client.to(client.roomId).emit('peer-connected', client.id);
+		client.to(client.roomId).emit('peer-connected', user.profile.email);
 		console.log("connected");
 	}
 
-	handleDisconnect(client: AuthSocket) {
-		client.to(client.roomId).emit('peer-disconnected', client.id);
+	async handleDisconnect(client: AuthSocket) {
+		const query = { _id: client.user.id as ObjectId };
+		const projection = { projection: { profile: { email: 1 } } }
+		const user = await this.usersRepository.findOne(query, projection);
+
+		client.to(client.roomId).emit('peer-disconnected', user.profile.email);
 		console.log("disconnected");
 	}
 
