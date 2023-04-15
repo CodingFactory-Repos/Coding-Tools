038a3bb3174278abdc2180c19e180985737ca23f
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-04-15 18:26:34 +0200

    feat(plugin): Create the viewport socket plugin that extends event emitter and will be used for the socket

---
+++ front-end/src/lib/pixi-tools-v2/plugins/viewportSocketPlugin.ts
@@ -0,0 +1,48 @@
+import { utils } from "pixi.js";
+import { ManagerOptions } from "socket.io-client";
+
+import { ViewportUI } from "../viewportUI";
+import { CanvasContainer } from "../types/pixi-aliases";
+import { ElementPosition } from "../types/pixi-container";
+import { SocketManager } from "../class/socketManager";
+
+interface CanvasSocketEvents {
+	"ws-element-deleted": (uuid: string) => void;
+	"ws-element-added": (element: CanvasContainer, isRemote: boolean) => void;
+	"ws-element-dragged": (uuid: string, position: ElementPosition) => void;
+	"ws-element-resized": () => void;
+	"ws-element-modified": () => void;
+}
+
+export interface CanvasSocketOptions {
+	uri: string,
+	roomId: string,
+	options: Partial<ManagerOptions>
+}
+
+export class ViewportSocketPlugin extends utils.EventEmitter<CanvasSocketEvents> {
+	protected readonly socketManager: SocketManager;
+
+	constructor(viewport: ViewportUI, socketOptions?: CanvasSocketOptions) {
+		const { uri, roomId, options } = socketOptions;
+		if(!uri) throw Error("Socket.io uri is required");
+
+		super();
+
+		this.socketManager = new SocketManager(uri, roomId, options, viewport);
+
+		this.on('ws-element-added', (child, isRemote) => {
+			if(!isRemote) {
+				this.socketManager.addElement(child.serializeData());
+			}
+		})
+
+		this.on('ws-element-dragged', (uuid, position) => {
+			this.socketManager.updateElementPosition(uuid, position);
+		})
+
+		this.on('ws-element-deleted', (uuid) => {
+			this.socketManager.deleteElement(uuid);
+		})
+	}
+}
