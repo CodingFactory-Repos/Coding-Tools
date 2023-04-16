ccef37b6a5414de5056e685be2b017972b41c4f1
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-04-17 00:59:33 +0200

    fix(instance): Useless instance initializer

--- front-end/src/lib/pixi-tools-v2/unsupported/lib_TextInput.ts
+++ front-end/src/lib/pixi-tools-v2/unsupported/lib_TextInput.ts
@@ -642,23 +642,3 @@ function DefaultBoxGenerator(styles) {
 		return box;
 	};
 }
-
-! Use
-
-new LibTextInput({
-	input: {
-		fontSize: '36px',
-		padding: '2px',
-		width: options.width,
-		height: options.height,
-		x: options.positionX,
-		y: options.positionY,
-		multiline: true,
-		overflow: 'hidden',
-	},
-	box: {
-		default: { fill: 0, rounded: 0, stroke: { color: 0, width: 0 } },
-		focused: { fill: 0, rounded: 0, stroke: { color: 0, width: 0 } },
-		disabled: { fill: 0, rounded: 0, stroke: { color: 0, width: 0 } },
-	},
-});
