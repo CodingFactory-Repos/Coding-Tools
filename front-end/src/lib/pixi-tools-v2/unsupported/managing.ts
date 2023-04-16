57c16dc7dc6098090948f7f54418ebe8ae840da1
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-04-17 00:53:31 +0200

    fix(comment): Uncomment the two files

--- front-end/src/lib/pixi-tools-v2/unsupported/managing.ts
+++ front-end/src/lib/pixi-tools-v2/unsupported/managing.ts
@@ -1,32 +1,32 @@
-// import { ConfigBluePrint } from '../types/blueprint-manager';
-// import { PixiEvents, PixiObject } from '../types';
+//@ts-nocheck
+import { ConfigBluePrint } from '../types/blueprint-manager';
 
-// export class Managed implements ConfigBluePrint.Config, ConfigBluePrint.ListOfBluePrintConfig {
-// 	// Define the list of existing blueprints
-// 	NumberOfBlueprint: ConfigBluePrint.Config[];
-// 	SelectedIndexBlueprint: number;
+export class Managed implements ConfigBluePrint.Config, ConfigBluePrint.ListOfBluePrintConfig {
+	// Define the list of existing blueprints
+	NumberOfBlueprint: ConfigBluePrint.Config[];
+	SelectedIndexBlueprint: number;
 
-// 	private _blueprints: ConfigBluePrint.Config;
+	private _blueprints: ConfigBluePrint.Config;
 
-// 	// Define the specific element to display
-// 	width: number;
-// 	height: number;
-// 	name: string;
-// 	type: string;
-// 	numberOfChildren: number;
+	// Define the specific element to display
+	width: number;
+	height: number;
+	name: string;
+	type: string;
+	numberOfChildren: number;
 
-// 	constructor(Width: number, Height: number, Name: string, Type: string, NumberOfChildren: number) {
-// 		let blueprint = new ConfigBluePrint.Config();
-// 		blueprint.width = Width;
-// 		blueprint.height = Height;
-// 		blueprint.name = Name;
-// 		blueprint.type = Type;
-// 		if (NumberOfChildren != 0 || NumberOfChildren != null) {
-// 			blueprint.numberOfChildren = NumberOfChildren;
-// 		}
-// 		this._blueprints = blueprint;
-// 	}
-// 	get blueprint() {
-// 		return this._blueprints;
-// 	}
-// }
+	constructor(Width: number, Height: number, Name: string, Type: string, NumberOfChildren: number) {
+		let blueprint = new ConfigBluePrint.Config();
+		blueprint.width = Width;
+		blueprint.height = Height;
+		blueprint.name = Name;
+		blueprint.type = Type;
+		if (NumberOfChildren != 0 || NumberOfChildren != null) {
+			blueprint.numberOfChildren = NumberOfChildren;
+		}
+		this._blueprints = blueprint;
+	}
+	get blueprint() {
+		return this._blueprints;
+	}
+}
