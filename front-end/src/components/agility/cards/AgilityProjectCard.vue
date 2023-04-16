8ed793903493e9efd46413cd512e94ba93029290
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-04-16 17:39:34 +0200

    feat(project-card): Add an image fallback in case the snapshot in undefined

--- front-end/src/components/agility/cards/AgilityProjectCard.vue
+++ front-end/src/components/agility/cards/AgilityProjectCard.vue
@@ -1,15 +1,33 @@
 <template>
 	<div class="w-52 h-fit flex flex-col justify-between bg-light-primary dark:bg-dark-tertiary p-3 rounded-lg gap-2 cursor-pointer select">
-		<img v-if="url" class="w-full h-48 rounded" :src="url" :alt="`project_${title}_snapshot`"/>
+		<img ref="imageRef" class="w-full h-48 rounded" :src="url" :alt="`${title} snapshot`"/>
 		<span class="dark:text-dark-font text-sm font-bold">{{ title }}</span>
 	</div>
 </template>
 
 <script lang="ts" setup>
+import { onMounted } from '@vue/runtime-core';
+import { ref } from '@vue/reactivity';
+
 defineProps({
 	title: { type: String, required: true },
 	isNew: { type: Boolean, required: false },
 	url: { type: String, required: false },
+})
+
+const imageRef = ref<HTMLImageElement>();
+
+const replaceGenericImage = () => {
+	if(imageRef.value) {
+		imageRef.value.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
+		imageRef.value.removeEventListener("error", replaceGenericImage);
+	}
+}
+
+onMounted(() => {
+	if(imageRef.value) {
+		imageRef.value.addEventListener("error", replaceGenericImage);
+	}
 })
 </script>
 
