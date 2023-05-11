600ac15e49e807d69439685556cde27550b825b1
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-05-12 00:57:42 +0200

    feat(user): Implement the routes to retrieve the user github stats

--- back-end/src/base/users/users.controller.ts
+++ back-end/src/base/users/users.controller.ts
@@ -1,10 +1,34 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Jwt } from '@/common/decorators/jwt.decorator';
+import { JwtAuthGuard } from '@/common/guards/auth.guard';
+import { Body, Controller, Get, Patch, Res, UseGuards } from '@nestjs/common';
 import { Response } from 'express';
+import { ObjectId } from 'mongodb';
+import { UsersService } from '@/base/users/users.service';
+import { ProfileBodyDTO } from '@/base/users/dto/users.dto';
 
 @Controller('users')
 export class UsersController {
-	@Get()
-	index(@Res() res: Response) {
-		return res.status(201).json({ status: 'ok' });
+	constructor(private readonly usersService: UsersService) {}
+	
+	@Get("github/stats")
+	@UseGuards(JwtAuthGuard)
+	async getGithubStat(@Jwt() userId: ObjectId, @Res() res: Response) {
+		const document = await this.usersService.getGithubStat(userId);
+		if(document === null)
+			return res.status(200).send(null);
+
+		res.setHeader('Content-Type', 'text/html');
+		return res.status(200).send(document);
+	}
+
+	@Get("github/languages")
+	@UseGuards(JwtAuthGuard)
+	async getGithubLanguages(@Jwt() userId: ObjectId, @Res() res: Response) {
+		const document = await this.usersService.getGithubLanguages(userId);
+		if(document === null)
+			return res.status(200).send(null);
+
+		res.setHeader('Content-Type', 'text/html');
+		return res.status(200).send(document);
 	}
 }
