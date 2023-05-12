38eff46e4b9f003862507494c7c786a816fa633c
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-05-13 00:06:21 +0200

    fix(profile): Implement the new component on the user and other user profile

--- front-end/src/views/app/AccountView.vue
+++ front-end/src/views/app/AccountView.vue
@@ -22,7 +22,10 @@
 					@open="toggleAccountForm(true)"
 					@close="toggleAccountForm(false)"
 				/>
-				<div class="w-full h-full p-4 flex flex-row gap-3 items-start justify-start flex-col sm:flex-row min-h-[420px]" v-if="!editActive">
+				<div
+					v-if="!editActive"
+					class="w-full h-full p-4 flex flex-row gap-3 items-start justify-start flex-col sm:flex-row min-h-[420px]"
+				>
 					<AccountProfile
 						:profile="profile"
 						:schoolProfile="schoolProfile"
@@ -31,30 +34,10 @@
 						:role="role"
 					/>
 
-					<div class="flex h-full flex-col gap-3 w-full sm:w-fit ">
-						<!-- <div class="stat-svg grow" v-html="svgGithubLanguages"/> -->
-						<div class="h-full flex flex-col rounded-lg bg-light-primary dark:bg-dark-highlight min-w-[300px] min-h-[388px] overflow-y-scroll p-2 w-full">
-							<span class="w-full flex justify-center text-sm pb-2">{{ schoolProfile.groupName }}</span>
-							<template v-if="relatedProfile.length > 0">
-								<div
-									v-for="(relatedUser, index) in relatedProfile"
-									:key="`related_user_${index}`"
-									@click="viewRelatedUserProfile(relatedUser.id)"
-									class="px-2 py-2 w-full flex gap-5 items-center bg-dark-tertiary rounded-lg cursor-pointer hover:scale-[1.02] transition-all"
-								>
-									<img
-										:src="relatedUser.picture || '/template-no-image.png'"
-										class="w-12 h-12 rounded-full"
-										alt="profile_picture"
-									/>
-									<span class="bold text-sm">{{ relatedUser.firstName + " " + relatedUser.lastName }}</span>
-								</div>
-							</template>
-							<div v-else class="w-full h-full flex justify-center items-center">
-								No related profile found..
-							</div>
-						</div>
-					</div>
+					<AcccountRelatedProfile
+						:groupName="schoolProfile.groupName"
+						:relatedProfiles="relatedProfiles"
+					/>
 				</div>
 				<AccountForm
 					v-else
@@ -77,10 +60,10 @@ import AccountProfile from '@/components/account/AccountProfile.vue';
 import AccountForm from '@/components/account/AccountForm.vue';
 import AccountHeader from '@/components/account/AccountHeader.vue';
 import FileUploader from '@/components/common/FileUploader.vue';
+import AcccountRelatedProfile from '@/components/account/AccountRelatedProfile.vue';
 import { UserProfile, UserSchoolProfile, UserBusinessProfile } from '@/store/interfaces/auth.interfaces';
 import { useAccountImageUpload } from '@/composables/useAccountImageUpload';
 import { useUserStore } from '@/store/modules/user.store';
-import { useRouter } from 'vue-router';
 
 const {
 	fileUploaderREF, tempPicture, pictureUrl,
@@ -90,7 +73,6 @@ addTo("profile", "background");
 
 const authStore = useAuthStore();
 const userStore = useUserStore();
-const router = useRouter();
 
 const editActive = ref(false);
 const user = computed(() => authStore.user);
@@ -100,7 +82,7 @@ const profile = computed(() => user.value.profile ?? {} as Partial<UserProfile>)
 const schoolProfile = computed(() => user.value.schoolProfile ?? {} as Partial<UserSchoolProfile>);
 const businessProfile = computed(() => user.value.businessProfile ?? {} as Partial<UserBusinessProfile>);
 const profileBackground = computed(() => profile.value?.background ?? "/template-no-image.png");
-const relatedProfile = computed(() => userStore.relatedProfiles);
+const relatedProfiles = computed(() => userStore.relatedProfiles);
 
 const toggleAccountForm = (value: boolean) => {
 	editActive.value = value;
@@ -110,16 +92,7 @@ watch(editActive, () => {
 	resetTempPicture();
 })
 
-const viewRelatedUserProfile = (id: string) => {
-	router.push(`/app/account/${id}`);
-}
-
-const svgGithubLanguages = ref<string>();
 onMounted(() => {
-	// http.get("users/github/languages")
-	// .then(response => svgGithubLanguages.value = response.data)
-	// .catch((err: AxiosError) => { console.error(err.message) });
-
 	if(userStore.relatedProfiles.length === 0) {
 		userStore.relatedGroupProfile();
 	}
--- front-end/src/views/app/RelatedProfileView.vue
+++ front-end/src/views/app/RelatedProfileView.vue
@@ -22,30 +22,10 @@
 						:role="role"
 					/>
 
-					<div class="flex h-full flex-col gap-3 w-full sm:w-fit">
-						<div class="h-full flex flex-col rounded-lg bg-light-primary dark:bg-dark-highlight min-w-[300px] min-h-[388px] overflow-y-scroll p-2 w-full">
-							<span class="w-full flex justify-center text-sm pb-2 text-black dark:text-white">{{ schoolProfile.groupName }}</span>
-							<hr class="w-full pb-4 dark:border-dark-tertiary"/>
-							<template v-if="relatedProfile.length > 0">
-								<div
-									v-for="(relatedUser, index) in relatedProfile"
-									:key="`related_user_${index}`"
-									@click="viewRelatedUserProfile(relatedUser.id)"
-									class="px-2 py-2 w-full flex gap-5 items-center bg-light-tertiary dark:bg-dark-tertiary rounded-lg cursor-pointer hover:scale-[1.02] transition-transform"
-								>
-									<img
-										:src="relatedUser.picture || '/template-no-image.png'"
-										class="w-12 h-12 rounded-full border border-dark-secondary"
-										alt="profile_picture"
-									/>
-									<span class="bold text-sm text-black dark:text-white">{{ relatedUser.firstName + " " + relatedUser.lastName }}</span>
-								</div>
-							</template>
-							<div v-else class="w-full h-full flex justify-center items-center">
-								No related profile found..
-							</div>
-						</div>
-					</div>
+					<AcccountRelatedProfile
+						:groupName="schoolProfile.groupName"
+						:relatedProfiles="relatedProfiles"
+					/>
 				</div>
 			</div>
 		</div>
@@ -57,7 +37,8 @@ import { computed, onMounted, ref } from 'vue';
 
 import AccountProfile from '@/components/account/AccountProfile.vue';
 import AccountHeader from '@/components/account/AccountHeader.vue';
-import { UserProfile, UserSchoolProfile, UserBusinessProfile, User } from '@/store/interfaces/auth.interfaces';
+import AcccountRelatedProfile from '@/components/account/AccountRelatedProfile.vue';
+import { UserProfile, UserSchoolProfile, UserBusinessProfile } from '@/store/interfaces/auth.interfaces';
 import { useUserStore } from '@/store/modules/user.store';
 import { useRoute, useRouter } from 'vue-router';
 import { useAuthStore } from '@/store/modules/auth.store';
@@ -67,7 +48,7 @@ const userStore = useUserStore();
 const router = useRouter();
 
 const userProfileData = computed(() => userStore.relatedUserProfile);
-const relatedProfile = computed(() => userProfileData.value.related ?? []);
+const relatedProfiles = computed(() => userProfileData.value.related ?? []);
 const user = computed(() => userProfileData.value.user ?? {});
 const userId = ref<string>(null);
 
