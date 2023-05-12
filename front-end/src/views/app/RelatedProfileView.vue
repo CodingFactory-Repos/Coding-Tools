e5390c6d2c84ea4d0fe9ae7ffe21b710a98884ea
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-05-12 23:53:58 +0200

    fix(theme): Fix the related user theme between light and dark

--- front-end/src/views/app/RelatedProfileView.vue
+++ front-end/src/views/app/RelatedProfileView.vue
@@ -24,20 +24,21 @@
 
 					<div class="flex h-full flex-col gap-3 w-full sm:w-fit">
 						<div class="h-full flex flex-col rounded-lg bg-light-primary dark:bg-dark-highlight min-w-[300px] min-h-[388px] overflow-y-scroll p-2 w-full">
-							<span class="w-full flex justify-center text-sm pb-2">{{ schoolProfile.groupName }}</span>
+							<span class="w-full flex justify-center text-sm pb-2 text-black dark:text-white">{{ schoolProfile.groupName }}</span>
+							<hr class="w-full pb-4 dark:border-dark-tertiary"/>
 							<template v-if="relatedProfile.length > 0">
 								<div
 									v-for="(relatedUser, index) in relatedProfile"
 									:key="`related_user_${index}`"
 									@click="viewRelatedUserProfile(relatedUser.id)"
-									class="px-2 py-2 w-full flex gap-5 items-center bg-dark-tertiary rounded-lg cursor-pointer hover:scale-[1.02] transition-all"
+									class="px-2 py-2 w-full flex gap-5 items-center bg-light-tertiary dark:bg-dark-tertiary rounded-lg cursor-pointer hover:scale-[1.02] transition-transform"
 								>
 									<img
 										:src="relatedUser.picture || '/template-no-image.png'"
 										class="w-12 h-12 rounded-full border border-dark-secondary"
 										alt="profile_picture"
 									/>
-									<span class="bold text-sm">{{ relatedUser.firstName + " " + relatedUser.lastName }}</span>
+									<span class="bold text-sm text-black dark:text-white">{{ relatedUser.firstName + " " + relatedUser.lastName }}</span>
 								</div>
 							</template>
 							<div v-else class="w-full h-full flex justify-center items-center">
