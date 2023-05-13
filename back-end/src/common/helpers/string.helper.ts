18f763c0382697dd2fb88dee9c3e55c80f787d1e
codefactor-io <support@codefactor.io>
2023-05-13 22:46:02 +0000

    [CodeFactor] Apply fixes

--- back-end/src/common/helpers/string.helper.ts
+++ back-end/src/common/helpers/string.helper.ts
@@ -63,7 +63,7 @@ export const parseCookieString = (cookieData: string) => {
 
 export const capitalizeString = (str: string) => {
 	const words = str.split(" ");
-	for (var i = 0; i < words.length; i++) {
+	for (let i = 0; i < words.length; i++) {
 	  words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
 	}
 	return words.join(" ");
