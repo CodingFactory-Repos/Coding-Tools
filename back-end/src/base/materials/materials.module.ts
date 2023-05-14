3b8033100474942f3fbf7cd82238a30167336c8d
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-05-14 16:28:38 +0200

    fix(jwt): Remove the jwt as it is not required.

--- back-end/src/base/materials/materials.module.ts
+++ back-end/src/base/materials/materials.module.ts
@@ -7,18 +7,9 @@ import { UsersRepository } from 'src/base/users/users.repository';
 import { MaterialsRepository } from 'src/base/materials/materials.repository';
 import { MaterialsService } from 'src/base/materials/materials.service';
 import { MaterialsController } from 'src/base/materials/materials.controller';
-import { JwtModule } from '@nestjs/jwt';
-import { config } from '@/config/config';
 
 @Module({
-	imports: [
-		DatabaseModule,
-		JwtModule.register({
-			secret: config.jwt.secret,
-			signOptions: { expiresIn: '1d' },
-		}),
-		forwardRef(() => AuthModule),
-	],
+	imports: [DatabaseModule, forwardRef(() => AuthModule)],
 	providers: [MaterialsService, MaterialsRepository, UsersRepository],
 	controllers: [MaterialsController],
 	exports: [MaterialsService, MaterialsRepository],
