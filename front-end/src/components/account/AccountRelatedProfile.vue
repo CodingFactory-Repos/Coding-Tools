1669d949edbc85c6c70d0bd71555abab391ec3e2
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-05-14 16:29:01 +0200

    fix(import): Removed built-in vue function, no need to import it

--- front-end/src/components/account/AccountRelatedProfile.vue
+++ front-end/src/components/account/AccountRelatedProfile.vue
@@ -26,7 +26,6 @@
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from 'vue';
 import { useRouter } from 'vue-router';
 import { UserProfileList } from '@/store/interfaces/user.interface';
 
