0810992fbe25350b649b55480d493cf0fa1f5970
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-05-15 02:33:47 +0200

    feat(model): Update the serializer to support the circle

--- front-end/src/lib/pixi-tools-v2/utils/modelBounds.ts
+++ front-end/src/lib/pixi-tools-v2/utils/modelBounds.ts
@@ -1,11 +1,27 @@
+import { Circle } from '../model/template';
 import { ModelGraphics } from '../types/pixi-class';
 
-export const modelBounds = (model: ModelGraphics) => ({
-	uuid: model.uuid,
-	bounds: {
-		x: model.x,
-		y: model.y,
-		width: model.width,
-		height: model.height,
-	},
-});
+export const modelBounds = (model: ModelGraphics) => {
+	if(model instanceof Circle) {
+		return {
+			uuid: model.uuid,
+			bounds: {
+				x: model.x,
+				y: model.y,
+				width: model.width,
+				height: model.height,
+				radius: model.radius
+			},
+		}
+	} else {
+		return {
+			uuid: model.uuid,
+			bounds: {
+				x: model.x,
+				y: model.y,
+				width: model.width,
+				height: model.height,
+			},
+		}
+	}
+};
--- front-end/src/lib/pixi-tools-v2/utils/modelSerializer.ts
+++ front-end/src/lib/pixi-tools-v2/utils/modelSerializer.ts
@@ -1,18 +1,41 @@
+import { Circle } from '../model/template';
 import { ModelGraphics } from '../types/pixi-class';
 
-export const modelSerializer = (model: ModelGraphics) => ({
-	uuid: model.uuid,
-	typeId: model.typeId,
-	bounds: {
-		x: model.x,
-		y: model.y,
-		width: model.width,
-		height: model.height,
-	},
-	properties: {
-		cursor: model.cursor,
-		interactive: model.interactive,
-		color: model.color,
-		alpha: model.alpha,
-	},
-});
+export const modelSerializer = (model: ModelGraphics) => {
+	if(model instanceof Circle) {
+		return {
+			uuid: model.uuid,
+			typeId: model.typeId,
+			bounds: {
+				x: model.x,
+				y: model.y,
+				width: model.width,
+				height: model.height,
+				radius: model.radius,
+			},
+			properties: {
+				cursor: model.cursor,
+				interactive: model.interactive,
+				color: model.color,
+				alpha: model.alpha,
+			},
+		}
+	} else {
+		return {
+			uuid: model.uuid,
+			typeId: model.typeId,
+			bounds: {
+				x: model.x,
+				y: model.y,
+				width: model.width,
+				height: model.height,
+			},
+			properties: {
+				cursor: model.cursor,
+				interactive: model.interactive,
+				color: model.color,
+				alpha: model.alpha,
+			},
+		}
+	}
+};
