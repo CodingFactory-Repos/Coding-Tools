e9b6a63ae3bd18a891cf5892be1b6221c5b27927
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-05-15 02:46:43 +0200

    feat(triangle): Add the triangle to the template

---
+++ front-end/src/lib/pixi-tools-v2/model/model-constructor/triangle.ts
@@ -0,0 +1,54 @@
+import { ModelGraphics } from '../../types/pixi-class';
+import { ElementBounds } from '../../types/pixi-container';
+import { GraphicTypeId, SerializedGraphic } from '../../types/pixi-serialize';
+import { modelBounds } from '../../utils/modelBounds';
+import { modelSerializer } from '../../utils/modelSerializer';
+
+export class Triangle extends ModelGraphics {
+	public readonly uuid: string;
+	public readonly typeId: GraphicTypeId;
+	public cursor: CSSStyleProperty.Cursor;
+	public color: number;
+
+	static registerGraphic(attributes: SerializedGraphic) {
+		return new Triangle(attributes);
+	}
+
+	constructor(attributes: SerializedGraphic) {
+		super();
+
+		const { uuid, typeId, bounds, properties } = attributes;
+
+		this.uuid = uuid;
+		this.typeId = typeId as GraphicTypeId;
+		this.interactive = properties.interactive;
+		this.cursor = properties.cursor;
+		this.color = properties.color;
+		this.alpha = properties.alpha;
+
+		this.draw(bounds);
+	}
+
+	public draw(bounds: ElementBounds) {
+		const { width, height, x, y } = bounds;
+		this.position.set(x, y);
+		this.height = height;
+		this.width = width;
+
+		this.clear();
+		this.beginFill(this.color);
+		this.moveTo(0, height);
+		this.lineTo(width / 2, 0);
+		this.lineTo(width, height);
+		this.lineTo(0, height);
+		this.endFill();
+	}
+
+	public serialized() {
+		return modelSerializer(this);
+	}
+
+	public serializedBounds() {
+		return modelBounds(this);
+	}
+}
