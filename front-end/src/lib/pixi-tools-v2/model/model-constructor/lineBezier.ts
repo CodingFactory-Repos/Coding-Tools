1bfd71c167fe1dfc03f1211389be15cfb98acca1
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-05-18 17:19:36 +0200

    feat(bezier): Implement a better bezier line and add the drawing of an arrow head

--- front-end/src/lib/pixi-tools-v2/model/model-constructor/lineBezier.ts
+++ front-end/src/lib/pixi-tools-v2/model/model-constructor/lineBezier.ts
@@ -1,13 +1,11 @@
-import { Point, Polygon } from 'pixi.js';
+import { LINE_CAP, Polygon } from 'pixi.js';
 import { ModelGraphics } from '../../types/pixi-class';
-// import { ElementBounds } from '../../types/pixi-container';
 import { GraphicTypeId, SerializedGraphic } from '../../types/pixi-serialize';
 import { modelBounds } from '../../utils/modelBounds';
 import { modelSerializer } from '../../utils/modelSerializer';
+import { ElementPosition } from '../../types/pixi-container';
 
-interface XYZ {
-	x: number;
-	y: number;
+interface XYZ extends ElementPosition {
 	z: number;
 }
 
@@ -16,9 +14,10 @@ export class LineBezier extends ModelGraphics {
 	public readonly typeId: GraphicTypeId;
 	public cursor: CSSStyleProperty.Cursor;
 	public color: number;
-	public start: { x: number; y: number };
-	public control: { x: number; y: number };
-	public end: { x: number; y: number };
+	public start: ElementPosition;
+	public end: ElementPosition;
+	public startControl: ElementPosition;
+	public endControl: ElementPosition;
 	public hitArea: Polygon;
 
 	static registerGraphic(attributes: SerializedGraphic) {
@@ -36,35 +35,71 @@ export class LineBezier extends ModelGraphics {
 		this.cursor = properties.cursor;
 		this.color = properties.color;
 		this.alpha = properties.alpha;
-		this.start = { x: 100, y: 100};
-		this.control = { x: 200, y: 50};
-		this.end = { x: 400, y: 300};
+		this.start = bounds.start;
+		this.end = bounds.end;
+		this.startControl = bounds.startControl;
+		this.endControl = bounds.endControl;
 
 		this.draw();
 	}
 
 	public draw() {
 		this.clear();
-		this.lineStyle(2, this.color);
+		this.lineStyle(4, this.color);
+		this.line.cap = LINE_CAP.ROUND;
+
+		const arrowSize = 10; // Adjust this value as needed
+		const angleOffset = Math.PI / 8; // Adjust this value to control the arrowhead shape
 
 		this.moveTo(this.start.x, this.start.y);
-		this.quadraticCurveTo(this.control.x, this.control.y, this.end.x, this.end.y);
+		this.bezierCurveTo(
+			this.startControl.x, this.startControl.y,
+			this.endControl.x, this.endControl.y,
+			this.end.x, this.end.y
+		);
+		this.drawArrowHead(arrowSize, angleOffset);
 
-		this.on("added", () => {
-			const points = this.geometry.points;
+		// generate geometry.points
+		this.getBounds();
+		this.perfectPolygonLine();
+	}
 
-			const od: Array<XYZ> = []
-			const even: Array<XYZ> = []
-			for (let index = 0; index * 2 < points.length; index++) {
-				const x = points[index * 2];
-				const y = points[index * 2 + 1];
-				const z = points[index * 2 + 2];
+	private drawArrowHead(arrowSize: number, angleOffset: number) {
+		const angle = Math.atan2(this.end.y - this.startControl.y, this.end.x - this.startControl.x);
 
-				if (index % 2 === 0) od.push({ x, y, z });
-				else even.push({ x, y, z });
-			}
-			this.hitArea = new Polygon([...od, ...even.reverse()]);
-		})
+		const arrowX = this.end.x;
+		const arrowY = this.end.y;
+
+		// Calculate the coordinates for the two points forming the base of the triangle
+		const baseX1 = arrowX - arrowSize * Math.cos(angle - angleOffset);
+		const baseY1 = arrowY - arrowSize * Math.sin(angle - angleOffset);
+		const baseX2 = arrowX - arrowSize * Math.cos(angle + angleOffset);
+		const baseY2 = arrowY - arrowSize * Math.sin(angle + angleOffset);
+
+		// Draw the triangle shape
+		this.beginFill(this.color);
+		this.moveTo(arrowX, arrowY);
+		this.lineTo(baseX1, baseY1);
+		this.lineTo(baseX2, baseY2);
+		this.lineTo(arrowX, arrowY);
+		this.endFill();
+	}
+
+	private perfectPolygonLine() {
+		const points = this.geometry.points;
+
+		const od: Array<XYZ> = []
+		const even: Array<XYZ> = []
+		// based of this logic : https://www.cnblogs.com/3body/p/14981937.html
+		for (let index = 0; index * 2 < points.length; index++) {
+			const x = points[index * 2];
+			const y = points[index * 2 + 1];
+			const z = points[index * 2 + 2];
+
+			if (index % 2 === 0) od.push({ x, y, z });
+			else even.push({ x, y, z });
+		}
+		this.hitArea = new Polygon([...od, ...even.reverse()]);
 	}
 
 	public serialized() {
