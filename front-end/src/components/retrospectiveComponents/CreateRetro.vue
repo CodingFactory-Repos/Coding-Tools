b11312ce6a18ec08d9b0bfd8f307450fcc927eac
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2023-05-18 12:35:59 +0200

    Style: Change home page style

--- front-end/src/components/retrospectiveComponents/CreateRetro.vue
+++ front-end/src/components/retrospectiveComponents/CreateRetro.vue
@@ -1,11 +1,25 @@
 <template>
-	<div>
-		<button @click="chooseTemplate">Créer une nouvelle rétrospective</button>
-	</div>
-	<div>
-		<Overlay v-model:active="active" :fullSize="false">
-			<ChooseTemplate />
-		</Overlay>
+	<div class="w-full flex flex-col gap-4 h-full justify-start items-start mx-5 mt-16">
+		<h1 class="text-2xl font-bold text-[#5c5f73] dark:text-dark-font">All retrospectives</h1>
+		<div class="w-full flex grow relative items-center justify-center">
+			<div
+				class="flex flex-col items-center justify-center gap-5 p-4 z-10 bg-light-primary dark:bg-dark-tertiary rounded-lg"
+			>
+				<h3 class="text-lg font-bold text-[#5c5f73] dark:text-dark-font text-center">Your saved retros will be shown here in the future.</h3>
+				<DefaultButton
+					type="button"
+					text="Start your first retro here !"
+					background="bg-pink-600"
+					text-style="text-white"
+					@click="chooseTemplate"
+				/>
+			</div>
+		</div>
+		<div class="w-full flex grow gap-3 flex-wrap">
+			<Overlay v-model:active="active" :fullSize="false">
+				<ChooseTemplate />
+			</Overlay>
+		</div>
 	</div>
 </template>
 
@@ -13,9 +27,11 @@
 import { defineComponent, ref } from 'vue';
 import Overlay from '@/components/retrospectiveComponents/utils/Overlay.vue';
 import ChooseTemplate from './ChooseTemplate.vue';
+import DefaultButton from "@/components/common/buttons/Default.vue";
 
 export default defineComponent({
 	components: {
+		DefaultButton,
 		Overlay,
 		ChooseTemplate,
 	},
