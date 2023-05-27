e93d9deda013950b5cc9b318f218e06f37d47b1d
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-05-27 21:20:56 +0200

    feat(serializer): Update the serializer to account for the bezier line

--- front-end/src/lib/pixi-tools-v2/utils/modelBounds.ts
+++ front-end/src/lib/pixi-tools-v2/utils/modelBounds.ts
@@ -1,4 +1,4 @@
-import { Circle } from '../model/template';
+import { Circle, LineBezier } from '../model/template';
 import { ModelGraphics } from '../types/pixi-class';
 
 export const modelBounds = (model: ModelGraphics) => {
@@ -12,6 +12,23 @@ export const modelBounds = (model: ModelGraphics) => {
 				height: model.height,
 				radius: model.radius
 			},
+		}
+	} else if(model instanceof LineBezier) {
+		return {
+			uuid: model.uuid,
+			lineControl: {
+				angleControl: model.angleControl,
+				startControl: model.startControl,
+				endControl: model.endControl,
+				start: model.start,
+				end: model.end,
+			},
+			bounds: {
+				x: model.x,
+				y: model.y,
+				width: model.width,
+				height: model.height,
+			}
 		}
 	} else {
 		return {
--- front-end/src/lib/pixi-tools-v2/utils/modelSerializer.ts
+++ front-end/src/lib/pixi-tools-v2/utils/modelSerializer.ts
@@ -1,4 +1,4 @@
-import { Circle } from '../model/template';
+import { Circle, LineBezier } from '../model/template';
 import { ModelGraphics } from '../types/pixi-class';
 
 export const modelSerializer = (model: ModelGraphics) => {
@@ -12,6 +12,30 @@ export const modelSerializer = (model: ModelGraphics) => {
 				width: model.width,
 				height: model.height,
 				radius: model.radius,
+			},
+			properties: {
+				cursor: model.cursor,
+				interactive: model.interactive,
+				color: model.color,
+				alpha: model.alpha,
+			},
+		}
+	} else if (model instanceof LineBezier) {
+		return {
+			uuid: model.uuid,
+			typeId: model.typeId,
+			lineControl: {
+				angleControl: model.angleControl,
+				startControl: model.startControl,
+				endControl: model.endControl,
+				start: model.start,
+				end: model.end,
+			},
+			bounds: {
+				x: model.x,
+				y: model.y,
+				width: model.width,
+				height: model.height,
 			},
 			properties: {
 				cursor: model.cursor,
