b5852b94da899f2e841f16590a6b1fbd955bd167
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2023-05-29 01:05:55 +0200

    update markdown viewer small styling + code display fixed

--- front-end/src/components/blog/MarkdonwViewer.vue
+++ front-end/src/components/blog/MarkdonwViewer.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="codeLab">
     <input type="file" accept=".md" @change="handleFileChange" />
-    <div v-if="fileContent !== ''" v-html="renderedMarkdown"></div>
+    <!-- <div v-if="fileContent !== ''" v-html="renderedMarkdown"></div> -->
+    <div v-for="(data, index) in resultArray" :key="index">
+      <div v-for="(line, row) in data" :key="row">
+        <div v-html="line" class="htmlData"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +20,9 @@ export default {
     return {
       fileContent: '',
       renderedMarkdown: '',
+      resultArray: [],
+      index: 0,
+      headers: [],
     };
   },
   methods: {
@@ -43,6 +51,44 @@ export default {
       });
 
       this.renderedMarkdown = md.render(this.fileContent);
+
+      let lines = this.renderedMarkdown.split('\n')
+
+      this.resultArray.push([])
+
+      let code = '';
+      let inCodeBlock = false
+
+      lines.forEach(line => {
+
+        if (line.includes('h2')) {
+          this.resultArray.push([]);
+          this.index++;
+          this.resultArray[this.index].push(line)
+          this.headers.push(line)
+        }
+        else if (line.includes('pre') || inCodeBlock) {
+          if (!inCodeBlock) {
+            inCodeBlock = true
+            code += line+'\n'
+          }
+          else if (line.includes('pre') && inCodeBlock) {
+            inCodeBlock = false
+            code += line
+
+            this.resultArray[this.index].push(code)
+            code = ''
+          }
+          else {
+            code += line+'\n'
+          }
+        }
+        else
+          this.resultArray[this.index].push(line)
+
+      });
+
+      console.log(this.headers)
     },
   },
 };
@@ -54,6 +100,12 @@ export default {
   .codeLab{
     max-width: 60%;
     margin: 0 auto;
-    line-height: 1.5em;
+    font-size: 17px;
+  }
+  
+  .htmlData h2 {
+    line-height: 2em;
+
+    font-size: 40px;
   }
 </style>
