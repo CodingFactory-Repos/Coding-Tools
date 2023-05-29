d55f46d66cb78835385af03b4f1ae62c24aea03d
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-05-29 21:47:00 +0200

    feat(mailjet): Add the mailjet event for sending the invitation through an email

---
+++ back-end/src/base/canvasRoom/events/canvasRoom.events.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+
+import { Events } from '@/common/providers/interfaces/events.interface';
+import { MailjetCanvasInvitationRequest } from '@/base/canvasRoom/events/canvasRoom.events.req';
+
+@Injectable()
+export class CanvasRoomEventEmitter {
+	constructor(private eventEmitter: EventEmitter2) {}
+
+	async invitationRequest(email: string, senderFirstName: string, senderLastName: string, projectTitle: string, token: string) {
+		this.eventEmitter.emit(Events.canvasInvitationRequest, new MailjetCanvasInvitationRequest(
+			email,
+			senderFirstName,
+			senderLastName,
+			projectTitle,
+			token,
+		));
+	}
+}
