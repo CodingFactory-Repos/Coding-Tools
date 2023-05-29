dfa27a2cf71c28607e3b7a2b3004da047eb30040
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2023-05-29 15:20:07 +0200

    wip styling + adde sidebar

--- front-end/src/components/blog/MarkdonwViewer.vue
+++ front-end/src/components/blog/MarkdonwViewer.vue
@@ -2,17 +2,72 @@
 
   <div class="codeLab">
     <input type="file" accept=".md" @change="handleFileChange" />
+
+
     <!-- <div v-if="fileContent !== ''" v-html="renderedMarkdown"></div> -->
-    <div v-for="(data, index) in resultArray" :key="index">
-      <div v-for="(line, row) in data" :key="row">
-        <div v-html="line" class="htmlData"></div>
-      </div>
+    <div v-if="resultArray.length>0" class="mb-2 tracking-tight text-gray-900 dark:text-white">
+
+		<!-- STICKY BAR -->
+		<div tabindex="-1" class="fixed top-0 left-0 z-0 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600 block">
+				<div class="flex items-center mx-auto">
+					<p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+						<h2>{{ title }}</h2>
+				</p>
+				<button 
+					@click.prevent="toggle()" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+					<span class="sr-only">Open sidebar</span>
+
+					<svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+						<path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+					</svg>
+				</button>
+
+				<!-- SIDEBAR -->
+				<aside v-if="open" id="codelab-sidebar" class="fixed inset-y-0 top-0 right-0 z-10 flex-shrink-0 bg-white xl:z-0 xl:sticky w-80 dark:bg-darker dark:text-light xl:border-l dark:border-indigo-800 focus:outline-none" aria-label="Sidebar">
+					<div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 transition-transform">
+						<div class="absolute right-0 p-1 transform -translate-x-full">
+							<button
+							@click.prevent="toggle()"
+							class="p-2 rounded-md text-dark dark:text-light focus:outline-none focus:ring"
+							>
+								<svg
+									class="w-5 h-5"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+								</svg>
+							</button>
+						</div>
+						<ul class="space-y-2 font-medium">
+							<li v-for="(item, index) in headers" :key="index">
+								<a @click.prevent="setStep(index)" :class="step == index ? 'light:bg-gray-100 dark:bg-gray-700 block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' : 'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+									<h2 class="ml-3">{{ index+1 }}. {{ item }}</h2>
+								</a>
+							</li> 
+						</ul>
+					</div>
+				</aside>
+			</div>
+		</div>
+
+		<!-- CONTENT HERE -->
+		<div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">				
+			<div v-for="(data, index) in resultArray[step]" :key="index">
+				<div v-for="(line, row) in data" :key="row">
+					<div v-html="line" class="htmlData"></div>
+				</div>
+			</div>
+		</div>
     </div>
+
   </div>
 
   <div>
     <button v-if="step>0" @click="step = step -= 1" class="backBtn py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white ">Back</button>
-    <button v-if="step<resultArray.length-1" @click="step=step=step+=1" class="nextBtn text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none">Next</button>
+    <button v-if="step<resultArray.length-2" @click="step=step=step+=1" class="nextBtn text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none">Next</button>
   </div>
   
 </template>
@@ -23,17 +78,42 @@ import hljs from 'highlight.js';
 
 export default {
   name: 'MarkdownViewer',
+  setup(){
+	// watchScreen() {
+    //         if (window.innerWidth <= 768) {
+    //             this.isUserPanelOpen = false
+    //         } else if (window.innerWidth >= 768 && window.innerWidth < 1280) {
+    //             this.isUserPanelOpen = false
+    //         } else if (window.innerWidth >= 1280) {
+    //             this.isUserPanelOpen = true
+    //         }
+    //     },
+
+  },
   data() {
     return {
-      fileContent: '',
-      renderedMarkdown: '',
-      resultArray: [],
-      index: 0,
-      headers: [],
-      step: 0,
+	isUserPanelOpen: false,
+		open: false,
+		fileContent: '',
+		renderedMarkdown: '',
+		resultArray: [],
+		index: 0,
+		headers: [],
+		step: 0,
+		title: '',
+		right: true,
+		firstSection: true,
     };
   },
   methods: {
+    toggle() {
+		console.log(this.resultArray)
+			this.open = !this.open;
+		},
+	setStep (i) {
+		console.log(i)
+		this.step = i;
+	},
     handleFileChange(event) {
       const file = event.target.files[0];
       if (file) {
@@ -44,6 +124,11 @@ export default {
         };
         reader.readAsText(file);
       }
+    },
+    getSubstring(str, start, end) {
+      let char1 = str.indexOf(start) + 1
+      let char2 = str.lastIndexOf(end)
+      return str.substring(char1, char2)
     },
     renderMarkdown() {
       const md = new MarkdownIt({
@@ -69,11 +154,22 @@ export default {
 
       lines.forEach(line => {
 
-        if (line.includes('h2')) {
-          this.resultArray.push([]);
-          this.index++;
-          this.resultArray[this.index].push(line)
-          this.headers.push(line)
+        if (line.includes('h1')) {
+          this.title = this.getSubstring(line, '>', '<')
+          this.resultArray[this.index].push({value: line})
+        }
+        else if (line.includes('h2')) {
+			if (this.firstSection) {
+				this.firstSection = false
+				this.resultArray[this.index].push({value: line})
+				this.headers.push(this.getSubstring(line, '>', '<'))
+			}
+			else {
+				this.resultArray.push([]);
+				this.index++;
+				this.resultArray[this.index].push({value: line})
+				this.headers.push(this.getSubstring(line, '>', '<'))
+			}
         }
         else if (line.includes('pre') || inCodeBlock) {
           if (!inCodeBlock) {
@@ -84,7 +180,7 @@ export default {
             inCodeBlock = false
             code += line
 
-            this.resultArray[this.index].push(code)
+            this.resultArray[this.index].push({value: code})
             code = ''
           }
           else {
@@ -92,12 +188,18 @@ export default {
           }
         }
         else
-          this.resultArray[this.index].push(line)
+          this.resultArray[this.index].push({value: line})
 
       });
 
-      console.log(this.headers)
+      // console.log(this.headers)
     },
+	openUserPanel() {
+            this.isUserPanelOpen = true
+            this.$nextTick(() => {
+                this.$refs.userPanel.focus()
+            })
+        },
   },
 };
 </script>
@@ -105,27 +207,31 @@ export default {
 <style lang="scss">
   @import 'node_modules/highlight.js/styles/atom-one-dark.css';
 
-  .codeLab{
-    max-width: 60%;
-    margin: 0 auto;
-    font-size: 17px;
-  }
+//   .codeLab{
+//     max-width: 60%;
+//     margin: 0 auto;
+//     font-size: 17px;
+//   }
   
   .htmlData h2 {
     line-height: 2em;
 
-    font-size: 40px;
+    font-size: 20px;
   }
 
   .backBtn {
     position: fixed;
-    bottom: 10%;
+    bottom: 5%;
     left: 16%;
   }
 
   .nextBtn {
     position: fixed;
-    bottom: 10%;
+    bottom: 5%;
     right: 10%;
+  }
+
+  .selected {
+	border: 3px grey;
   }
 </style>
