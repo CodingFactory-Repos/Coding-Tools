ff0b3cad6b294c233716e9b12cbf6a13e3cc447d
Kevw94 <pinerokevin@icloud.com>
2023-05-30 20:41:47 +0200

    Add alert when user connect or disconnect from the retro

--- front-end/src/composables/useSocketRetro.ts
+++ front-end/src/composables/useSocketRetro.ts
@@ -2,11 +2,21 @@ import { manager } from '@/api/network/socket.io';
 import { Postit, Postits } from '@/store/interfaces/retrospective.interface';
 import { useRetrospectiveStore } from '@/store/retrospective.store';
 import { Socket } from 'socket.io-client';
+import Swal from 'sweetalert2';
 
 function handleSocketEvents(socket: Socket) {
 	const retrospectiveStore = useRetrospectiveStore();
 
 	socket.on("peer-connected", (email) => {
+		Swal.fire({
+			position: 'top-end',
+			text: `${email} has joined`,
+			showConfirmButton: false,
+			timer: 1500,
+			color: '#40c060',
+			padding: '0em',
+			toast: true
+		})
 		retrospectiveStore.participantJoin(email);
 	})
 
@@ -26,7 +36,16 @@ function handleSocketEvents(socket: Socket) {
 		retrospectiveStore.updateUserCursor(userCursor);
 	});
 
-	socket.on('peer-disconnected', (user) => {
+	socket.on("peer-disconnected", (user) => {
+		Swal.fire({
+			position: 'top-end',
+			text: `${user.email} has left`,
+			showConfirmButton: false,
+			timer: 1500,
+			color: '#d33',
+			padding: '0em',
+			toast: true
+		})
 		retrospectiveStore.removeCursor(user);
 		retrospectiveStore.participantLeave(user);
 	});
