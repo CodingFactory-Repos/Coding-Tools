59697d77a68e473a7630b571d38cddcdef373dd1
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-05-31 11:01:58 +0200

    feat(models): Condition the drawing of the border on a model element

--- front-end/src/lib/pixi-tools-v2/model/model-constructor/rectangle.ts
+++ front-end/src/lib/pixi-tools-v2/model/model-constructor/rectangle.ts
@@ -9,6 +9,8 @@ export class Rectangle extends ModelGraphics {
 	public readonly uuid: string;
 	public readonly typeId: GraphicTypeId;
 	public cursor: CSSStyleProperty.Cursor;
+	public borderWidth: number;
+	public borderColor: number;
 	public color: number;
 
 	static registerGraphic(attributes: SerializedGraphic) {
@@ -23,6 +25,8 @@ export class Rectangle extends ModelGraphics {
 		this.uuid = uuid;
 		this.typeId = typeId as GraphicTypeId;
 		this.interactive = properties.interactive;
+		this.borderWidth = properties.borderWidth;
+		this.borderColor = properties.borderColor;
 		this.cursor = properties.cursor;
 		this.color = properties.color;
 		this.alpha = properties.alpha;
@@ -37,9 +41,12 @@ export class Rectangle extends ModelGraphics {
 		this.width = width;
 
 		this.clear();
+		if(this.borderWidth > 0)
+			this.lineStyle(this.borderWidth, this.borderColor, 1);
 		this.beginFill(this.color);
 		this.drawRect(0, 0, width, height);
 		this.endFill();
+
 	}
 
 	public serialized() {
