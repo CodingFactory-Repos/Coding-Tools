fc58e6a4ecd75ac10e697f56cc769d9e3e043884
ThomasCast3 <91255859+ThomasCast3@users.noreply.github.com>
2023-06-02 18:03:42 +0200

    jpo form search participant part1

--- front-end/src/components/RessourcesComponents/NewParticipant.vue
+++ front-end/src/components/RessourcesComponents/NewParticipant.vue
@@ -1,47 +1,75 @@
 <template>
     <div>
-        <button type="button" @click="showNewParticipant">add participant</button>
+        <button type="button" @click="showNewParticipant">add participant</button>    <!--btn afficher gestion d'ajout-->
         <div v-show="showParticipant">
-            <input  type="text"  placeholder="first name :" v-model="firstName">
-				<input  type="text"  placeholder="last name :" v-model="lastName">
-				<button type="button" @click="addParticipants(); showNewParticipant()">Ok</button> <br>
+            <input  type="text"  placeholder="name :" v-model="name" @input="searchUser">       <!--input = name et lance recherche de correspondance-->
+				<button type="button" @click="addParticipants(); showNewParticipant()">Ok</button> <br>     <!--ajout d'un new participant et fermer div d'ajout-->
         </div>
-        <p v-for="user in participants">{{ user.name }}</p><br>
+        <p v-for="user in participants">{{ user.name }}</p><br>  <!--afficher les participants ajouter-->
     </div>
 </template>
 
 <script lang="ts">
-
+import {http} from "@/api/network/axios"
 export default{
     data(){
         return{
         firstName : "",
-		lastName : "", 
+		lastName : "",
+        name : "",
+        usersNames: [],
+        userName : {firstName: "", lastName: ""},
         showParticipant : false,
         }
     },
     props : {
-        participants : {
+        participants : {        //props participants a envoyer pour le form
             type : Array<{name : string}>,
             required : true, 
         }
     },
     methods:{
-        showNewParticipant(){
+        showNewParticipant(){   //gestion d'affichage d'ajout
             this.showParticipant = !this.showParticipant;
         },
 
-        addParticipants(){
-            let name = "";
+        addParticipants(){    
 			let participant = {name : ""};
-			if(this.lastName != "" && this.firstName != ""){
-				name  =  this.firstName + " " + this.lastName;
-                participant = { name : name};
+			if(this.name != ""){   // si le participant ajouter est pas nul: 
+                participant = { name : this.name};
 				this.participants.push(participant);
-				this.firstName = "";
-				this.lastName = "";
+				this.name = "";
 			}
 		},
+        searchUser(){    //recherche de participant
+            this.getUsers();  // recuperer les users de la bdd
+            this.usersNames.forEach(element =>{
+                console.log(element);
+            })
+         
+        },
+        getUsers() {
+		    http.get('http://localhost:8010/openhouses/users').then((response) => {
+			    this.users = response.data;
+                this.users.forEach(element => {
+                    this.userName = {firstName: element.profile.firstName, lastName: element.profile.lastName};
+                    if(!this.userCheckList()){
+                        this.usersNames.push(this.userName);
+                    }
+                });
+		    });
+        },
+        userCheckList(){
+            let alreadyInList = false;
+            this.usersNames.forEach(element => {
+                if(element.lastName == this.userName.lastName){
+                    if(element.firstName == this.userName.firstName){
+                        alreadyInList = true;
+                    }
+                }
+            });
+            return alreadyInList;
+        },
     },
 }
 </script>
--- front-end/src/components/RessourcesComponents/OpenHouses.vue
+++ front-end/src/components/RessourcesComponents/OpenHouses.vue
@@ -15,9 +15,7 @@ export default {
             /* list of all open houses */
 			openHouses: [],
             openForm : false,
-
-            usersNames: [],
-            userName : {firstName: "", lastName: ""},
+           
 
         }   
     }, 
@@ -25,34 +23,12 @@ export default {
         FormOpenHouse
 	},
     methods : {
-        getUsers() {
-          //  this.openForm=!this.openForm
-            
-		    http.get('http://localhost:8010/openhouses/users').then((response) => {
-			    this.users = response.data;
-                this.users.forEach(element => {
-                    this.userName = {firstName: element.profile.firstName, lastName: element.profile.lastName};
-                    if(!this.userCheckList()){
-                        this.usersNames.push(this.userName);
-                    }
-                });
-		    });
-        },
+       
 
-        userCheckList(){
-            let alreadyInList = false;
-            this.usersNames.forEach(element => {
-                if(element.lastName == this.userName.lastName){
-                    if(element.firstName == this.userName.firstName){
-                        alreadyInList = true;
-                    }
-                }
-            });
-            return alreadyInList;
-        },
+        
 
-        async mounted() {
-            await http.get('http://localhost:8010/openhouses').then((response) => {
+         mounted() {
+             http.get('http://localhost:8010/openhouses').then((response) => {
                 console.log(response);
                 this.openHouses = response.data;
                 console.log(this.openHouses);
