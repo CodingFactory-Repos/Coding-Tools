695c730fb0deb1ad718b5d7a7054eeb65dcef279
Iagomorph <philemon.wild@gmail.com>
2023-06-04 14:40:52 +0200

    fix(chat): switch ecosystem to js

--- back-end/ecosystem.config.js
+++ back-end/ecosystem.config.js
@@ -52,7 +52,7 @@ module.exports = {
 			host: '0.0.0.0',
 			ref: 'origin/master',
 			repo: 'git@github.com:repo.git',
-			path: 'dist/server.ts',
+			path: 'dist/server.js',
 			'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --only prod',
 		},
 	},
