9e7d3f7e97a75dd0f70dd9a04a4c22b2117a19ee
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2023-06-04 12:27:17 +0200

    cleaning

--- front-end/src/components/blog/MarkdownViewer.vue
+++ front-end/src/components/blog/MarkdownViewer.vue
@@ -1,47 +1,44 @@
 <template>
 
   <div class="codeLab text-gray-900 dark:text-white">
-    <input type="file" accept=".md" @change="handleFileChange" />
 
-
-    <!-- <div v-if="fileContent !== ''" v-html="renderedMarkdown"></div> -->
     <div v-if="markdown.length>0" class=" mb-2 tracking-tight text-gray-900 dark:text-white">
 
-		<!-- STICKY BAR -->
-		<div tabindex="-1" class="relative top-0 left-0 z-0 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600 block">
-				<div class="flex items-center mx-auto">
-					<p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-						<h2>{{ title }}</h2>
-				</p>
-				<button 
-					@click.prevent="toggle()" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-					<span class="sr-only">Open sidebar</span>
+      <!-- STICKY BAR -->
+      <div tabindex="-1" class="relative top-0 left-0 z-0 flex justify-between w-full p-4 border-b border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600 block">
+          <div class="flex items-center mx-auto">
+            <p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+              <h2>{{ title }}</h2>
+          </p>
+          <button 
+            @click.prevent="toggle()" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <span class="sr-only">Open sidebar</span>
 
-					<svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-						<path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-					</svg>
-				</button>
+            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+            </svg>
+          </button>
 
 
-			</div>
-		</div>
+        </div>
+      </div>
 
-		<!-- CONTENT HERE -->
-		<div class="flex flex-col items-center">
-			<div v-if="step===0" class="mt-12 block max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">				
-				<h2>About this coding tutorial</h2>
-				<div>Last updated : </div>
-				<div>Redacted by : </div>
-			</div>
+      <!-- CONTENT HERE -->
+      <div class="flex flex-col items-center">
+        <div v-if="step===0" class="mt-12 block max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">				
+          <h2>About this coding tutorial</h2>
+          <div>Last updated : </div>
+          <div>Redacted by : </div>
+        </div>
 
-			<div class="block max-w-xl p-6 mt-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">				
-				<div v-for="(data, index) in markdown[step]" :key="index">
-					<div v-for="(line, row) in data" :key="row">
-						<div v-html="line" class="htmlData"></div>
-					</div>
-				</div>
-			</div>
-		</div>
+        <div class="block max-w-xl p-6 mt-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">				
+          <div v-for="(data, index) in markdown[step]" :key="index">
+            <div v-for="(line, row) in data" :key="row">
+              <div v-html="line" class="htmlData"></div>
+            </div>
+          </div>
+        </div>
+      </div>
 		
     </div>
   </div>
@@ -120,7 +117,6 @@ export default {
     setStep (i) {
       this.step = i;
     },
-    // handleFileChange(event) {
     //   const file = event.target.files[0];
     //   if (file) {
     //     const reader = new FileReader();
@@ -206,14 +202,15 @@ export default {
 
     //   // console.log(this.resultArray)
     // },
-	openUserPanel() {
-            this.isUserPanelOpen = true
-            this.$nextTick(() => {
-                this.$refs.userPanel.focus()
-            })
-        },
-  },
-};
+    openUserPanel() {
+              this.isUserPanelOpen = true
+              this.$nextTick(() => {
+                  this.$refs.userPanel.focus()
+              })
+          },
+    },
+  }
+;
 </script>
 
 <style lang="scss">
