033df63d64c19d022086e8dcab98feeef684f41d
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-06-04 02:15:50 +0200

    feat(blueprint): Create the two functions for the elevator pitch and personas

---
+++ front-end/src/lib/pixi-tools-v2/blueprint/elevatorPitch.ts
@@ -0,0 +1,51 @@
+import { Point } from 'pixi.js';
+import { FramedContainer } from '../class/framedContainer';
+import { lowestNumberFinder } from '../utils/numberFinder';
+import { ViewportUI } from '../viewportUI';
+import { SerializedContainer } from '../types/pixi-serialize';
+import { PixiEventMode } from '../types/pixi-enums';
+
+export const elavatorPitch = (
+	viewport: ViewportUI,
+	point: Point,
+	width: number,
+	height: number,
+): Partial<SerializedContainer> => {
+	const allFrames = viewport.children.filter((ctn) => ctn instanceof FramedContainer);
+	const frameNumbers = allFrames.map((frame) => frame.frameNumber);
+	const frameNumber = lowestNumberFinder(frameNumbers);
+
+	const centerX = width / 2;
+
+	return {
+		typeId: 'frame',
+		background: {
+			typeId: 'framebox',
+			properties: {
+				cursor: 'pointer',
+				eventMode: PixiEventMode.STATIC,
+				color: 0xffffff,
+				alpha: 1,
+				borderWidth: 0,
+				borderColor: 0x000000,
+			},
+			bounds: {
+				x: point.x - centerX,
+				y: point.y - height / 2,
+				width,
+				height,
+			},
+		},
+		properties: {
+			cursor: 'pointer',
+			eventMode: PixiEventMode.STATIC,
+			tabNumberContext: -1,
+			isAttachedToFrame: false,
+			frameNumber: frameNumber,
+			disabled: false,
+		},
+		childs: [
+
+		]
+	}
+}
