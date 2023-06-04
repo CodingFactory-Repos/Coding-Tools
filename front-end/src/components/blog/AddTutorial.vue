5ba025297d5453c2128ee636801b8dcf6a5b0185
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2023-06-04 12:01:58 +0200

    add tutorial component wip

--- front-end/src/components/blog/AddTutorial.vue
+++ front-end/src/components/blog/AddTutorial.vue
@@ -1,5 +1,6 @@
 <template>
-	<div class="text-center max-w-full w-4/5 m-auto h-full">
+    <!-- <div id="createTutorialContainer"> -->
+        <div class="text-center max-w-full w-4/5 m-auto h-full">
 		<h2 class="text-3xl font-bold pt-5 text-gray-900">Create Tutorial</h2>
 		<form @submit.prevent="addArticle">
 			<div class="grid gap-6 mb-6 md:grid-cols-2 justify-items-center">
@@ -21,34 +22,42 @@
 					<label
 						for="countries"
 						class="block mb-2 text-lg font-medium text-gray-900 dark:text-white"
-						>Import your .md file below !</label
+						>Import your .md file</label
 					>
 					<div class="relative mb-6">
-                        <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
+                        <input type="file" accept=".md" @change="handleFileChange" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input">
 					</div>
 				</div>
 			</div>
 
-			<label for="title" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white"
+            <label for="title" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white"
 				>Description</label
 			>
 			<div class="mb-6 relative z-10">
 				<div class="relative mb-6 flex">
-					<mavon-editor
+					<input
+                        type="text"
 						language="fr"
 						class="p-2.5 w-full text-sm z-1 text-gray-900 bg-white rounded-l-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						v-model="descriptions"
 					/>
 				</div>
 			</div>
-			<div class="h-10 mb-6">
-				<datepicker
-					v-model="date"
-					:full-month-name="true"
-					placeholder="YYYY-MM-DD"
-					:typeable="true"
-					class="vuejs3-datepicker w-[300px] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block pl-4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-				/>
+
+			<label for="title" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white"
+				>Content</label
+			>
+			<div class="mb-6 relative z-10">
+				<div class="relative mb-6 flex">
+					<mavon-editor
+                        v-model="content"
+                        :toolbars="mavonOptions"
+						language="fr"
+						class="p-2.5 w-full text-sm z-1 text-gray-900 bg-white rounded-l-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+
+                        defaultOpen="true"
+					/>
+				</div>
 			</div>
 
 			<div class="grid gap-6 mb-6 md:grid-cols-2 justify-items-center">
@@ -82,8 +91,6 @@
 				</div>
 			</div>
 
-            
-
 			<div>
 				<button
 					type="submit"
@@ -95,21 +102,25 @@
 		</form>
 	</div>
     <div>
-                <MarkdownViewer></MarkdownViewer>
-            </div>
+        <MarkdownViewer :markdown="formatedArray" v-bind="formatedArray"></MarkdownViewer>
+    </div>
+    <!-- </div> -->
+
 </template>
 
 <script lang="ts" setup>
 // Post the data to the API
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { useArticleStore } from '@/store/modules/article.store';
 import { useAuthStore } from '@/store/modules/auth.store';
-import datepicker from 'vuejs3-datepicker';
 import Swal from 'sweetalert2';
 import MarkdownViewer from './MarkdownViewer.vue'
+import MarkdownIt from 'markdown-it';
+import hljs from 'highlight.js';
 
 // use router
 import { useRouter } from 'vue-router';
+import { result } from 'lodash-es';
 const router = useRouter();
 
 // use the store
@@ -119,13 +130,53 @@ const authStore = useAuthStore();
 // form data
 const title = ref('');
 const picture = ref('');
-const descriptions = ref('...');
+const descriptions = ref('');
+const content = ref()
 const tags = ref('');
 const type = ref('');
 const date = ref(new Date());
 
-const onFilePicked = () => {
-    console.log("test")
+// page functionnality
+const formatedArray = ref([[]]);
+const firstSection = ref(true);
+const fileContent = ref()
+const renderedMarkdown = ref()
+const indexArray = ref(0)
+const headers = ref([])
+const mavonOptions = ref({
+    bold: true,
+    italic: true,
+    header: true,
+    underline: true,
+    strikethrough: true,
+    mark: true,
+    superscript: true,
+    subscript: true,
+    quote: true,
+    ol: true,
+    ul: true,
+    link: true,
+    imagelink: true,
+    code: true,
+    table: true,
+})
+
+watch(content, (newContent) => {
+    content.value = newContent
+    renderMarkdown()
+})
+
+const handleFileChange = (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = () => {
+            fileContent.value = reader.result;
+            content.value = reader.result
+        };
+        reader.readAsText(file);
+    }
 }
 
 // Function to post the data to the API
@@ -144,12 +195,13 @@ const addArticle = async () => {
 	}
 
 	let data = {
-		// owner: ObjectId(authStore.user._id),
+		owner: authStore.user._id,
 		title: title.value,
 		descriptions: descriptions.value,
+        content: content.value,
 		picture: picture.value,
 		tags: tags.value,
-		type: type.value,
+		type: 'tutos',
 		date: date.value.toString(),
 	};
 
@@ -162,7 +214,7 @@ const addArticle = async () => {
 	}).then(async (result) => {
 		if (result.isConfirmed) {
 			// post the data
-			// await articleStore.addArticle(data);
+			await articleStore.addArticle(data);
 			// redirect to the article page
 			router.push('/app/blog');
 		}
@@ -170,12 +222,87 @@ const addArticle = async () => {
 
 	//reset the form
 	title.value = '';
-	descriptions.value = '...';
+	descriptions.value = '';
+    content.value = '';
 	picture.value = '';
 	tags.value = '';
 	type.value = '';
 	date.value = new Date();
 };
+
+const renderMarkdown = () => {
+    
+    const md = new MarkdownIt({
+        highlight(code, lang) {
+            let highlightedCode = code;
+
+            if(hljs.getLanguage(lang)){
+                highlightedCode = hljs.highlight(code, { language: lang, ignoreIllegals: true }).value
+            }
+
+            return `<pre class='hljs overflow-x-scroll'><code class="${lang}">${highlightedCode}</code></pre>`
+        },
+    });
+
+    renderedMarkdown.value = md.render(content.value);
+
+    let lines = renderedMarkdown.value.split('\n')
+
+    formatedArray.value = [[]]
+
+      let code = '';
+      let inCodeBlock = false
+      firstSection.value = true
+      indexArray.value = 0
+
+      lines.forEach(line => {
+
+        if (line.startsWith('<h1>')) {
+          formatedArray.value[indexArray.value].push({value: line})
+        }
+        else if (line.startsWith('<h2>')) {
+          if (firstSection.value == true) {
+            firstSection.value = false
+            formatedArray.value[indexArray.value].push({value: line})
+            headers.value.push(getSubstring(line, '>', '<'))
+          }
+          else {
+            formatedArray.value.push([]);
+            indexArray.value++;
+            formatedArray.value[indexArray.value].push({value: line})
+            headers.value.push(getSubstring(line, '>', '<'))
+          }
+        }
+        else if (line.includes("<pre class='hljs overflow-x-scroll'>") || inCodeBlock) {
+
+          if (!inCodeBlock) {
+            inCodeBlock = true
+            code += line+'\n'
+          }
+
+          else if (line.includes('</pre>')) {
+            inCodeBlock = false
+            code += line
+
+            formatedArray.value[indexArray.value].push({value: code})
+            code = ''
+          }
+          else {
+            code += line+'\n'
+          }
+        }
+        else {
+          formatedArray.value[indexArray.value].push({value: line})
+        }
+
+      });
+    }
+
+const getSubstring = (str, start, end) => {
+      let char1 = str.indexOf(start) + 1
+      let char2 = str.lastIndexOf(end+1)
+      return str.substring(char1, char2)
+}
 </script>
 
 <style scoped>
