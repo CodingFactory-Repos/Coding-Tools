b0bed51b323ec51d17a0c118ccb04d0edc681ef1
Héloïse Le Lez <lelez.heloise@gmail.com>
2023-06-05 09:52:21 +0200

    change title in eng to fr

--- front-end/src/views/MaterialDashboardView.vue
+++ front-end/src/views/MaterialDashboardView.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="py-8">
-		<h1 class="text-dark-primary dark:text-light-primary text-center">Inventory Dashboard</h1>
+		<h1 class="text-dark-primary dark:text-light-primary text-center">Dashboard de l'inventaire</h1>
 		<div class="grid grid-cols-3 gap-6 py-10 px-20">
 			<DataOlderMaterials ref="bar" />
 			<DataNbMaterials ref="doughnut" />
