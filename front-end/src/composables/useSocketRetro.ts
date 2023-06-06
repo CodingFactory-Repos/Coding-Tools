691f7cc6445290751750f6235ecd114296a0afc5
AyakorK <guillaume.moret@yahoo.com>
2023-06-06 15:05:50 +0200

    Run lint everywhere

--- front-end/src/composables/useSocketRetro.ts
+++ front-end/src/composables/useSocketRetro.ts
@@ -79,10 +79,9 @@ function handleSocketEvents(socket: Socket) {
 
 	// @@@@@@@@@@@ END TIMER SECTION @@@@@@@@@
 
-	socket.on("update-visibility", (postits: Postits) => {
-		retrospectiveStore.setSocketVisibility(postits)
-	})
-
+	socket.on('update-visibility', (postits: Postits) => {
+		retrospectiveStore.setSocketVisibility(postits);
+	});
 }
 
 export const socketRetro: { socket: Socket } = {
