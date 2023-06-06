0c260ae6c8007c8e61f7065280bf663bc14663e8
AyakorK <guillaume.moret@yahoo.com>
2023-06-06 15:03:59 +0200

    Add a return button to fluidify navigation

---
+++ front-end/src/components/RollCallComponents/ReturnButtonComponent.vue
@@ -0,0 +1,11 @@
+<template>
+	<button class="bg-blue-500 text-white py-2 px-4 rounded mt-2 mb-4 max-w-128 absolute top-2 left-16" @click="goBack">
+		Retour en arrière
+	</button>
+</template>
+
+<script setup>
+const goBack = () => {
+	window.history.back();
+};
+</script>
--- front-end/src/views/calls/GroupsView.vue
+++ front-end/src/views/calls/GroupsView.vue
@@ -2,9 +2,11 @@
 import StudentListComponent from '@/components/RollCallComponents/StudentListComponent.vue';
 import GroupsComponent from '@/components/RollCallComponents/GroupsComponent.vue';
 import Chat from '@/components/RollCallComponents/Chat.vue';
+import ReturnButtonComponent from "@/components/RollCallComponents/ReturnButtonComponent.vue";
 
 export default {
 	components: {
+		ReturnButtonComponent,
 		Chat,
 		StudentListComponent,
 		GroupsComponent,
@@ -19,6 +21,7 @@ export default {
 // and pass it to the QrCode component
 </script>
 <template>
+	<ReturnButtonComponent />
 	<StudentListComponent />
 	<GroupsComponent />
 	<Chat room-id="1" />
