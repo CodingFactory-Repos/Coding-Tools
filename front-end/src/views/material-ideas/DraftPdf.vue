0a67a11828bf28cbee49ddeecb33da0f39f7fcf8
Héloïse Le Lez <lelez.heloise@gmail.com>
2023-06-07 09:24:06 +0200

    style

--- front-end/src/views/material-ideas/DraftPdf.vue
+++ front-end/src/views/material-ideas/DraftPdf.vue
@@ -1,11 +1,11 @@
 <template>
-	<button class="rounded-lg pt-7 flex justify-around" @click="generatePdf">
+	<button class="rounded-lg pt-7 flex justify-center items-center" @click="generatePdf">
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="16"
 			height="16"
 			fill="currentColor"
-			class=""
+			class="mr-2"
 			viewBox="0 0 16 16"
 		>
 			<path
@@ -15,7 +15,7 @@
 				d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"
 			/>
 		</svg>
-		<p>Télécharcher un draft de devis</p>
+		<p class="ml-2">Télécharcher un draft de devis</p>
 	</button>
 </template>
 
@@ -120,33 +120,3 @@ export default {
 	},
 };
 </script>
-<!-- [
-								[
-									{
-										text: 'Nom',
-										margin: 5,
-									},
-									{
-										text: 'Description',
-										margin: 5,
-									},
-									{
-										text: 'Prix',
-										margin: 5,
-									},
-								],
-								[
-									{
-										text: this.item.title,
-										margin: 5,
-									},
-									{
-										text: this.item.desc,
-										margin: 5,
-									},
-									{
-										text: this.item.price,
-										margin: 5,
-									},
-								],
-							], -->
