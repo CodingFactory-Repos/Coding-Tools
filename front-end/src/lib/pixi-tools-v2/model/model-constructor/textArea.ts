3c228cba221d37ea54d9b927545cd6f232f1d1aa
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-06-07 10:42:48 +0200

    feat(model): Refacto the text-area model to split the logic

--- front-end/src/lib/pixi-tools-v2/model/model-constructor/textArea.ts
+++ front-end/src/lib/pixi-tools-v2/model/model-constructor/textArea.ts
@@ -1,133 +1,86 @@
 
+import { TextStyle, Text } from 'pixi.js';
 import { ModelGraphics } from '../../types/pixi-class';
 import { ElementBounds } from '../../types/pixi-container';
-import { GraphicTypeId, SerializedGraphic, SerializedGraphicBounds, SerializedGraphicColorimetry } from '../../types/pixi-serialize';
+import { GraphicTypeId, SerializedGraphic } from '../../types/pixi-serialize';
 import { modelBounds } from '../../utils/modelBounds';
 import { modelColorimetry } from '../../utils/modelColorimetry';
 import { modelSerializer } from '../../utils/modelSerializer';
-import * as PIXI from 'pixi.js';
 
 export class TextArea extends ModelGraphics {
-  public readonly uuid: string;
-  public readonly typeId: GraphicTypeId;
-  public cursor: CSSStyleProperty.Cursor;
-  public borderWidth: number;
-  public borderColor: number;
-  public color: number;
-  public text: string;
-  public isEditing: boolean = false;
-  public textSprite: PIXI.Text;
-  public inputField: HTMLInputElement;
+	public readonly uuid: string;
+	public readonly typeId: GraphicTypeId;
+	public cursor: CSSStyleProperty.Cursor;
+	public borderWidth: number;
+	public borderColor: number;
+	public color: number;
 
-  static registerGraphic(attributes: SerializedGraphic) {
-    return new TextArea(attributes);
-  }
+	public text: string;
+	public textSprite: Text;
+	public inputField: HTMLInputElement;
+	public textStyle: TextStyle;
 
-  constructor(attributes: SerializedGraphic) {
-    super();
+	static registerGraphic(attributes: SerializedGraphic) {
+		return new TextArea(attributes);
+	}
 
-    const { uuid, typeId, bounds, properties } = attributes;
+	constructor(attributes: SerializedGraphic) {
+		super();
 
-    this.uuid = uuid;
-    this.typeId = typeId as GraphicTypeId;
-    this.eventMode = properties.eventMode;
-    this.borderWidth = properties.borderWidth;
-    this.borderColor = properties.borderColor;
-    this.cursor = properties.cursor;
-    this.color = properties.color;
-    this.alpha = properties.alpha;
-    this.text = 'Exemple de texte';
-    this.draw(bounds);
-  }
+		const { uuid, typeId, bounds, properties } = attributes;
 
-  private updateText() {
-    if (this.textSprite) {
-      this.textSprite.text = this.text;
-    }
-  }
+		this.uuid = uuid;
+		this.typeId = typeId as GraphicTypeId;
+		this.eventMode = properties.eventMode;
+		this.borderWidth = properties.borderWidth;
+		this.borderColor = properties.borderColor;
+		this.cursor = properties.cursor;
+		this.color = properties.color;
+		this.alpha = properties.alpha;
 
-  public draw(bounds: Partial<ElementBounds>) {
-    const { width, height, x, y } = bounds;
-    this.position.set(x, y);
+		this.text = 'Exemple de texte';
+		this.textStyle = new TextStyle({
+			fill: this.color,
+			fontSize: 14,
+		});
+		this.textSprite = new Text(this.text, this.textStyle);
+		this.textSprite.eventMode = "static";
+		this.addChild(this.textSprite);
+		this.updateText();
 
-    this.removeChildren();
+		this.draw(bounds);
+	}
 
-    this.beginFill(this.color);
-    if (this.borderWidth > 0) {
-      this.lineStyle(this.borderWidth, this.borderColor, 1);
-    }
-    this.drawRect(0, 0, width, height);
-    this.endFill();
+	public updateText() {
+		if (this.textSprite) {
+			this.textSprite.text = this.text;
+		}
+	}
 
-    const textStyle = new PIXI.TextStyle({
-      fill: 'black',
-      fontSize: 14,
-    });
-    this.textSprite = new PIXI.Text(this.text, textStyle);
-    this.textSprite.x = 0;
-    this.textSprite.y = 0;
-    this.addChild(this.textSprite);
+	public draw(bounds: Partial<ElementBounds>) {
+		const { width, height, x, y } = bounds;
+		this.position.set(x, y);
 
-    this.textSprite.eventMode = "static";
+		this.clear();
+		this.beginFill(this.color, 0);
+		if (this.borderWidth > 0) {
+			this.lineStyle(this.borderWidth, this.borderColor, 1);
+		}
+		this.drawRect(0, 0, width, height);
+		this.endFill();
+		
+		this.textSprite.position.set(0, 0);
+	}
 
-    this.textSprite.on('pointerdown', () => {
-      this.startEditing();
-    });
+	public serialized() {
+		return modelSerializer(this);
+	}
 
-    this.textSprite.on('pointerup', () => {
-      this.endEditing();
-    });
+	public serializedBounds() {
+		return modelBounds(this);
+	}
 
-    this.updateText();
-  }
-
-  public startEditing() {
-    if (!this.isEditing) {
-      this.isEditing = true;
-      this.textSprite.visible = false;
-      const viewport = document.getElementById("viewport")
-
-
-      this.inputField = document.createElement('input');
-      this.inputField.type = 'text';
-      this.inputField.style.position = 'absolute';
-      this.inputField.style.color = 'black';
-      this.inputField.style.left = `${this.x}px`;
-      this.inputField.style.top = `${this.y}px`;
-      this.inputField.style.width = `${this.textSprite.width}px`;
-      this.inputField.style.height = `${this.textSprite.height}px`;
-      this.inputField.value = this.text;
-      this.inputField.addEventListener('input', () => {
-        this.text = this.inputField.value;
-        this.updateText();
-      });
-      viewport.appendChild(this.inputField);
-      this.inputField.focus();
-    }
-  }
-
-  public endEditing() {
-    if (this.isEditing) {
-      this.isEditing = false;
-      this.textSprite.visible = true;
-      if (this.inputField && this.inputField.parentNode) {
-        this.inputField.parentNode.removeChild(this.inputField);
-      }
-
-      this.text = this.inputField.value;
-      this.updateText();
-    }
-  }
-
-  public serialized() {
-    return modelSerializer(this);
-  }
-
-  public serializedBounds() {
-    return modelBounds(this);
-  }
-
-  public serializedColorimetry() {
-    return modelColorimetry(this);
-  }
+	public serializedColorimetry() {
+		return modelColorimetry(this);
+	}
 }
