0436292196f60617fb0f8b11bb722996bad1893c
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-06-08 08:54:47 +0200

    fix(tracking): Add the text-container to the tracking

--- front-end/src/lib/pixi-tools-v2/plugins/viewportSocketPlugin.ts
+++ front-end/src/lib/pixi-tools-v2/plugins/viewportSocketPlugin.ts
@@ -15,6 +15,7 @@ import {
 	SerializedColorimetry,
 } from '../types/pixi-serialize';
 import { LineContainer } from '../class/lineContainer';
+import { TextContainer } from '../class/textContainer';
 
 interface CanvasSocketEvents {
 	'ws-element-deleted': (uuid: string, uuidFrame?: string) => void;
@@ -47,6 +48,7 @@ export class ViewportSocketPlugin extends utils.EventEmitter<CanvasSocketEvents>
 	public readonly elements:
 		| Record<string, CanvasContainer>
 		| Record<string, ModelGraphics>
+		| Record<string, TextContainer>
 		| Record<string, LineContainer> = {};
 
 	constructor(viewport: ViewportUI, socketOptions?: CanvasSocketOptions) {
@@ -113,9 +115,17 @@ export class ViewportSocketPlugin extends utils.EventEmitter<CanvasSocketEvents>
 			this.elements[container.uuid] = container;
 			const genericChild = container.getGraphicChildren()[0];
 			this.elements[genericChild.uuid] = genericChild;
+
+			// TODO: Thomas, you need to support the TextContainer tracking when it's inside the generic container
 		}
 
 		if (container instanceof LineContainer) {
+			this.elements[container.uuid] = container;
+			const child = container.children[0];
+			this.elements[child.uuid] = child;
+		}
+
+		if (container instanceof TextContainer) {
 			this.elements[container.uuid] = container;
 			const child = container.children[0];
 			this.elements[child.uuid] = child;
