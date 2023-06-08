b27a78e28a601ac6c74264d302b4edb6a746aa85
Vahé Krikorian <vahe.krikorian@cloud4dev.fr>
2023-06-08 14:40:40 +0200

    get selected group ids

--- front-end/src/views/OrganisationView.vue
+++ front-end/src/views/OrganisationView.vue
@@ -18,8 +18,8 @@
                         <span style="font-size:20px;" >Critères d'acceptation:<br/></span>
                         <span style="font-size:20px;" >Valeur:<br/></span>
                         <div style="position:absolute;right:10px;bottom:10px;">
-                            <button><img class="icon" src="https://cdn-icons-png.flaticon.com/512/2089/2089792.png" alt="orga_1"></button>
-                            <button style="background-color:red;"><img class="icon" src="https://cdn-icons-png.flaticon.com/512/542/542724.png" alt="orga_2"></button>
+                            <button><img class="icon" src="https://cdn-icons-png.flaticon.com/512/2089/2089792.png"></button>
+                            <button style="background-color:red;"><img class="icon" src="https://cdn-icons-png.flaticon.com/512/542/542724.png"></button>
                         </div>
                     </div>
                 </div>
@@ -30,24 +30,32 @@
                     <div class="iconeGroupe" v-for="group in groups">
                         <span style="font-size:20px;" v-for="student in group">{{ student }}<br/></span>
                         <div style="position:absolute;left:10px;bottom:10px;">
-                            <button @click="openProject(group)" class="rounded-lg hover:bg-stone-300">Ouvrir le projet</button>
+                            <button @click="openProject(group)" class="text-white font-bold rounded-lg text-sm px-4 py-2 focus:outline-none flex justify-center items-center gap-2 gradiant">Ouvrir le projet</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div v-show="showProject" class="ml-12">
-            <div>
-                <p class="text-3xl font-bold dark:text-[#9ca3af] text-[#213547]"></p>
-                <p class="text-lg font-medium dark:text-[#9ca3af] text-[#213547]"></p>
+        <div v-show="showProject" class="">
+            <div class="static">
+                <p class="text-3xl font-bold dark:text-[#9ca3af] text-[#213547]">Groupe 1</p>                
+                <p class="text-lg font-medium dark:text-[#9ca3af] text-[#213547]">{{ selectedCourse }}</p>
+                <button class="absolute -mt-12 right-10 md:right-14 lg:right-20 text-white p-2 rounded-lg hover:bg-light-secondary dark:hover:bg-dark-tertiary gradiant bg-[#24292E] hover:bg-[#24292E99] shadow">
+                    Nouveau board
+            </button>
             </div>
-            <div class="w-full justify-center h-fit rounded-lg bg-light-primary dark:bg-dark-tertiary py-4 px-4" v-if="courseId != ''">
+            <div class="flex">
+            <div class="w-full h-fit justify-center rounded-lg bg-light-tertiary dark:bg-dark-tertiary py-4 px-4" v-if="courseId != ''">
                 <h1 class="dark:text-[#9ca3af] text-[#213547]">Membres</h1>
             <div class="flex flex-row flex-wrap gap-3 mt-4">
                 <div class="bg-[#5C5F73] rounded-lg px-4 py-2" v-for="student in selectedGroup" :key="student">
-                    <p class="text-2xl text-[#9ca3af]">{{ student }}</p>
+                    <p class="text-2xl text-white">{{ student }}</p>
                 </div>
             </div>
+            </div>
+            <button class="p-2 button-add-members hover:bg-light-secondary dark:hover:bg-dark-tertiary gradiant relative right-4 bg-[#24292E] hover:bg-[#24292E99] shadow">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" width="1.5em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
+            </button>
             </div>
         </div>
     </div>
@@ -61,8 +69,11 @@ let courseId = '';
 let courses = [];
 let studentList = [];
 let groups = [];
+let groups2 = [];
 let showProject = '';
 let selectedGroup = []
+let selectedIDs = [];
+let selectedCourse = ''
 
 export default {
 
@@ -72,11 +83,14 @@ export default {
 		return {
             courses,
             courseId,
-			groups,
+            groups,
+            groups2,
 			studentList,
 			isPO: false,
             showProject, 
             selectedGroup,
+            selectedCourse,
+            selectedIDs,
 		};
     },    
 
@@ -86,7 +100,7 @@ export default {
 	methods: {
 		getCourses: withErrorHandler(async function () {
 			http.get(`/courses`).then((response) => {
-				this.courses = response.data;
+				this.courses = [...response.data];
 			});
 		}),
         getStudentList: withErrorHandler(async function () {
@@ -107,19 +121,24 @@ export default {
         openProject(group) {
             this.showProject = true;
             this.selectedGroup = [...group];
-            console.log(this.selectedGroup)
+            this.selectedIDs = this.groups2[this.groups.indexOf(group)];
+            console.log('G1: ',this.selectedGroup);
+            console.log('G2: ',this.selectIDs);
         },
   
         setCourseId(event) {
             let course;
             this.courses.forEach(courseElt => {
                 if(courseElt._id == event.target.value) {
+                    console.log('Course: ',courseElt);
                     course = courseElt;
                 }
             });
 
+            this.selectedCourse = course.tag;
             this.courseId = course._id;
-            this.groups = course.groups;
+            this.groups = JSON.parse(JSON.stringify(course.groups));
+            this.groups2 = JSON.parse(JSON.stringify(course.groups));
             this.getStudentList();
         }
 	},
@@ -127,7 +146,6 @@ export default {
 </script>
 
 <style>
-/** Don't add global style, thank you ! */
 .groups {
     display: flex;
     flex-wrap: wrap;
@@ -168,8 +186,22 @@ export default {
 .relative {
     position: relative;
 }
+button {
+    background-color: #bfbfbf;
+    padding: 5px;
+    font-size: 15px;
+}
 .icon {
     width: 20px;
     height: 20px;
+}
+
+svg {
+    fill:#ffffff;
+}
+
+.button-add-members {
+    border-top-right-radius: 0.5rem;
+    border-bottom-right-radius: 0.5rem;
 }
 </style>
