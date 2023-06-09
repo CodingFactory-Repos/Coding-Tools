227fdba297607869d4852dc319ce33570c1e99a5
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2023-06-09 13:19:29 +0200

    Svg Folder + Parts CoursDetails

--- front-end/src/components/RessourcesComponents/CoursDetails.vue
+++ front-end/src/components/RessourcesComponents/CoursDetails.vue
@@ -8,9 +8,9 @@
 	<div class="text-center pt-4">
 		<h1 class="text-4xl font-bold">TEST</h1>
 	</div>
-	<div class="cours-detail bg-grey-800">
+	<div class="cours-detail bg-grey-800 mb-5">
 		<div class="corps-enseignant">
-			<h4 class="text-lg font-bold mb-2 border-b border-gray-300">Corps enseignant du cours</h4>
+			<h4 class="text-2xl font-bold mb-2 border-b border-gray-300">Corps enseignant du cours</h4>
 
 			<ul class="space-y-2">
 				<li v-for="teacher in teachers" :key="teacher.id" class="flex items-center">
@@ -19,12 +19,25 @@
 						alt="Profile Picture"
 						class="w-8 h-8 rounded-full mr-2"
 					/>
-					<span class="text-white-800">{{ teacher.name }}</span>
+					<span class="text-white-800 text-lg mb-5">{{ teacher.name }}</span>
 				</li>
 			</ul>
+			<div class="detail-actions">
+				<div class="corps-ensaignant">
+					<h4 class="text-2xl font-bold mb-2 border-b border-gray-300">Détails et actions</h4>
+					<ul class="space-y-2">
+						<li>
+							<h4 class="text-2xl font-bold mb-2">Annuaire</h4>
+							<p class="text-blue-500 underline cursor-pointer">
+								Afficher tout les membres de votre cours
+							</p>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 		<div class="contenu-cours">
-			<h4 class="text-lg font-bold mb-2 border-b border-gray-300">Contenu du cours</h4>
+			<h4 class="text-2xl font-bold mb-2 border-b border-gray-300">Contenu du cours</h4>
 			<ul class="space-y-4">
 				<li v-for="course in courses" :key="course.id">
 					<div class="course-item" :class="{ open: course.open }" @click="toggleCourse(course)">
@@ -40,107 +53,57 @@
 					</div>
 				</li>
 			</ul>
+			<h4 class="text-2xl font-bold mb-2 mt-3 border-b border-gray-300">Groupes</h4>
+			<ul class="space-y-4">
+				<li v-for="groupe in groupes" :key="groupe.id">
+					<div class="course-item" :class="{ open: groupe.open }" @click="toggleGroupe(groupe)">
+						<h5 class="text-xl font-semibold">{{ groupe.title }}</h5>
+						<p class="text-gray-600">{{ groupe.description }}</p>
+					</div>
+					<div v-if="groupe.open" class="course-files">
+						<ul class="space-y-2">
+							<li v-for="file in groupe.files" :key="file.id">
+								<a :href="file.url" target="_blank" class="text-blue-500">{{ file.name }}</a>
+							</li>
+						</ul>
+					</div>
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
 
-<script>
-export default {
-	data() {
-		return {
-			teachers: [
-				{
-					id: 1,
-					name: 'John Doe',
-					profilePicture: 'path/to/profile-picture1.jpg',
-				},
-				{
-					id: 2,
-					name: 'Jane Smith',
-					profilePicture: 'path/to/profile-picture2.jpg',
-				},
-			],
-			courses: [
-				{
-					id: 1,
-					title: 'Course 1',
-					description: 'Description of Course 1',
-					open: false,
-					files: [
-						{
-							id: 1,
-							name: 'File 1.pdf',
-							url: 'path/to/file1.pdf',
-						},
-						{
-							id: 2,
-							name: 'File 2.pdf',
-							url: 'path/to/file2.pdf',
-						},
-					],
-				},
-				{
-					id: 2,
-					title: 'Course 2',
-					description: 'Description of Course 2',
-					open: false,
-					files: [
-						{
-							id: 3,
-							name: 'File 3.pdf',
-							url: 'path/to/file3.pdf',
-						},
-					],
-				},
-				{
-					id: 3,
-					title: 'Course 3',
-					description: 'Description of Course 3',
-					open: false,
-					files: [
-						{
-							id: 4,
-							name: 'File 4.pdf',
-							url: 'path/to/file4.pdf',
-						},
-						{
-							id: 5,
-							name: 'File 5.pdf',
-							url: 'path/to/file5.pdf',
-						},
-					],
-				},
-			],
-		};
-	},
-	methods: {
-		toggleCourse(course) {
-			course.open = !course.open;
-		},
-	},
-};
+<script lang="ts">
+import { computed, onMounted, ref } from 'vue';
+import { useCoursStore } from '@/store/modules/course.store';
+import { useAuthStore } from '@/store/modules/auth.store';
+import { useRouter } from 'vue-router';
+import Folder from '@/components/common/svg/Folder.vue';
+
 // get store
-// const course = useCoursStore();
-// const oneItems = computed(() => course.oneItems);
+// Use the openHouses store
+const courseStore = useCoursStore();
+const authStore = useAuthStore();
+const oneItems = computed(() => courseStore.oneItems);
 
-// const router = useRouter();
+const router = useRouter();
 
-// // get id from url
-// const _id = computed(() => {
-// 	const url = window.location.href;
-// 	const id = url.substring(url.lastIndexOf('/') + 1);
-// 	return id;
-// });
+// get id from url
+const _id = computed(() => {
+	const url = window.location.href;
+	const id = url.substring(url.lastIndexOf('/') + 1);
+	return id;
+});
 
-// // get openHouse by id
-// const getCourseById = async (_id: string) => {
-// 	await course.getCourseById(_id);
-// };
+// get openHouse by id
+const getCourseById = async (_id: string) => {
+	await courseStore.getCourseById(_id);
+};
 
-// // fetch openHouse data on mounted
-// onMounted(() => {
-// 	getCourseById(_id.value);
-// });
+// fetch openHouse data on mounted
+onMounted(() => {
+	getCourseById(_id.value);
+});
 </script>
 <style scoped>
 .cours-detail {
