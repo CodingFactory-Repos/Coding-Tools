5c0629918242e39568423d83939fa21430b182a4
AyakorK <guillaume.moret@yahoo.com>
2023-06-13 01:31:15 +0200

    Move the groups to an other page by removing useless buttons and adding to the drawer layout

--- front-end/src/views/calls/RollCallView.vue
+++ front-end/src/views/calls/RollCallView.vue
@@ -1,13 +1,11 @@
 <script lang="ts">
 import CallsComponent from '@/components/RollCallComponents/CallsComponent.vue';
 import StudentListComponent from '@/components/RollCallComponents/StudentListComponent.vue';
-import MakeGroupsComponent from '@/components/RollCallComponents/MakeGroupsComponent.vue';
 import Chat from '@/components/RollCallComponents/Chat.vue';
 export default {
 	components: {
 		CallsComponent,
 		StudentListComponent,
-		MakeGroupsComponent,
 		Chat,
 	},
 	data() {
@@ -22,6 +20,5 @@ export default {
 <template>
 	<CallsComponent />
 	<StudentListComponent />
-	<MakeGroupsComponent />
 	<Chat room-id="1" />
 </template>
