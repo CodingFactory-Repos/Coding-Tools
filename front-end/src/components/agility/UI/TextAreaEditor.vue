a51c8ce019a8668e78ddfb4eb036e96535685b68
Thomlam <lamiablethomas@gmail.com>
2023-06-15 18:17:59 +0200

    feature(AddMoreActionClick) : Add finaly action click for toggle oblique, Italic and more

--- front-end/src/components/agility/UI/TextAreaEditor.vue
+++ front-end/src/components/agility/UI/TextAreaEditor.vue
@@ -21,18 +21,11 @@
             Italic
           </button>
 		   <button
-            :class="{ 'bg-blue-500 text-white': isItalic }"
-            @click="toggleItalic"
+            :class="{ 'bg-blue-500 text-white': isOblique }"
+            @click="toggleOblique"
             class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded mr-2"
           >
-            Italic
-          </button>
-		   <button
-            :class="{ 'bg-blue-500 text-white': isItalic }"
-            @click="toggleItalic"
-            class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded mr-2"
-          >
-            Italic
+            Oblique
           </button>
         </div>
         <div class="flex mb-2">
@@ -43,12 +36,20 @@
             <option value="Verdana">Verdana</option>
           </select>
         </div>
+          <div class="flex mb-2">
+          <label class="mr-2">Alignment :</label>
+          <select v-model="fontAlignment" class="bg-white border border-gray-300 rounded px-2 py-1">
+            <option value="left">Left</option>
+            <option value="center">Center</option>
+            <option value="right">Right</option>
+          </select>
+        </div>
         <div class="flex mb-2">
           <label class="mr-2">Taille :</label>
           <select v-model="fontSize" class="bg-white border border-gray-300 rounded px-2 py-1">
-            <option value="12px">Petit</option>
-            <option value="16px">Moyen</option>
-            <option value="20px">Grand</option>
+            <option value="12">Petit</option>
+            <option value="16">Moyen</option>
+            <option value="20">Grand</option>
           </select>
         </div>
       </div>
@@ -76,7 +77,16 @@ import { TextContainer } from '@/lib/pixi-tools-v2/class/textContainer';
 
 
 const isTextAreaEdited = ref(false);
+const isBold = ref(false);
+const isItalic = ref(false);
+const isOblique = ref(false);
+
+
 const projectStore = useProjectStore();
+const selectedContainers = computed(() => projectStore.getSelected);
+const fontFamily = ref('Arial');
+const fontAlignment = ref('left');
+const fontSize = ref('12');
 
 
 const toggleTextAreaEditor = () => {
@@ -87,4 +97,56 @@ const toggleTextAreaEditor = () => {
 watch(isTextAreaEdited, (val) => {
 	projectStore.scene.viewport.manager.isEditingContainerProperties = val;
 })
+
+const toggleItalic = () => {
+  isItalic.value = !isItalic.value;
+  const ctn = selectedContainers.value[0]
+  if (ctn instanceof TextContainer && isItalic) {
+    ctn.textGraphic.textSprite.fontStyle = "italic";
+  }
+  else  ctn.textGraphic.textSprite.fontStyle = "normal";
+}
+const toggleBold = () => {
+  isBold.value = !isBold.value;
+  const ctn = selectedContainers.value[0]
+  if (ctn instanceof TextContainer && isBold) {
+      ctn.textGraphic.textSprite.fontWeight = "bold";
+  }
+  else if (ctn instanceof TextContainer) ctn.textGraphic.textSprite.fontWeight = "bold";
+  else return;
+    projectStore.scene.viewport.socketPlugin.emit(
+      'ws-text-updated',
+      ctn.uuid,
+      ctn.serializeData()
+    )
+}
+const toggleOblique= () => {
+  isOblique.value = !isOblique.value;
+  const ctn = selectedContainers.value[0]
+  if (ctn instanceof TextContainer && isOblique) {
+    ctn.textGraphic.textSprite.fontStyle = "oblique";
+
+
+  }
+  else if (ctn instanceof TextContainer) ctn.textGraphic.textSprite.fontStyle = "normal";
+  else return;
+    projectStore.scene.viewport.socketPlugin.emit(
+      'ws-text-updated',
+      ctn.uuid,
+      ctn.serializeData()
+    )
+}
+
+
+
+
+
+const GetTextArea = ()=> {
+  const ctn = selectedContainers.value[0]
+  if (ctn instanceof TextContainer) {    
+  }
+			if(parent instanceof TextContainer){
+       
+			} 
+}
 </script>
