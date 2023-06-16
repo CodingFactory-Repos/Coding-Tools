58d917664f46a4d85baef0c4c42a376d3f1a0f0c
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2023-06-16 14:01:36 +0200

    Change CourseDetails

--- front-end/src/components/RessourcesComponents/AddCourse.vue
+++ front-end/src/components/RessourcesComponents/AddCourse.vue
@@ -2,10 +2,12 @@
 	<div class="modal">
 		<div class="modal-overlay" @click="$emit('close')"></div>
 		<div class="modal-container">
-			<h3 class="text-xl pb-2 font-medium leading-6 text-gray-900 flex justify-center mb-3">
+			<h3
+				class="text-xl pb-2 font-medium leading-6 text-gray-900 dark:text-white flex justify-center mb-3"
+			>
 				Ajouter un cours
 			</h3>
-			<div class="modal-body">
+			<div class="modal-body text-gray-900 dark:text-white">
 				<form class="w-full max-w-sm">
 					<input type="text" name="title" v-model="tag" placeholder="Title :" /><br />
 					<VueDatePicker
@@ -37,14 +39,14 @@
 					<input type="file" id="file" ref="fileInput" @change="onFileSelected" />
 					<div class="flex justify-center mt-6">
 						<button
-							class="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+							class="px-4 py-2 text-gray-900 dark:text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
 							type="button"
 							@click="$emit('close')"
 						>
 							Fermer
 						</button>
 						<button
-							class="ml-4 px-4 py-2 text-white bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+							class="ml-4 px-4 py-2 text-gray-900 dark:text-white bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
 							type="submit"
 							@click="AddCourses"
 						>
